74866ad4c1ccb44cb54e137b4c49ff630f6aa777
Maninder Singh <maninder.singh@gojek.com>
2021-02-05 11:43:20 +0530

    Authentication using plugin instead of if else block , seprate http method handler

--- app/group/user/handler.ts
+++ app/group/user/handler.ts
@@ -1,61 +1,68 @@
 import Hapi from '@hapi/hapi';
-import Boom from '@hapi/boom';
 import * as Schema from './schema';
 import * as Resource from './resource';
-import CasbinSingleton from '../../../lib/casbin';
 
-const hasAccess = (request: Hapi.Request) => {
-  if (request.method === 'get') {
-    return true; // get api is open for all
+const iamConfig = {
+  iam: {
+    authorize: [
+      {
+        action: { baseName: 'iam' },
+        resource: [{ requestKey: 'groupId', iamKey: 'group' }]
+      }
+    ]
   }
-
-  const { id: userId } = request.auth.credentials;
-  const { groupIdentifier } = request.params;
-  let action = 'iam.create';
-  if (request.method === 'put') {
-    action = 'iam.manage';
-  } else if (request.method === 'delete') {
-    action = '';
-  }
-  return CasbinSingleton.enforcer?.enforceJson(
-    { user: userId },
-    {
-      group: groupIdentifier
-    },
-    {
-      action
-    }
-  );
 };
 
-export const operation = {
-  description: 'group and user mapping for create/update/delete resource',
+export const get = {
+  description: 'fetch user and group mapping',
+  tags: ['api'],
+  handler: async (request: Hapi.Request) => {
+    const { groupId, userId } = request.params;
+    return Resource.get(groupId, userId);
+  }
+};
+
+export const post = {
+  description: 'create group and user mapping',
   tags: ['api'],
   validate: {
     payload: Schema.payloadSchema
   },
+  app: iamConfig,
   handler: async (request: Hapi.Request) => {
     const { payload } = request;
     const { groupId, userId } = request.params;
     const { id: loggedInUserId } = request.auth.credentials;
+    return Resource.create(groupId, userId, loggedInUserId, payload);
+  }
+};
 
-    if (!hasAccess(request)) {
-      return Boom.unauthorized(
-        "Sorry you don't have permission to be perform this action"
-      );
-    }
+export const put = {
+  description: 'update group and user mapping',
+  tags: ['api'],
+  validate: {
+    payload: Schema.payloadSchema
+  },
+  app: iamConfig,
+  handler: async (request: Hapi.Request) => {
+    const { payload } = request;
+    const { groupId, userId } = request.params;
+    const { id: loggedInUserId } = request.auth.credentials;
+    return Resource.update(groupId, userId, loggedInUserId, payload);
+  }
+};
 
-    switch (request.method) {
-      case 'get':
-        return Resource.get(groupId, userId);
-      case 'post':
-        return Resource.create(groupId, userId, loggedInUserId, payload);
-      case 'put':
-        return Resource.update(groupId, userId, loggedInUserId, payload);
-      case 'delete':
-        return Resource.remove(groupId, userId, loggedInUserId, payload);
-      default:
-        throw new Error('Requested method not supported');
-    }
+export const remove = {
+  description: 'delete group and user mapping',
+  tags: ['api'],
+  validate: {
+    payload: Schema.payloadSchema
+  },
+  app: iamConfig,
+  handler: async (request: Hapi.Request) => {
+    const { payload } = request;
+    const { groupId, userId } = request.params;
+    const { id: loggedInUserId } = request.auth.credentials;
+    return Resource.remove(groupId, userId, loggedInUserId, payload);
   }
 };
