0f4a81d940b43dc764434b001ed57999a72ba72c
Shreyas Adiyodi <shreyasadiyodi@gmail.com>
2021-02-08 20:22:26 +0530

    Fix group/user handler tests Use test config for authentication

--- test/app/group/user/handler.ts
+++ test/app/group/user/handler.ts
@@ -2,7 +2,6 @@ import Code from 'code';
 import Lab from '@hapi/lab';
 import Hapi from '@hapi/hapi';
 import Sinon from 'sinon';
-import { factory } from 'typeorm-seeding';
 import { lab } from '../../../setup';
 import * as Config from '../../../../config/config';
 import * as groupPlugin from '../../../../app/group';
@@ -11,6 +10,11 @@ import * as GroupUserResource from '../../../../app/group/user/resource';
 exports.lab = Lab.script();
 let server: Hapi.Server;
 const Sandbox = Sinon.createSandbox();
+
+const TEST_AUTH = {
+  strategy: 'test',
+  credentials: { id: 'dev.test' }
+};
 
 lab.before(async () => {
   const plugins = [groupPlugin];
@@ -50,27 +54,24 @@ lab.experiment('Group:User::Handler', () => {
           }
         ]
       };
-      const request = {
+      const request: any = {
         method: 'POST',
         url: `/api/groups/${payload.policies[0].resource.group}/users/${payload.policies[0].subject.user}`,
-        payload: payload,
-        headers: {
-          'X-Goog-Authenticated-User-Email':
-            'accounts.google.com:dev.test@go-jek.com'
-        }
+        payload,
+        auth: TEST_AUTH
       };
       const result: any = payload.policies.map((policy: any) => {
         return { ...policy, success: true };
       });
       createStub = Sandbox.stub(GroupUserResource, 'create').returns(result);
       const response = await server.inject(request);
-      // Sandbox.assert.calledWithExactly(
-      //   createStub,
-      //   'test_group',
-      //   'test_user',
-      //   'dev.test',
-      //   payload
-      // );
+      Sandbox.assert.calledWithExactly(
+        createStub,
+        'test_group',
+        'test_user',
+        TEST_AUTH.credentials.id,
+        payload
+      );
 
       Code.expect(response.result).to.equal(result);
       Code.expect(response.statusCode).to.equal(200);
@@ -114,24 +115,21 @@ lab.experiment('Group:User::Handler', () => {
       const request = {
         method: 'PUT',
         url: `/api/groups/${payload.policies[0].resource.group}/users/${payload.policies[0].subject.user}`,
-        payload: payload,
-        headers: {
-          'X-Goog-Authenticated-User-Email':
-            'accounts.google.com:dev.test@go-jek.com'
-        }
+        payload,
+        auth: TEST_AUTH
       };
       const result: any = payload.policies.map((policy: any) => {
         return { ...policy, success: true };
       });
       createStub = Sandbox.stub(GroupUserResource, 'update').returns(result);
       const response = await server.inject(request);
-      // Sandbox.assert.calledWithExactly(
-      //   createStub,
-      //   'test_group',
-      //   'test_user',
-      //   'dev.test',
-      //   payload
-      // );
+      Sandbox.assert.calledWithExactly(
+        createStub,
+        'test_group',
+        'test_user',
+        TEST_AUTH.credentials.id,
+        payload
+      );
 
       Code.expect(response.result).to.equal(result);
       Code.expect(response.statusCode).to.equal(200);
