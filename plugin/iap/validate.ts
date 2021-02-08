fed1fcacc9e6ee25780117019725ebd599b787d1
Shreyas Adiyodi <shreyas.adiyodi@go-jek.com>
2021-02-08 19:58:33 +0530

    Minor change in policy payload schema config

--- plugin/iap/validate.ts
+++ plugin/iap/validate.ts
@@ -4,7 +4,7 @@ import { getUsernameFromEmail } from './utils';
 const validateByEmail = async (request: Hapi.Request, email: string) => {
   // TODO: fetch user from db using username and upsert and validate the user
   const username = getUsernameFromEmail(email);
-  const id = '0c351e11-776f-41e1-a023-45e27e1728ee'; // alphanumeric user id
+  const id = '5655ed21-eaab-41d0-ae54-1acce751b307'; // alphanumeric user id
   const credentials = { id, username, email };
 
   return { isValid: true, credentials };
