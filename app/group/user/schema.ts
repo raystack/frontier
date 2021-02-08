1e833245459640b232d5df08d0ead0adce0a4e63
Shreyas Adiyodi <shreyasadiyodi@gmail.com>
2021-02-08 19:58:33 +0530

    Minor change in policy payload schema config

--- app/group/user/schema.ts
+++ app/group/user/schema.ts
@@ -1,16 +1,14 @@
 import Joi from 'joi';
 import Config from '../../../config/config';
 
-export const policiesSchema = Joi.array()
-  .items(
-    Joi.object().keys({
-      operation: Joi.string().required(),
-      subject: Joi.object().required(),
-      resource: Joi.object().required(),
-      action: Joi.object().required()
-    })
-  )
-  .options(Config.get('/validationOptions'));
+export const policiesSchema = Joi.array().items(
+  Joi.object().keys({
+    operation: Joi.string().required(),
+    subject: Joi.object().required(),
+    resource: Joi.object().required(),
+    action: Joi.object().required()
+  })
+);
 
 export const payloadSchema = Joi.object()
   .label('GroupUserMappingCreatePayload')
