fa423fb1a6ea2ba8e5d3a78d5a06539aaebbcb1c
Praveen Yadav <pyadav9678@gmail.com>
2021-02-11 16:57:24 +0530

    test: implement check for IAP plugin

---
+++ test/plugin/iap/index.ts
@@ -0,0 +1,61 @@
+import Code from 'code';
+import Lab from '@hapi/lab';
+import Hapi from '@hapi/hapi';
+import Sinon from 'sinon';
+import { lab } from '../../setup';
+import * as iapPlugin from '../../../plugin/iap';
+import * as profilePlugin from '../../../app/profile';
+import * as Config from '../../../config/config';
+
+exports.lab = Lab.script();
+let server: Hapi.Server;
+const Sandbox = Sinon.createSandbox();
+
+lab.before(async () => {
+  const plugins = [iapPlugin, profilePlugin];
+  server = new Hapi.Server({ port: Config.get('/port/web'), debug: false });
+  await server.register(plugins);
+});
+
+lab.after(async () => {
+  await server.stop();
+});
+
+lab.afterEach(() => {
+  Sandbox.restore();
+});
+
+lab.experiment('IAPPlugin', () => {
+  let request, payload;
+
+  lab.beforeEach(async () => {
+    payload = {
+      message: 'pong',
+      status: 'ok',
+      statusCode: 200
+    };
+  });
+
+  lab.test('should reject /api/profile on missing iap headers', async () => {
+    request = {
+      method: 'GET',
+      url: `/api/profile`,
+      payload
+    };
+    const response = await server.inject(request);
+    Code.expect(response.statusCode).to.equal(401);
+  });
+
+  lab.test('should allow /api/profile on providing iap headers', async () => {
+    request = {
+      method: 'GET',
+      url: `/api/profile`,
+      payload,
+      headers: {
+        'x-goog-authenticated-user-email': 'praveen.yadav@gojek.com'
+      }
+    };
+    const response = await server.inject(request);
+    Code.expect(response.statusCode).to.equal(200);
+  });
+});
