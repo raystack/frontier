040ad1ff538b5d4baa82a0eb518ad1df0666f825
Shreyas Adiyodi <shreyas.adiyodi@go-jek.com>
2021-02-16 17:24:23 +0530

    feat: get list of groups based on attribute filters

---
+++ app/group/util.ts
@@ -0,0 +1,40 @@
+import * as R from 'ramda';
+import { toCamelCase, parsePolicies } from '../policy/util';
+
+type JSObj = Record<string, undefined>;
+
+type GroupRawResult = {
+  is_member: string;
+  member_count: string;
+  raw_attributes: JSObj[];
+  id: string;
+  displayName: string;
+  metadata: JSObj;
+  group_arr: JSObj[];
+  raw_member_policies?: JSObj[];
+};
+
+const parseGroupResult = (group: GroupRawResult) => {
+  const {
+    raw_attributes: rawAttributes,
+    is_member: isMember,
+    member_count: memberCount,
+    group_arr: groupArr,
+    raw_member_policies: rawMemberPolicies
+  } = group;
+
+  const attributes = rawAttributes.map(
+    R.pipe(R.propOr('{}', 'v1'), JSON.parse)
+  );
+  const parsedGroup = R.head(groupArr);
+
+  return {
+    ...toCamelCase(parsedGroup || {}),
+    isMember,
+    memberPolicies: parsePolicies(rawMemberPolicies || []),
+    memberCount: +memberCount,
+    attributes
+  };
+};
+
+export const parseGroupListResult = R.map(parseGroupResult);
--- test/app/group/resource.ts
+++ test/app/group/resource.ts
@@ -6,8 +6,11 @@ import Faker from 'faker';
 import { factory } from 'typeorm-seeding';
 import { lab } from '../../setup';
 import { Group } from '../../../model/group';
+import { User } from '../../../model/user';
 import * as Resource from '../../../app/group/resource';
 import * as PolicyResource from '../../../app/policy/resource';
+import CasbinSingleton from '../../../lib/casbin';
+import * as Config from '../../../config/config';
 
 exports.lab = Lab.script();
 const Sandbox = Sinon.createSandbox();
@@ -214,6 +217,200 @@ lab.experiment('Group::resource', () => {
         ...payload,
         policyOperationResult: [{ operation: 'create', success: true }]
       });
+    });
+  });
+
+  lab.experiment('get list of groups', () => {
+    let groups, users, currentUser;
+
+    const sortByDisplayName = R.sortBy(R.propOr(null, 'displayName'));
+    const removeExtraKeys = R.map(R.omit(['createdAt', 'updatedAt']));
+
+    const sortMemberPolicies = (results) => {
+      const sortByUserId = R.sortBy(R.path(['subject', 'user']));
+
+      return results.map((group) => {
+        const sortedPolicies = sortByUserId(group.memberPolicies);
+        return R.assoc('memberPolicies', sortedPolicies, group);
+      });
+    };
+
+    const parseResults = R.pipe(
+      removeExtraKeys,
+      sortByDisplayName,
+      sortMemberPolicies
+    );
+
+    const getMemberPolicy = (user, group, role) => {
+      return {
+        subject: { user: user.id },
+        resource: { group: group.id },
+        action: { role }
+      };
+    };
+
+    lab.beforeEach(async () => {
+      // setup data
+      const dbUri = Config.get('/postgres').uri;
+      const enforcer = await CasbinSingleton.create(dbUri);
+
+      // create 5 groups
+      groups = await factory(Group)().createMany(5);
+      // create 10 users
+      users = await factory(User)().createMany(10);
+      currentUser = R.head(users);
+
+      const getId = R.path(['id']);
+
+      const makeUserTeamAdmin = async (user, group) => {
+        const groupId = getId(group);
+        const userId = getId(user);
+
+        await enforcer?.addSubjectGroupingJsonPolicy(
+          { user: userId },
+          { group: groupId }
+        );
+        await enforcer?.addJsonPolicy(
+          { user: userId },
+          { group: groupId },
+          { role: 'team.admin' }
+        );
+      };
+
+      // map 3 groups with gojek, and also with currentUser
+      await Promise.all(
+        R.take(3, groups).map(async (group) => {
+          const groupId = getId(group);
+          await enforcer?.addResourceGroupingJsonPolicy(
+            { group: groupId },
+            { entity: 'gojek' }
+          );
+
+          await enforcer?.addSubjectGroupingJsonPolicy(
+            { user: getId(currentUser) },
+            { group: groupId }
+          );
+        })
+      );
+
+      // map 2 groups with gofin
+      await Promise.all(
+        R.takeLast(2, groups).map(async (group) => {
+          await enforcer?.addResourceGroupingJsonPolicy(
+            { group: getId(group) },
+            { entity: 'gofin' }
+          );
+        })
+      );
+
+      // add 1,2 users as admin of group[0]
+      await Promise.all(
+        R.take(2, users).map(async (user) => {
+          const group = groups[0];
+          await makeUserTeamAdmin(user, group);
+        })
+      );
+
+      // add 3 users as admin of group[1]
+      await Promise.all(
+        R.takeLast(3, users).map(async (user) => {
+          const group = groups[1];
+          await makeUserTeamAdmin(user, group);
+        })
+      );
+    });
+
+    lab.test('should get list of groups', async () => {
+      const result = await Resource.list({}, currentUser.id);
+
+      const gojekAttributes = [{ entity: 'gojek' }];
+      const gofinAttributes = [{ entity: 'gofin' }];
+
+      const expectedResult = [
+        {
+          ...groups[0],
+          isMember: true,
+          memberCount: 2,
+          attributes: gojekAttributes,
+          memberPolicies: []
+        },
+        {
+          ...groups[1],
+          isMember: true,
+          memberCount: 4,
+          attributes: gojekAttributes,
+          memberPolicies: []
+        },
+        {
+          ...groups[2],
+          isMember: true,
+          memberCount: 1,
+          attributes: gojekAttributes,
+          memberPolicies: []
+        },
+        {
+          ...groups[3],
+          isMember: false,
+          memberCount: 0,
+          attributes: gofinAttributes,
+          memberPolicies: []
+        },
+        {
+          ...groups[4],
+          isMember: false,
+          memberCount: 0,
+          attributes: gofinAttributes,
+          memberPolicies: []
+        }
+      ];
+
+      const sortedResult = parseResults(result);
+      const sortedExpectedResult = parseResults(expectedResult);
+      Code.expect(sortedResult).to.equal(<any>sortedExpectedResult);
+    });
+
+    lab.test('should get list of groups with filters', async () => {
+      const result = await Resource.list(
+        { entity: 'gojek', member_role: 'team.admin' },
+        currentUser.id
+      );
+
+      const gojekAttributes = [{ entity: 'gojek' }];
+
+      const expectedResult = [
+        {
+          ...groups[0],
+          isMember: true,
+          memberCount: 2,
+          attributes: gojekAttributes,
+          memberPolicies: [
+            getMemberPolicy(users[0], groups[0], 'team.admin'),
+            getMemberPolicy(users[1], groups[0], 'team.admin')
+          ]
+        },
+        {
+          ...groups[1],
+          isMember: true,
+          memberCount: 4,
+          attributes: gojekAttributes,
+          memberPolicies: [
+            getMemberPolicy(users[7], groups[1], 'team.admin'),
+            getMemberPolicy(users[8], groups[1], 'team.admin'),
+            getMemberPolicy(users[9], groups[1], 'team.admin')
+          ]
+        },
+        {
+          ...groups[2],
+          isMember: true,
+          memberCount: 1,
+          attributes: gojekAttributes,
+          memberPolicies: []
+        }
+      ];
+      debugger;
+      const sortedResult = parseResults(result);
+      const sortedExpectedResult = parseResults(expectedResult);
+      Code.expect(sortedResult).to.equal(<any>sortedExpectedResult);
     });
   });
 });
