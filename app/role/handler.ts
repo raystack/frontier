77b96e3f8a0f372d9d61fc203d74a395fc8125a5
Shreyas Adiyodi <shreyasadiyodi@gmail.com>
2021-03-18 17:36:48 +0530

    feat: implement role creation along with action mapping API

--- app/role/handler.ts
+++ app/role/handler.ts
@@ -1,5 +1,6 @@
 import Hapi from '@hapi/hapi';
 import * as Resource from './resource';
+import * as Schema from './schema';
 
 export const get = {
   description: 'fetch roles based on attributes',
@@ -9,5 +10,17 @@ export const get = {
     return Resource.get(
       attributes.constructor === String ? [attributes] : attributes
     );
+  }
+};
+
+export const create = {
+  description: 'create roles along with action mapping',
+  tags: ['api'],
+  validate: {
+    payload: Schema.createPayload
+  },
+  handler: async (request: Hapi.Request) => {
+    const user: any = request.auth?.credentials;
+    return Resource.create(<any>request.payload, user);
   }
 };
