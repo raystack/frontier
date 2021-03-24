b100853e8f4d54a234be6b3e0fdc953ac4209752
Shreyas Adiyodi <shreyasadiyodi@gmail.com>
2021-03-24 13:57:37 +0530

    feat: add test github action

--- factory/group.ts
+++ factory/group.ts
@@ -8,7 +8,7 @@ define(Group, (faker: typeof Faker) => {
 
   const group = new Group();
   group.id = faker.random.uuid();
-  group.groupname = name.toLowerCase();
+  group.groupname = faker.random.uuid();
   group.displayname = name;
   group.metadata = {
     email: `${group.groupname}@go-jek.com`
