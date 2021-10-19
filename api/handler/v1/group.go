98a2f974947c2269d8ecefb2bd39426459d15742
Kartik Verma <kartik.verma@gojek.com>
2021-10-19 13:41:31 +0530

    fix: update grpc implementations

--- api/handler/v1/group.go
+++ api/handler/v1/group.go
@@ -5,22 +5,22 @@ import (
 	shieldv1 "github.com/odpf/shield/api/protos/github.com/odpf/proton/shield/v1"
 )
 
-func (v Dep) GetAllGroups(ctx context.Context, request *shieldv1.GetAllGroupsRequest) (*shieldv1.GetAllGroupsResponse, error) {
+func (v Dep) ListGroups(ctx context.Context, request *shieldv1.ListGroupsRequest) (*shieldv1.ListGroupsResponse, error) {
 	panic("implement me")
 }
 
-func (v Dep) CreateGroup(ctx context.Context, request *shieldv1.CreateGroupRequest) (*shieldv1.GroupResponse, error) {
+func (v Dep) CreateGroup(ctx context.Context, request *shieldv1.CreateGroupRequest) (*shieldv1.CreateGroupResponse, error) {
 	panic("implement me")
 }
 
-func (v Dep) GetGroupByID(ctx context.Context, request *shieldv1.GetGroupRequest) (*shieldv1.GroupResponse, error) {
+func (v Dep) GetGroup(ctx context.Context, request *shieldv1.GetGroupRequest) (*shieldv1.GetGroupResponse, error) {
 	panic("implement me")
 }
 
-func (v Dep) GetGroupUsers(ctx context.Context, request *shieldv1.GetGroupRequest) (*shieldv1.GetGroupUsersResponse, error) {
+func (v Dep) ListGroupUsers(ctx context.Context, request *shieldv1.ListGroupUsersRequest) (*shieldv1.ListGroupUsersResponse, error) {
 	panic("implement me")
 }
 
-func (v Dep) UpdateGroupByID(ctx context.Context, request *shieldv1.UpdateGroupRequest) (*shieldv1.GroupResponse, error) {
+func (v Dep) UpdateGroup(ctx context.Context, request *shieldv1.UpdateGroupRequest) (*shieldv1.UpdateGroupResponse, error) {
 	panic("implement me")
 }
