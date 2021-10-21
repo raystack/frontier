33cfaf3433d427e18600f0b08c4026f4253f79bb
Kartik Verma <kartik.verma@gojek.com>
2021-10-21 15:44:07 +0530

    fix: use error status instead of panic for unimplemented methods

--- api/handler/v1/group.go
+++ api/handler/v1/group.go
@@ -3,25 +3,28 @@ package v1
 import (
 	"context"
 
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
 	shieldv1 "go.buf.build/odpf/gw/odpf/proton/odpf/shield/v1"
 )
 
 func (v Dep) ListGroups(ctx context.Context, request *shieldv1.ListGroupsRequest) (*shieldv1.ListGroupsResponse, error) {
-	panic("implement me")
+	return nil, status.Errorf(codes.Unimplemented, "method not implemented")
 }
 
 func (v Dep) CreateGroup(ctx context.Context, request *shieldv1.CreateGroupRequest) (*shieldv1.CreateGroupResponse, error) {
-	panic("implement me")
+	return nil, status.Errorf(codes.Unimplemented, "method not implemented")
 }
 
 func (v Dep) GetGroup(ctx context.Context, request *shieldv1.GetGroupRequest) (*shieldv1.GetGroupResponse, error) {
-	panic("implement me")
+	return nil, status.Errorf(codes.Unimplemented, "method not implemented")
 }
 
 func (v Dep) ListGroupUsers(ctx context.Context, request *shieldv1.ListGroupUsersRequest) (*shieldv1.ListGroupUsersResponse, error) {
-	panic("implement me")
+	return nil, status.Errorf(codes.Unimplemented, "method not implemented")
 }
 
 func (v Dep) UpdateGroup(ctx context.Context, request *shieldv1.UpdateGroupRequest) (*shieldv1.UpdateGroupResponse, error) {
-	panic("implement me")
+	return nil, status.Errorf(codes.Unimplemented, "method not implemented")
 }
--- api/handler/v1/org.go
+++ api/handler/v1/org.go
@@ -3,21 +3,24 @@ package v1
 import (
 	"context"
 
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
 	shieldv1 "go.buf.build/odpf/gw/odpf/proton/odpf/shield/v1"
 )
 
 func (v Dep) ListOrganizations(ctx context.Context, request *shieldv1.ListOrganizationsRequest) (*shieldv1.ListOrganizationsResponse, error) {
-	panic("implement me")
+	return nil, status.Errorf(codes.Unimplemented, "method not implemented")
 }
 
 func (v Dep) CreateOrganization(ctx context.Context, request *shieldv1.CreateOrganizationRequest) (*shieldv1.CreateOrganizationResponse, error) {
-	panic("implement me")
+	return nil, status.Errorf(codes.Unimplemented, "method not implemented")
 }
 
 func (v Dep) GetOrganization(ctx context.Context, request *shieldv1.GetOrganizationRequest) (*shieldv1.GetOrganizationResponse, error) {
-	panic("implement me")
+	return nil, status.Errorf(codes.Unimplemented, "method not implemented")
 }
 
 func (v Dep) UpdateOrganization(ctx context.Context, request *shieldv1.UpdateOrganizationRequest) (*shieldv1.UpdateOrganizationResponse, error) {
-	panic("implement me")
+	return nil, status.Errorf(codes.Unimplemented, "method not implemented")
 }
