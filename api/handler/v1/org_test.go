008af2e90d3e07871b7874e29dcb8b076d60a599
Kartik Verma <kartik.verma@gojek.com>
2021-10-21 09:41:19 +0530

    add: tests for create org

--- api/handler/v1/org_test.go
+++ api/handler/v1/org_test.go
@@ -4,57 +4,62 @@ import (
 	"context"
 	"errors"
 	"testing"
+	"time"
 
 	"github.com/odpf/shield/org"
 	"github.com/stretchr/testify/assert"
 
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/structpb"
+	"google.golang.org/protobuf/types/known/timestamppb"
 
 	shieldv1 "go.buf.build/odpf/gw/odpf/proton/odpf/shield/v1"
 )
 
-//var testOrgMap = map[string]org.Organization{
-//	"9f256f86-31a3-11ec-8d3d-0242ac130003": {
-//		Id:   "9f256f86-31a3-11ec-8d3d-0242ac130003",
-//		Name: "Org 1",
-//		Slug: "org-1",
-//		Metadata: map[string]interface{}{
-//			"email": "org1@org1.com",
-//			"count": 10,
-//		},
-//		CreatedAt: time.Time{},
-//		UpdatedAt: time.Time{},
-//	}, "a755e76c-31a3-11ec-8d3d-0242ac130003": {
-//		Id:   "a755e76c-31a3-11ec-8d3d-0242ac130003",
-//		Name: "Org 2",
-//		Slug: "org-2",
-//		Metadata: map[string]interface{}{
-//			"email":       "org2@org2.com",
-//			"admin_count": 2,
-//		},
-//		CreatedAt: time.Time{},
-//		UpdatedAt: time.Time{},
-//	}, "b07131f8-31a3-11ec-8d3d-0242ac130003": {
-//		Id:   "b07131f8-31a3-11ec-8d3d-0242ac130003",
-//		Name: "Org 3",
-//		Slug: "org-3",
-//		Metadata: map[string]interface{}{
-//			"email": "org3@org3.com",
-//		},
-//		CreatedAt: time.Time{},
-//		UpdatedAt: time.Time{},
-//	}, "bb97e9e6-31a3-11ec-8d3d-0242ac130003": {
-//		Id:   "bb97e9e6-31a3-11ec-8d3d-0242ac130003",
-//		Name: "Org 4",
-//		Slug: "org-4",
-//		Metadata: map[string]interface{}{
-//			"Project": "Project 1",
-//		},
-//		CreatedAt: time.Time{},
-//		UpdatedAt: time.Time{},
-//	},
-//}
+var testOrgMap = map[string]org.Organization{
+	"9f256f86-31a3-11ec-8d3d-0242ac130003": {
+		Id:   "9f256f86-31a3-11ec-8d3d-0242ac130003",
+		Name: "Org 1",
+		Slug: "org-1",
+		Metadata: map[string]interface{}{
+			"email": "org1@org1.com",
+			"count": 10,
+		},
+		CreatedAt: time.Time{},
+		UpdatedAt: time.Time{},
+	},
+	//}, "a755e76c-31a3-11ec-8d3d-0242ac130003": {
+	//	Id:   "a755e76c-31a3-11ec-8d3d-0242ac130003",
+	//	Name: "Org 2",
+	//	Slug: "org-2",
+	//	Metadata: map[string]interface{}{
+	//		"email":       "org2@org2.com",
+	//		"admin_count": 2,
+	//	},
+	//	CreatedAt: time.Time{},
+	//	UpdatedAt: time.Time{},
+	//}, "b07131f8-31a3-11ec-8d3d-0242ac130003": {
+	//	Id:   "b07131f8-31a3-11ec-8d3d-0242ac130003",
+	//	Name: "Org 3",
+	//	Slug: "org-3",
+	//	Metadata: map[string]interface{}{
+	//		"email": "org3@org3.com",
+	//	},
+	//	CreatedAt: time.Time{},
+	//	UpdatedAt: time.Time{},
+	//}, "bb97e9e6-31a3-11ec-8d3d-0242ac130003": {
+	//	Id:   "bb97e9e6-31a3-11ec-8d3d-0242ac130003",
+	//	Name: "Org 4",
+	//	Slug: "org-4",
+	//	Metadata: map[string]interface{}{
+	//		"Project": "Project 1",
+	//	},
+	//	CreatedAt: time.Time{},
+	//	UpdatedAt: time.Time{},
+	//},
+
+}
 
 func TestListOrganizations(t *testing.T) {
 	t.Parallel()
@@ -66,12 +71,37 @@ func TestListOrganizations(t *testing.T) {
 		err        error
 	}{
 		{
-			title: "error in fetching org list",
+			title: "error in Org Service",
 			mockOrgSrv: mockOrgSrv{ListOrganizationsFunc: func(ctx context.Context) (organizations []org.Organization, err error) {
 				return []org.Organization{}, errors.New("some error")
 			}},
 			want: nil,
 			err:  status.Errorf(codes.Internal, internalServerError.Error()),
+		}, {
+			title: "success",
+			mockOrgSrv: mockOrgSrv{ListOrganizationsFunc: func(ctx context.Context) (organizations []org.Organization, err error) {
+				var testOrgList []org.Organization
+				for _, o := range testOrgMap {
+					testOrgList = append(testOrgList, o)
+				}
+				return testOrgList, nil
+			}},
+			want: &shieldv1.ListOrganizationsResponse{Organizations: []*shieldv1.Organization{
+				{
+					Id:   "9f256f86-31a3-11ec-8d3d-0242ac130003",
+					Name: "Org 1",
+					Slug: "org-1",
+					Metadata: &structpb.Struct{
+						Fields: map[string]*structpb.Value{
+							"email": structpb.NewStringValue("org1@org1.com"),
+							"count": structpb.NewNumberValue(10),
+						},
+					},
+					CreatedAt: timestamppb.New(time.Time{}),
+					UpdatedAt: timestamppb.New(time.Time{}),
+				},
+			}},
+			err: nil,
 		},
 	}
 
@@ -81,7 +111,7 @@ func TestListOrganizations(t *testing.T) {
 
 			mockDep := Dep{OrgService: tt.mockOrgSrv}
 			resp, err := mockDep.ListOrganizations(context.Background(), nil)
-			assert.Equal(t, resp, tt.want)
+			assert.EqualValues(t, resp, tt.want)
 			assert.EqualValues(t, err, tt.err)
 		})
 	}
@@ -93,16 +123,47 @@ func TestCreateOrganization(t *testing.T) {
 	table := []struct {
 		title      string
 		mockOrgSrv mockOrgSrv
-		want       *shieldv1.ListOrganizationsResponse
+		req        *shieldv1.CreateOrganizationRequest
+		want       *shieldv1.CreateOrganizationResponse
 		err        error
 	}{
 		{
 			title: "error in fetching org list",
-			mockOrgSrv: mockOrgSrv{ListOrganizationsFunc: func(ctx context.Context) (organizations []org.Organization, err error) {
-				return []org.Organization{}, errors.New("some error")
+			mockOrgSrv: mockOrgSrv{CreateOrganizationFunc: func(ctx context.Context, o org.Organization) (org.Organization, error) {
+				return org.Organization{}, errors.New("some error")
+			}},
+			req: &shieldv1.CreateOrganizationRequest{Body: &shieldv1.OrganizationRequestBody{
+				Name:     "some org",
+				Slug:     "abc",
+				Metadata: &structpb.Struct{},
 			}},
 			want: nil,
-			err:  status.Errorf(codes.Internal, internalServerError.Error()),
+			err:  grpcInternalServerError,
+		},
+		{
+			title: "success",
+			mockOrgSrv: mockOrgSrv{CreateOrganizationFunc: func(ctx context.Context, o org.Organization) (org.Organization, error) {
+				return org.Organization{
+					Id:       "new-abc",
+					Name:     "some org",
+					Slug:     "abc",
+					Metadata: nil,
+				}, nil
+			}},
+			req: &shieldv1.CreateOrganizationRequest{Body: &shieldv1.OrganizationRequestBody{
+				Name:     "some org",
+				Slug:     "abc",
+				Metadata: &structpb.Struct{},
+			}},
+			want: &shieldv1.CreateOrganizationResponse{Organization: &shieldv1.Organization{
+				Id:        "new-abc",
+				Name:      "some org",
+				Slug:      "abc",
+				Metadata:  &structpb.Struct{Fields: map[string]*structpb.Value{}},
+				CreatedAt: timestamppb.New(time.Time{}),
+				UpdatedAt: timestamppb.New(time.Time{}),
+			}},
+			err: nil,
 		},
 	}
 
@@ -111,9 +172,9 @@ func TestCreateOrganization(t *testing.T) {
 			t.Parallel()
 
 			mockDep := Dep{OrgService: tt.mockOrgSrv}
-			resp, err := mockDep.ListOrganizations(context.Background(), nil)
-			assert.Equal(t, resp, tt.want)
-			assert.EqualValues(t, err, tt.err)
+			resp, err := mockDep.CreateOrganization(context.Background(), tt.req)
+			assert.EqualValues(t, tt.want, resp)
+			assert.EqualValues(t, tt.err, err)
 		})
 	}
 }
