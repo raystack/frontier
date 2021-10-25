4deba3dfcb57af0d0dbf59e1bfb889e17633fed1
Kartik Verma <kartik.verma@gojek.com>
2021-10-25 11:09:41 +0530

    feat: add UNIQUE constraint to org  name and slug

--- embeds/migrations/20211020192111_create_organization_table.up.sql
+++ embeds/migrations/20211020192111_create_organization_table.up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS organizations
 (
     id         uuid        PRIMARY KEY     DEFAULT uuid_generate_v4(),
-    name       varchar     NOT NULL,
-    slug       varchar     NOT NULL,
+    name       varchar     UNIQUE NOT NULL,
+    slug       varchar     UNIQUE NOT NULL,
     metadata   jsonb,
     created_at timestamptz NOT NULL DEFAULT NOW(),
     updated_at timestamptz NOT NULL DEFAULT NOW(),
