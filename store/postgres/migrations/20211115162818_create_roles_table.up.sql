df942ae4177d120c7d9af794909408530c8615a4
Kartik Verma <kartik.verma@gojek.com>
2021-11-15 16:38:28 +0530

    sql: add schema migration for  table

---
+++ store/postgres/migrations/20211115162818_create_roles_table.up.sql
@@ -0,0 +1,19 @@
+-- | Column name      | type     | Example                  |
+-- |------------------|----------|--------------------------|
+-- | id <PK>          | string   | org_manager              |
+-- | name             | string   | Org Manager              |
+-- | types            | []string | ["user" + "group"]       |
+-- | definition_level | string   | organization             |
+-- | metadata         | json     | {"resource": "firehose"} |
+
+CREATE TABLE IF NOT EXISTS roles
+(
+    id                      varchar         PRIMARY KEY,
+    name                    varchar         UNIQUE NOT NULL,
+    types                   varchar[]       NOT NULL ,
+    definition_level        varchar         NOT NULL,
+    metadata                jsonb,
+    created_at              timestamptz     NOT NULL            DEFAULT NOW(),
+    updated_at              timestamptz     NOT NULL            DEFAULT NOW(),
+    deleted_at              timestamptz
+);
