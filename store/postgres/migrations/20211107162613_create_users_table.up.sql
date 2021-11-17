1efea1626fa9fcd750d39469d98f02587bc44b92
Kartik Verma <kartik.verma@gojek.com>
2021-11-17 17:05:02 +0530

    fix: user schema sql format

--- store/postgres/migrations/20211107162613_create_users_table.up.sql
+++ store/postgres/migrations/20211107162613_create_users_table.up.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS users (
-  id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-  name varchar UNIQUE NOT NULL,
-  email varchar UNIQUE NOT NULL,
-  metadata jsonb,
-  created_at timestamptz NOT NULL DEFAULT NOW(),
-  updated_at timestamptz NOT NULL DEFAULT NOW(),
-  deleted_at timestamptz
+    id          uuid            PRIMARY KEY         DEFAULT uuid_generate_v4(),
+    name        varchar         UNIQUE  NOT NULL,
+    email       varchar         UNIQUE  NOT NULL,
+    metadata    jsonb,
+    created_at  timestamptz             NOT NULL    DEFAULT NOW(),
+    updated_at  timestamptz             NOT NULL    DEFAULT NOW(),
+    deleted_at  timestamptz
 );
