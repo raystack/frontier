95e22f425f33bf59c1f8e3519431a10a0fa00cf9
Rishabh Mishra <me@rsbh.dev>
2021-11-22 00:06:30 +0530

    chore: add get policy api

--- api/handler/handler.go
+++ api/handler/handler.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"github.com/odpf/shield/model"
 	"net/http"
+	"strings"
 
 	"github.com/odpf/salt/server"
 	v1 "github.com/odpf/shield/api/handler/v1"
@@ -41,6 +42,22 @@ func Register(ctx context.Context, s *server.MuxServer, gw *server.GRPCGateway, 
 				return
 			}
 			policy, err := deps.V1.PolicyService.CreatePolicy(context.Background(), payload)
+			if err != nil {
+				return
+			}
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusCreated)
+			json.NewEncoder(w).Encode(policy)
+		default:
+			fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
+		}
+	}))
+
+	s.RegisterHandler("/policies/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "GET":
+			params := strings.Split(r.URL.Path, "/")
+			policy, err := deps.V1.PolicyService.GetPolicy(context.Background(), params[2])
 			if err != nil {
 				return
 			}
