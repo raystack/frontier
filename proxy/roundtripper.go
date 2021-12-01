9a387de900c885bda09ac8b84ec2b6ae90a22781
Kartik Verma <kartik.verma@gojek.com>
2021-12-01 10:07:36 +0530

    fix: revert roundtripper

--- proxy/roundtripper.go
+++ proxy/roundtripper.go
@@ -1,17 +1,18 @@
 package proxy
 
 import (
+	"crypto/tls"
+	"net"
 	"net/http"
 
-	"github.com/odpf/shield/hook"
-
 	"github.com/odpf/salt/log"
+
+	"golang.org/x/net/http2"
 )
 
 type h2cTransportWrapper struct {
-	transport *http.Transport
+	transport *http2.Transport
 	log       log.Logger
-	hook      hook.Service
 }
 
 func (t *h2cTransportWrapper) RoundTrip(req *http.Request) (*http.Response, error) {
@@ -21,21 +22,18 @@ func (t *h2cTransportWrapper) RoundTrip(req *http.Request) (*http.Response, erro
 	}
 	t.log.Debug("proxy request", "host", req.URL.Host, "path", req.URL.Path,
 		"scheme", req.URL.Scheme, "protocol", req.Proto)
-
-	res, err := t.transport.RoundTrip(req)
-	if err != nil {
-		return res, err
-	}
-
-	return t.hook.ServeHook(res, nil)
+	return t.transport.RoundTrip(req)
 }
 
-func NewH2cRoundTripper(log log.Logger, hook hook.Service) http.RoundTripper {
-	transport := &http.Transport{
+func NewH2cRoundTripper(log log.Logger) http.RoundTripper {
+	transport := &http2.Transport{
+		DialTLS: func(network, addr string, cfg *tls.Config) (net.Conn, error) {
+			return net.Dial(network, addr)
+		},
+		AllowHTTP: true,
 	}
 	return &h2cTransportWrapper{
 		transport: transport,
 		log:       log,
-		hook:      hook,
 	}
 }
