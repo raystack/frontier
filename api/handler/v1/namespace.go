84684faa2b217f6aca6a24baadbe83e16eac7b56
Rishabh Mishra <me@rsbh.dev>
2021-12-03 14:48:37 +0530

    feat: add namespace apis

---
+++ api/handler/v1/namespace.go
@@ -0,0 +1,125 @@
+package v1
+
+import (
+	"context"
+	"errors"
+
+	grpczap "github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
+	"github.com/odpf/shield/internal/schema"
+	"github.com/odpf/shield/model"
+	shieldv1 "go.buf.build/odpf/gwv/odpf/proton/odpf/shield/v1"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+type NamespaceService interface {
+	GetNamespace(ctx context.Context, id string) (model.Namespace, error)
+	ListNamespaces(ctx context.Context) ([]model.Namespace, error)
+	CreateNamespace(ctx context.Context, ns model.Namespace) (model.Namespace, error)
+	UpdateNamespace(ctx context.Context, id string, ns model.Namespace) (model.Namespace, error)
+}
+
+var grpcNamespaceNotFoundErr = status.Errorf(codes.NotFound, "namespace doesn't exist")
+
+func (v Dep) ListNamespaces(ctx context.Context, request *shieldv1.ListNamespacesRequest) (*shieldv1.ListNamespacesResponse, error) {
+	logger := grpczap.Extract(ctx)
+	var namespaces []*shieldv1.Namespace
+
+	nsList, err := v.NamespaceService.ListNamespaces(ctx)
+	if err != nil {
+		logger.Error(err.Error())
+		return nil, grpcInternalServerError
+	}
+
+	for _, ns := range nsList {
+		nsPB, err := transformNamespaceToPB(ns)
+		if err != nil {
+			logger.Error(err.Error())
+			return nil, grpcInternalServerError
+		}
+
+		namespaces = append(namespaces, &nsPB)
+	}
+
+	return &shieldv1.ListNamespacesResponse{Namespaces: namespaces}, nil
+}
+
+func (v Dep) CreateNamespace(ctx context.Context, request *shieldv1.CreateNamespaceRequest) (*shieldv1.CreateNamespaceResponse, error) {
+	logger := grpczap.Extract(ctx)
+
+	newNS, err := v.NamespaceService.CreateNamespace(ctx, model.Namespace{
+		Id:   request.GetBody().Id,
+		Name: request.GetBody().Name,
+	})
+
+	if err != nil {
+		logger.Error(err.Error())
+		return nil, grpcInternalServerError
+	}
+
+	nsPB, err := transformNamespaceToPB(newNS)
+
+	if err != nil {
+		logger.Error(err.Error())
+		return nil, grpcInternalServerError
+	}
+
+	return &shieldv1.CreateNamespaceResponse{Namespace: &nsPB}, nil
+}
+
+func (v Dep) GetNamespace(ctx context.Context, request *shieldv1.GetNamespaceRequest) (*shieldv1.GetNamespaceResponse, error) {
+	logger := grpczap.Extract(ctx)
+
+	fetchedNS, err := v.NamespaceService.GetNamespace(ctx, request.GetId())
+	if err != nil {
+		logger.Error(err.Error())
+		switch {
+		case errors.Is(err, schema.NamespaceDoesntExist):
+			return nil, grpcNamespaceNotFoundErr
+		case errors.Is(err, schema.InvalidUUID):
+			return nil, grpcBadBodyError
+		default:
+			return nil, grpcInternalServerError
+		}
+	}
+
+	nsPB, err := transformNamespaceToPB(fetchedNS)
+	if err != nil {
+		logger.Error(err.Error())
+		return nil, grpcInternalServerError
+	}
+
+	return &shieldv1.GetNamespaceResponse{Namespace: &nsPB}, nil
+}
+
+func (v Dep) UpdateNamespace(ctx context.Context, request *shieldv1.UpdateNamespaceRequest) (*shieldv1.UpdateNamespaceResponse, error) {
+	logger := grpczap.Extract(ctx)
+
+	updatedNS, err := v.NamespaceService.UpdateNamespace(ctx, request.GetId(), model.Namespace{
+		Id:   request.GetId(),
+		Name: request.GetBody().Name,
+	})
+
+	if err != nil {
+		logger.Error(err.Error())
+		return nil, grpcInternalServerError
+	}
+
+	nsPB, err := transformNamespaceToPB(updatedNS)
+	if err != nil {
+		logger.Error(err.Error())
+		return nil, grpcInternalServerError
+	}
+
+	return &shieldv1.UpdateNamespaceResponse{Namespace: &nsPB}, nil
+}
+
+func transformNamespaceToPB(ns model.Namespace) (shieldv1.Namespace, error) {
+	return shieldv1.Namespace{
+		Id:        ns.Id,
+		Name:      ns.Name,
+		CreatedAt: timestamppb.New(ns.CreatedAt),
+		UpdatedAt: timestamppb.New(ns.UpdatedAt),
+	}, nil
+}
---
+++ api/handler/v1/namespace_test.go
@@ -0,0 +1,179 @@
+package v1
+
+import (
+	"context"
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/odpf/shield/model"
+	shieldv1 "go.buf.build/odpf/gwv/odpf/proton/odpf/shield/v1"
+
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+var testNsMap = map[string]model.Namespace{
+	"team": {
+		Id:        "team",
+		Name:      "Team",
+		CreatedAt: time.Time{},
+		UpdatedAt: time.Time{},
+	},
+	"org": {
+		Id:        "org",
+		Name:      "Org",
+		CreatedAt: time.Time{},
+		UpdatedAt: time.Time{},
+	},
+	"project": {
+		Id:        "project",
+		Name:      "Project",
+		CreatedAt: time.Time{},
+		UpdatedAt: time.Time{},
+	},
+}
+
+func TestListNamespaces(t *testing.T) {
+	t.Parallel()
+	table := []struct {
+		title            string
+		mockNamespaceSrv mockNamespaceSrv
+		req              *shieldv1.ListNamespacesRequest
+		want             *shieldv1.ListNamespacesResponse
+		err              error
+	}{
+		{
+			title: "error in Namespace Service",
+			mockNamespaceSrv: mockNamespaceSrv{ListNamespacesFunc: func(ctx context.Context) (namespaces []model.Namespace, err error) {
+				return []model.Namespace{}, errors.New("some error")
+			}},
+			want: nil,
+			err:  status.Errorf(codes.Internal, internalServerError.Error()),
+		},
+		{
+			title: "success",
+			mockNamespaceSrv: mockNamespaceSrv{ListNamespacesFunc: func(ctx context.Context) (namespaces []model.Namespace, err error) {
+				var testNSList []model.Namespace
+				for _, ns := range testNsMap {
+					testNSList = append(testNSList, ns)
+				}
+				return testNSList, nil
+			}},
+			want: &shieldv1.ListNamespacesResponse{Namespaces: []*shieldv1.Namespace{
+				{
+					Id:        "team",
+					Name:      "Team",
+					CreatedAt: timestamppb.New(time.Time{}),
+					UpdatedAt: timestamppb.New(time.Time{}),
+				},
+				{
+					Id:        "org",
+					Name:      "Org",
+					CreatedAt: timestamppb.New(time.Time{}),
+					UpdatedAt: timestamppb.New(time.Time{}),
+				},
+				{
+					Id:        "project",
+					Name:      "Project",
+					CreatedAt: timestamppb.New(time.Time{}),
+					UpdatedAt: timestamppb.New(time.Time{}),
+				},
+			}},
+			err: nil,
+		},
+	}
+
+	for _, tt := range table {
+		t.Run(tt.title, func(t *testing.T) {
+			t.Parallel()
+
+			mockDep := Dep{NamespaceService: tt.mockNamespaceSrv}
+			resp, err := mockDep.ListNamespaces(context.Background(), tt.req)
+
+			assert.EqualValues(t, tt.want, resp)
+			assert.EqualValues(t, tt.err, err)
+		})
+	}
+}
+
+func TestCreateNamespace(t *testing.T) {
+	t.Parallel()
+
+	table := []struct {
+		title            string
+		mockNamespaceSrv mockNamespaceSrv
+		req              *shieldv1.CreateNamespaceRequest
+		want             *shieldv1.CreateNamespaceResponse
+		err              error
+	}{
+		{
+			title: "error in creating namespace",
+			mockNamespaceSrv: mockNamespaceSrv{CreateNamespaceFunc: func(ctx context.Context, ns model.Namespace) (model.Namespace, error) {
+				return model.Namespace{}, errors.New("some error")
+			}},
+			req: &shieldv1.CreateNamespaceRequest{Body: &shieldv1.NamespaceRequestBody{
+				Id:   "team",
+				Name: "Team",
+			}},
+			want: nil,
+			err:  grpcInternalServerError,
+		},
+		{
+			title: "success",
+			mockNamespaceSrv: mockNamespaceSrv{CreateNamespaceFunc: func(ctx context.Context, ns model.Namespace) (model.Namespace, error) {
+				return model.Namespace{
+					Id:   "team",
+					Name: "Team",
+				}, nil
+			}},
+			req: &shieldv1.CreateNamespaceRequest{Body: &shieldv1.NamespaceRequestBody{
+				Id:   "team",
+				Name: "Team",
+			}},
+			want: &shieldv1.CreateNamespaceResponse{Namespace: &shieldv1.Namespace{
+				Id:        "team",
+				Name:      "Team",
+				CreatedAt: timestamppb.New(time.Time{}),
+				UpdatedAt: timestamppb.New(time.Time{}),
+			}},
+			err: nil,
+		},
+	}
+
+	for _, tt := range table {
+		t.Run(tt.title, func(t *testing.T) {
+			t.Parallel()
+
+			mockDep := Dep{NamespaceService: tt.mockNamespaceSrv}
+			resp, err := mockDep.CreateNamespace(context.Background(), tt.req)
+			assert.EqualValues(t, tt.want, resp)
+			assert.EqualValues(t, tt.err, err)
+		})
+	}
+}
+
+type mockNamespaceSrv struct {
+	GetNamespaceFunc    func(ctx context.Context, id string) (model.Namespace, error)
+	CreateNamespaceFunc func(ctx context.Context, ns model.Namespace) (model.Namespace, error)
+	ListNamespacesFunc  func(ctx context.Context) ([]model.Namespace, error)
+	UpdateNamespaceFunc func(ctx context.Context, id string, ns model.Namespace) (model.Namespace, error)
+}
+
+func (m mockNamespaceSrv) GetNamespace(ctx context.Context, id string) (model.Namespace, error) {
+	return m.GetNamespaceFunc(ctx, id)
+}
+
+func (m mockNamespaceSrv) ListNamespaces(ctx context.Context) ([]model.Namespace, error) {
+	return m.ListNamespacesFunc(ctx)
+}
+
+func (m mockNamespaceSrv) CreateNamespace(ctx context.Context, ns model.Namespace) (model.Namespace, error) {
+	return m.CreateNamespace(ctx, ns)
+}
+
+func (m mockNamespaceSrv) UpdateNamespace(ctx context.Context, id string, ns model.Namespace) (model.Namespace, error) {
+	return m.UpdateNamespaceFunc(ctx, id, ns)
+}
