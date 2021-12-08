4640a267be1cf3c00945c2075c71ec757999aed0
sushmith <sushmith.bhatta@gojek.com>
2021-12-08 15:41:45 +0530

    chore: change zap log formatting for stdout

--- logger/logger.go
+++ logger/logger.go
@@ -1,6 +1,8 @@
 package logger
 
 import (
+	"os"
+
 	"github.com/odpf/salt/log"
 	"github.com/odpf/shield/config"
 	"go.uber.org/zap"
@@ -12,8 +14,12 @@ func InitLogger(appConfig *config.Shield) *log.Zap {
 	cfg.Level = zap.NewAtomicLevelAt(atomicLevel(appConfig.Log.Level))
 	cfg.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
 	cfg.DisableCaller = true
+	consoleEncoder := zapcore.NewConsoleEncoder(cfg.EncoderConfig)
 
-	opt := log.ZapWithConfig(cfg, zap.AddCallerSkip(1)) // <---- @TODO to remove this
+	opt := log.ZapWithConfig(cfg, zap.WrapCore(func(c zapcore.Core) zapcore.Core {
+		return zapcore.NewCore(consoleEncoder, zapcore.AddSync(os.Stdout), cfg.Level)
+	}))
+
 	logger := log.NewZap(opt)
 	return logger
 }
