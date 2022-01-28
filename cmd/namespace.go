fe3e135619a4dd80754cfaaf53dcd1742225c7f8
Ishan Arya <ishanarya0@gmail.com>
2022-01-28 12:27:13 +0530

    feat: namespace get & list cmd

--- cmd/namespace.go
+++ cmd/namespace.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"context"
 	"fmt"
+	"os"
 	"strconv"
 
 	"github.com/MakeNowJust/heredoc"
@@ -25,6 +26,8 @@ func NamespaceCommand(logger log.Logger, appConfig *config.Shield) *cli.Command 
 		Example: heredoc.Doc(`
 			$ shield namespace create
 			$ shield namespace update
+			$ shield namespace get
+			$ shield namespace list
 		`),
 		Annotations: map[string]string{
 			"group:core": "true",
@@ -33,6 +36,8 @@ func NamespaceCommand(logger log.Logger, appConfig *config.Shield) *cli.Command 
 
 	cmd.AddCommand(createNamespaceCommand(logger, appConfig))
 	cmd.AddCommand(updateNamespaceCommand(logger, appConfig))
+	cmd.AddCommand(getNamespaceCommand(logger, appConfig))
+	cmd.AddCommand(listNamespaceCommand(logger, appConfig))
 
 	return cmd
 }
@@ -143,6 +148,116 @@ func updateNamespaceCommand(logger log.Logger, appConfig *config.Shield) *cli.Co
 
 	cmd.Flags().StringVarP(&filePath, "file", "f", "", "Path to the provider config")
 	cmd.MarkFlagRequired("file")
+
+	return cmd
+}
+
+func getNamespaceCommand(logger log.Logger, appConfig *config.Shield) *cli.Command {
+
+	cmd := &cli.Command{
+		Use:   "get",
+		Short: "get all namespaces",
+		Args:  cli.ExactArgs(1),
+		Example: heredoc.Doc(`
+			$ shield namespace get <namespace-id>
+		`),
+		Annotations: map[string]string{
+			"group:core": "true",
+		},
+		RunE: func(cmd *cli.Command, args []string) error {
+			spinner := printer.Spin("")
+			defer spinner.Stop()
+
+			host := appConfig.App.Host + ":" + strconv.Itoa(appConfig.App.Port)
+			ctx := context.Background()
+			client, cancel, err := createClient(ctx, host)
+			if err != nil {
+				return err
+			}
+			defer cancel()
+
+			namespaceID := args[0]
+			res, err := client.GetNamespace(ctx, &shieldv1beta1.GetNamespaceRequest{
+				Id: namespaceID,
+			})
+			if err != nil {
+				return err
+			}
+
+			report := [][]string{}
+
+			namespace := res.GetNamespace()
+
+			spinner.Stop()
+
+			report = append(report, []string{"ID", "NAME", "CREATED AT", "UPDATED AT"})
+			report = append(report, []string{
+				namespace.GetId(),
+				namespace.GetName(),
+				namespace.GetCreatedAt().AsTime().String(),
+				namespace.GetUpdatedAt().AsTime().String(),
+			})
+			printer.Table(os.Stdout, report)
+
+			spinner.Stop()
+
+			return nil
+		},
+	}
+
+	return cmd
+}
+
+func listNamespaceCommand(logger log.Logger, appConfig *config.Shield) *cli.Command {
+
+	cmd := &cli.Command{
+		Use:   "list",
+		Short: "list all namespaces",
+		Args:  cli.NoArgs,
+		Example: heredoc.Doc(`
+			$ shield namespace list
+		`),
+		Annotations: map[string]string{
+			"group:core": "true",
+		},
+		RunE: func(cmd *cli.Command, args []string) error {
+			spinner := printer.Spin("")
+			defer spinner.Stop()
+
+			host := appConfig.App.Host + ":" + strconv.Itoa(appConfig.App.Port)
+			ctx := context.Background()
+			client, cancel, err := createClient(ctx, host)
+			if err != nil {
+				return err
+			}
+			defer cancel()
+
+			res, err := client.ListNamespaces(ctx, &shieldv1beta1.ListNamespacesRequest{})
+			if err != nil {
+				return err
+			}
+
+			report := [][]string{}
+			namespaces := res.GetNamespaces()
+
+			spinner.Stop()
+
+			fmt.Printf(" \nShowing %d namespaces\n \n", len(namespaces))
+
+			report = append(report, []string{"ID", "NAME", "CREATED AT", "UPDATED AT"})
+			for _, n := range namespaces {
+				report = append(report, []string{
+					n.GetId(),
+					n.GetName(),
+					n.GetCreatedAt().AsTime().String(),
+					n.GetUpdatedAt().AsTime().String(),
+				})
+			}
+			printer.Table(os.Stdout, report)
+
+			return nil
+		},
+	}
 
 	return cmd
 }
