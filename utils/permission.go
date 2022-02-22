f1f8d20c1269e3a9d42389eb4389642fe669f37b
Rishabh Mishra <me@rsbh.dev>
2022-02-22 15:14:35 +0530

    fix(permission): dont add prefix to system namespace

--- utils/permission.go
+++ utils/permission.go
@@ -3,12 +3,31 @@ package utils
 import (
 	"fmt"
 
+	"github.com/odpf/shield/internal/bootstrap/definition"
+
 	"github.com/odpf/shield/model"
 )
 
 const NON_RESOURCE_ID = "*"
 
+var systemNSIds = []string{definition.TeamNamespace.Id, definition.UserNamespace.Id, definition.OrgNamespace.Id, definition.ProjectNamespace.Id}
+
+func StrListHas(list []string, a string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
+
+/*
+ /project/uuid/
+*/
 func CreateResourceId(resource model.Resource) string {
+	if ok := StrListHas(systemNSIds, resource.NamespaceId); ok {
+		return resource.Name
+	}
 	if resource.Name == NON_RESOURCE_ID {
 		return fmt.Sprintf("p/%s/%s", resource.ProjectId, resource.NamespaceId)
 	}
@@ -18,7 +37,3 @@ func CreateResourceId(resource model.Resource) string {
 func CreateNamespaceID(backend, resourceType string) string {
 	return fmt.Sprintf("%s_%s", backend, resourceType)
 }
-
-/*
- /project/uuid/
-*/
