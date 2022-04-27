b427ec270b3f4ec56557e0a7e081bab57adb5e52
Kartik Verma <kartik.verma@gojek.com>
2022-04-27 11:46:16 +0530

    fix: should create role for team (#101)

--- internal/bootstrap/bootstrap_test.go
+++ internal/bootstrap/bootstrap_test.go
@@ -8,12 +8,23 @@ import (
 )
 
 func TestGetResourceRole(t *testing.T) {
-	t.Run("should create role for resources", func(t *testing.T) {
+	t.Run("should create role for team", func(t *testing.T) {
 		output := getResourceRole("admin", model.Namespace{Id: "team"})
 		expected := model.Role{
 			Id:        "team_admin",
 			Name:      "team_admin",
-			Namespace: model.Namespace{Id: "team"},
+			Namespace: model.Namespace{Id: "team", Name: "Team"},
+			Types:     []string{"user"},
+		}
+		assert.EqualValues(t, expected, output)
+	})
+
+	t.Run("should create role for resource", func(t *testing.T) {
+		output := getResourceRole("admin", model.Namespace{Id: "kafka"})
+		expected := model.Role{
+			Id:        "kafka_admin",
+			Name:      "kafka_admin",
+			Namespace: model.Namespace{Id: "kafka"},
 			Types:     []string{"user", "team#team_member"},
 		}
 		assert.EqualValues(t, expected, output)
