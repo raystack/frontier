b6d872ce78ac32f8ea8cfe63f9da6f048040a35a
Kartik Verma <kartik.verma@gojek.com>
2022-08-02 00:44:02 +0530

    fix tests

--- core/bootstrap/bootstrap_test.go
+++ core/bootstrap/bootstrap_test.go
@@ -23,10 +23,11 @@ func TestGetResourceRole(t *testing.T) {
 	t.Run("should create role for resource", func(t *testing.T) {
 		output := getResourceRole("admin", namespace.Namespace{ID: "kafka"})
 		expected := role.Role{
-			ID:        "kafka_admin",
-			Name:      "kafka_admin",
-			Namespace: namespace.Namespace{ID: "kafka"},
-			Types:     []string{"user", "team#team_member"},
+			ID:          "kafka_admin",
+			Name:        "kafka_admin",
+			Namespace:   namespace.Namespace{ID: "kafka"},
+			NamespaceID: "kafka",
+			Types:       []string{"user", "team#team_member"},
 		}
 		assert.EqualValues(t, expected, output)
 	})
@@ -34,10 +35,11 @@ func TestGetResourceRole(t *testing.T) {
 	t.Run("should assign role for other namespace for resources", func(t *testing.T) {
 		output := getResourceRole("organization.organization_admin", namespace.Namespace{ID: "team"})
 		expected := role.Role{
-			ID:        "organization_admin",
-			Name:      "organization_admin",
-			Namespace: namespace.Namespace{ID: "organization"},
-			Types:     []string{"user", "team#team_member"},
+			ID:          "organization_admin",
+			Name:        "organization_admin",
+			Namespace:   namespace.Namespace{ID: "organization"},
+			NamespaceID: "organization",
+			Types:       []string{"user", "team#team_member"},
 		}
 		assert.EqualValues(t, expected, output)
 	})
