6d32ca1d2eb706a7acb24b071848bbd74655c52b
Kartik Verma <kartik.verma@gojek.com>
2022-08-16 11:11:52 +0530

    feat: prevent proxy context cancelled (#144)
    * correct the port on docker-compose
    
    * wrap context to prevent cancel
    
    * add ctx to new file
    
    * catch client error before roundtrip

--- internal/proxy/roundtripper.go
+++ internal/proxy/roundtripper.go
@@ -32,7 +32,10 @@ func (t *h2cTransportWrapper) RoundTrip(req *http.Request) (*http.Response, erro
 	// we need to apply errors if it failed in Director
 	if err, ok := req.Context().Value(ctxRequestErrorKey).(error); ok {
 		return nil, err
+	} else if req.Context().Err() != nil {
+		return nil, req.Context().Err()
 	}
+
 	t.log.Debug("proxy request", "host", req.URL.Host, "path", req.URL.Path,
 		"scheme", req.URL.Scheme, "protocol", req.Proto)
 
@@ -41,6 +44,8 @@ func (t *h2cTransportWrapper) RoundTrip(req *http.Request) (*http.Response, erro
 	if req.Header.Get("Content-Type") == "application/grpc" {
 		transport = t.grpcTransport
 	}
+
+	req = req.WithContext(WithoutCancel(req.Context()))
 
 	res, err := transport.RoundTrip(req)
 	if err != nil {
