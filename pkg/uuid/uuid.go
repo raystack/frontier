619900fb8e62815a181fab53af13c1fc401a1ac9
Muhammad Abduh <mabdh@users.noreply.github.com>
2022-08-19 11:47:39 +0700

    docs: add more docs in pkg

--- pkg/uuid/uuid.go
+++ pkg/uuid/uuid.go
@@ -2,9 +2,12 @@ package uuid
 
 import "github.com/google/uuid"
 
-// type alias
+// NewString is type alias to `github.com/google/uuid`.NewString
 var NewString = uuid.NewString
 
+// IsValid returns true if passed string in uuid format
+// defined by `github.com/google/uuid`.Parse
+// else return false
 func IsValid(key string) bool {
 	_, err := uuid.Parse(key)
 	return err == nil
