9a665228fe10a58f9665d49b1c32ee6a34b24476
Ishan Arya <ishanarya0@gmail.com>
2022-11-07 19:24:38 +0530

    feat: relation creation in resource creation hook

--- internal/proxy/hook/authz/authz.go
+++ internal/proxy/hook/authz/authz.go
@@ -4,19 +4,27 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/mitchellh/mapstructure"
 
 	"github.com/odpf/salt/log"
 	"github.com/odpf/shield/core/namespace"
 	"github.com/odpf/shield/core/project"
+	"github.com/odpf/shield/core/relation"
 	"github.com/odpf/shield/core/resource"
+	"github.com/odpf/shield/core/user"
 	"github.com/odpf/shield/internal/proxy/hook"
-	"github.com/odpf/shield/internal/proxy/middleware/attributes"
+	"github.com/odpf/shield/internal/proxy/middleware"
+	"github.com/odpf/shield/pkg/body_extractor"
 )
 
 type ResourceService interface {
 	Create(ctx context.Context, resource resource.Resource) (resource.Resource, error)
+}
+
+type RelationService interface {
+	Create(ctx context.Context, relation relation.RelationV2) (relation.RelationV2, error)
 }
 
 type Authz struct {
@@ -28,25 +36,38 @@ type Authz struct {
 	// To skip all the next hooks and just respond back
 	escape hook.Service
 
+	identityProxyHeaderKey string
+
 	resourceService ResourceService
+
+	relationService RelationService
 }
 
 type ProjectService interface {
 	Get(ctx context.Context, id string) (project.Project, error)
 }
 
-func New(log log.Logger, next, escape hook.Service, resourceService ResourceService) Authz {
+func New(log log.Logger, next, escape hook.Service, resourceService ResourceService, relationService RelationService, identityProxyHeaderKey string) Authz {
 	return Authz{
-		log:             log,
-		next:            next,
-		escape:          escape,
-		resourceService: resourceService,
+		log:                    log,
+		next:                   next,
+		escape:                 escape,
+		resourceService:        resourceService,
+		relationService:        relationService,
+		identityProxyHeaderKey: identityProxyHeaderKey,
 	}
+}
+
+type Relation struct {
+	Role             string `yaml:"role" mapstructure:"role"`
+	SubjectPrincipal string `yaml:"subject_principal" mapstructure:"subject_principal"`
+	SubjectID        string `yaml:"subject_id" mapstructure:"subject_id"`
 }
 
 type Config struct {
 	Action     string                    `yaml:"action" mapstructure:"action"`
 	Attributes map[string]hook.Attribute `yaml:"attributes" mapstructure:"attributes"`
+	Relations  []Relation                `yaml:"relations" mapstructure:"relations"`
 }
 
 func (a Authz) Info() hook.Info {
@@ -61,18 +82,13 @@ func (a Authz) ServeHook(res *http.Response, err error) (*http.Response, error) 
 		return a.escape.ServeHook(res, err)
 	}
 
-	hookSpec, ok := hook.ExtractHook(res.Request, a.Info().Name)
+	ruleFromRequest, ok := hook.ExtractRule(res.Request)
 	if !ok {
 		return a.next.ServeHook(res, nil)
 	}
 
-	attrs, ok := attributes.GetAttributesFromContext(res.Request.Context())
+	hookSpec, ok := hook.ExtractHook(res.Request, a.Info().Name)
 	if !ok {
-		return a.escape.ServeHook(res, fmt.Errorf("unable to fetch permission attributes from context"))
-	}
-	ns := attrs["namespace"]
-
-	if ns == nil {
 		return a.next.ServeHook(res, nil)
 	}
 
@@ -81,11 +97,107 @@ func (a Authz) ServeHook(res *http.Response, err error) (*http.Response, error) 
 		return a.next.ServeHook(res, nil)
 	}
 
-	if ns == "" {
+	if ruleFromRequest.Backend.Namespace == "" {
 		return a.next.ServeHook(res, fmt.Errorf("namespace variable not defined in rules"))
 	}
 
-	resources, err := a.createResources(attrs)
+	attributes := map[string]interface{}{}
+	attributes["namespace"] = ruleFromRequest.Backend.Namespace
+
+	identityProxyHeaderValue := res.Request.Header.Get(a.identityProxyHeaderKey)
+	attributes["user"] = identityProxyHeaderValue
+	res.Request = res.Request.WithContext(user.SetContextWithEmail(res.Request.Context(), identityProxyHeaderValue))
+
+	for id, attr := range config.Attributes {
+		bdy, _ := middleware.ExtractRequestBody(res.Request)
+		bodySource := &res.Body
+		if attr.Source == string(hook.SourceRequest) {
+			bodySource = &bdy
+		}
+
+		headerSource := &res.Header
+		if attr.Source == string(hook.SourceRequest) {
+			headerSource = &res.Request.Header
+		}
+
+		switch attr.Type {
+		case hook.AttributeTypeGRPCPayload:
+			if !strings.HasPrefix(res.Header.Get("Content-Type"), "application/grpc") {
+				a.log.Error("middleware: not a grpc request", "attr", attr)
+				return a.escape.ServeHook(res, fmt.Errorf("invalid header for http request: %s", res.Header.Get("Content-Type")))
+			}
+
+			payloadField, err := body_extractor.GRPCPayloadHandler{}.Extract(bodySource, attr.Index)
+			if err != nil {
+				a.log.Error("middleware: failed to parse grpc payload", "err", err)
+				return a.escape.ServeHook(res, fmt.Errorf("unable to parse grpc payload"))
+			}
+			attributes[id] = payloadField
+
+			a.log.Info("middleware: extracted", "field", payloadField, "attr", attr)
+		case hook.AttributeTypeJSONPayload:
+			if attr.Key == "" {
+				a.log.Error("middleware: payload key field empty")
+				return a.escape.ServeHook(res, fmt.Errorf("payload key field empty"))
+			}
+
+			payloadField, err := body_extractor.JSONPayloadHandler{}.Extract(bodySource, attr.Key)
+			if err != nil {
+				a.log.Error("middleware: failed to parse json payload", "err", err)
+				return a.escape.ServeHook(res, fmt.Errorf("failed to parse json payload"))
+			}
+			attributes[id] = payloadField
+
+			a.log.Info("middleware: extracted", "field", payloadField, "attr", attr)
+		case hook.AttributeTypeHeader:
+			if attr.Key == "" {
+				a.log.Error("middleware: header key field empty", "err", err)
+				return a.escape.ServeHook(res, fmt.Errorf("failed to parse json payload"))
+			}
+			headerAttr := headerSource.Get(attr.Key)
+			if headerAttr == "" {
+				a.log.Error(fmt.Sprintf("middleware: header %s is empty", attr.Key))
+				return a.escape.ServeHook(res, fmt.Errorf("failed to parse json payload"))
+			}
+
+			attributes[id] = headerAttr
+			a.log.Info("middleware: extracted", "field", headerAttr, "attr", attr)
+
+		case hook.AttributeTypeQuery:
+			if attr.Key == "" {
+				a.log.Error("middleware: query key field empty")
+				return a.escape.ServeHook(res, fmt.Errorf("failed to parse json payload"))
+			}
+			queryAttr := res.Request.URL.Query().Get(attr.Key)
+			if queryAttr == "" {
+				a.log.Error(fmt.Sprintf("middleware: query %s is empty", attr.Key))
+				return a.escape.ServeHook(res, fmt.Errorf("failed to parse json payload"))
+			}
+
+			attributes[id] = queryAttr
+			a.log.Info("middleware: extracted", "field", queryAttr, "attr", attr)
+
+		case hook.AttributeTypeConstant:
+			if attr.Value == "" {
+				a.log.Error("middleware: constant value empty")
+				return a.escape.ServeHook(res, fmt.Errorf("failed to parse json payload"))
+			}
+
+			attributes[id] = attr.Value
+			a.log.Info("middleware: extracted", "constant_key", res, "attr", attributes[id])
+
+		default:
+			a.log.Error("middleware: unknown attribute type", "attr", attr)
+			return a.escape.ServeHook(res, fmt.Errorf("unknown attribute type: %v", attr))
+		}
+	}
+
+	paramMap, _ := middleware.ExtractPathParams(res.Request)
+	for key, value := range paramMap {
+		attributes[key] = value
+	}
+
+	resources, err := a.createResources(attributes)
 	if err != nil {
 		a.log.Error(err.Error())
 		return a.escape.ServeHook(res, fmt.Errorf(err.Error()))
@@ -97,6 +209,26 @@ func (a Authz) ServeHook(res *http.Response, err error) (*http.Response, error) 
 			return a.escape.ServeHook(res, fmt.Errorf(err.Error()))
 		}
 		a.log.Info(fmt.Sprintf("Resource %s created with ID %s", newResource.URN, newResource.Idxa))
+
+		for _, rel := range config.Relations {
+			newRelation, err := a.relationService.Create(res.Request.Context(), relation.RelationV2{
+				Object: relation.Object{
+					ID:          resource.Name,
+					NamespaceID: resource.NamespaceID,
+				},
+				Subject: relation.Subject{
+					RoleID:    rel.Role,
+					Namespace: rel.SubjectPrincipal,
+					ID:        rel.SubjectID,
+				},
+			})
+			if err != nil {
+				a.log.Error(err.Error())
+				return a.escape.ServeHook(res, fmt.Errorf(err.Error()))
+			}
+
+			a.log.Info(fmt.Sprintf("created relation: %s for %s %s", newRelation.Subject.RoleID, newRelation.Subject.ID, newRelation.Subject.Namespace))
+		}
 	}
 
 	return a.next.ServeHook(res, nil)
@@ -159,9 +291,7 @@ func getAttributesValues(attributes interface{}) ([]string, error) {
 	var values []string
 	switch attributes.(type) {
 	case []string:
-		for _, i := range attributes.([]string) {
-			values = append(values, i)
-		}
+		values = append(values, attributes.([]string)...)
 	case string:
 		values = append(values, attributes.(string))
 	case []interface{}:
