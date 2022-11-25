d831e7e8740d6b7a87574d2a6e00b4f94ca17d55
Kartik Verma <kartik.verma@gojek.com>
2022-11-25 14:14:54 +0530

    feat: authz middleware

--- internal/proxy/middleware/authz/authz.go
+++ internal/proxy/middleware/authz/authz.go
@@ -3,17 +3,17 @@ package authz
 import (
 	"context"
 	"fmt"
-	"net/http"
-
 	"github.com/mitchellh/mapstructure"
 	"github.com/odpf/salt/log"
+	"github.com/odpf/shield/pkg/body_extractor"
+	"net/http"
+	"strings"
 
 	"github.com/odpf/shield/core/action"
 	"github.com/odpf/shield/core/namespace"
 	"github.com/odpf/shield/core/resource"
 	"github.com/odpf/shield/core/user"
 	"github.com/odpf/shield/internal/proxy/middleware"
-	"github.com/odpf/shield/internal/proxy/middleware/attributes"
 )
 
 type ResourceService interface {
@@ -33,8 +33,15 @@ type Authz struct {
 }
 
 type Config struct {
-	Actions    []string                        `yaml:"actions" mapstructure:"actions"`
-	Attributes map[string]middleware.Attribute `yaml:"attributes" mapstructure:"attributes"` // auth field -> Attribute
+	Actions     []string                        `yaml:"actions" mapstructure:"actions"`
+	Permissions []Permission                    `yaml:"permissions" mapstructure:"permissions"`
+	Attributes  map[string]middleware.Attribute `yaml:"attributes" mapstructure:"attributes"`
+}
+
+type Permission struct {
+	Name      string
+	Namespace string
+	Attribute string
 }
 
 func New(
@@ -60,30 +67,30 @@ func (c Authz) Info() *middleware.MiddlewareInfo {
 }
 
 func (c *Authz) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	//usr, err := c.userService.FetchCurrentUser(req.Context())
-	//if err != nil {
-	//	c.log.Error("middleware: failed to get user details", "err", err.Error())
-	//	c.notAllowed(rw)
-	//	return
-	//}
+	usr, err := c.userService.FetchCurrentUser(req.Context())
+	if err != nil {
+		c.log.Error("middleware: failed to get user details", "err", err.Error())
+		c.notAllowed(rw)
+		return
+	}
 
-	//req.Header.Set(c.userIDHeaderKey, usr.ID)
+	req.Header.Set(c.userIDHeaderKey, usr.ID)
+
+	rule, ok := middleware.ExtractRule(req)
+	if !ok {
+		c.next.ServeHTTP(rw, req)
+		return
+	}
+
 	wareSpec, ok := middleware.ExtractMiddleware(req, c.Info().Name)
 	if !ok {
 		c.next.ServeHTTP(rw, req)
 		return
 	}
 
-	permissionAttributes, ok := attributes.GetAttributesFromContext(req.Context())
-	if !ok {
-		c.log.Error("unable to fetch permission attributes from context")
+	if rule.Backend.Namespace == "" {
+		c.log.Error("namespace is not defined for this rule")
 		c.notAllowed(rw)
-		return
-	}
-	ns := permissionAttributes["namespace"]
-
-	if ns == nil {
-		c.next.ServeHTTP(rw, req)
 		return
 	}
 
@@ -95,39 +102,133 @@ func (c *Authz) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	if ns == "" {
-		c.log.Error("namespace is not defined for this rule")
-		c.notAllowed(rw)
-		return
-	}
+	permissionAttributes := map[string]interface{}{}
 
-	resources, err := createResources(permissionAttributes)
-	if err != nil {
-		c.log.Error("error while creating resource obj", "err", err)
-		c.notAllowed(rw)
-		return
-	}
-	for _, resource := range resources {
-		isAuthorized := false
-		for _, actionID := range config.Actions {
-			isAuthorized, err = c.resourceService.CheckAuthz(req.Context(), resource, action.Action{ID: actionID})
-			if err != nil {
-				c.log.Error("error while creating resource obj", "err", err)
+	permissionAttributes["namespace"] = rule.Backend.Namespace
+
+	permissionAttributes["user"] = req.Header.Get(c.userIDHeaderKey)
+
+	for res, attr := range config.Attributes {
+		_ = res
+
+		switch attr.Type {
+		case middleware.AttributeTypeGRPCPayload:
+			// check if grpc request
+			if !strings.HasPrefix(req.Header.Get("Content-Type"), "application/grpc") {
+				c.log.Error("middleware: not a grpc request", "attr", attr)
 				c.notAllowed(rw)
 				return
 			}
 
-			if isAuthorized {
-				break
+			// TODO: we can optimise this by parsing all field at once
+			payloadField, err := body_extractor.GRPCPayloadHandler{}.Extract(&req.Body, attr.Index)
+			if err != nil {
+				c.log.Error("middleware: failed to parse grpc payload", "err", err)
+				return
 			}
-		}
 
-		c.log.Info("authz check successful", "user", permissionAttributes["user"], "resource", resource.Name, "result", isAuthorized)
-		if !isAuthorized {
-			c.log.Info("user not allowed to make request", "user", permissionAttributes["user"], "resource", resource.Name, "result", isAuthorized)
+			permissionAttributes[res] = payloadField
+			c.log.Info("middleware: extracted", "field", payloadField, "attr", attr)
+
+		case middleware.AttributeTypeJSONPayload:
+			if attr.Key == "" {
+				c.log.Error("middleware: payload key field empty")
+				c.notAllowed(rw)
+				return
+			}
+			payloadField, err := body_extractor.JSONPayloadHandler{}.Extract(&req.Body, attr.Key)
+			if err != nil {
+				c.log.Error("middleware: failed to parse grpc payload", "err", err)
+				c.notAllowed(rw)
+				return
+			}
+
+			permissionAttributes[res] = payloadField
+			c.log.Info("middleware: extracted", "field", payloadField, "attr", attr)
+
+		case middleware.AttributeTypeHeader:
+			if attr.Key == "" {
+				c.log.Error("middleware: header key field empty")
+				c.notAllowed(rw)
+				return
+			}
+			headerAttr := req.Header.Get(attr.Key)
+			if headerAttr == "" {
+				c.log.Error(fmt.Sprintf("middleware: header %s is empty", attr.Key))
+				c.notAllowed(rw)
+				return
+			}
+
+			permissionAttributes[res] = headerAttr
+			c.log.Info("middleware: extracted", "field", headerAttr, "attr", attr)
+
+		case middleware.AttributeTypeQuery:
+			if attr.Key == "" {
+				c.log.Error("middleware: query key field empty")
+				c.notAllowed(rw)
+				return
+			}
+			queryAttr := req.URL.Query().Get(attr.Key)
+			if queryAttr == "" {
+				c.log.Error(fmt.Sprintf("middleware: query %s is empty", attr.Key))
+				c.notAllowed(rw)
+				return
+			}
+
+			permissionAttributes[res] = queryAttr
+			c.log.Info("middleware: extracted", "field", queryAttr, "attr", attr)
+
+		case middleware.AttributeTypeConstant:
+			if attr.Value == "" {
+				c.log.Error("middleware: constant value empty")
+				c.notAllowed(rw)
+				return
+			}
+
+			permissionAttributes[res] = attr.Value
+			c.log.Info("middleware: extracted", "constant_key", res, "attr", permissionAttributes[res])
+
+		default:
+			c.log.Error("middleware: unknown attribute type", "attr", attr)
 			c.notAllowed(rw)
 			return
 		}
+	}
+
+	paramMap, mapExists := middleware.ExtractPathParams(req)
+	if !mapExists {
+		c.log.Error("middleware: path param map doesn't exist")
+		c.notAllowed(rw)
+		return
+	}
+
+	for key, value := range paramMap {
+		permissionAttributes[key] = value
+	}
+
+	isAuthorized := false
+	for _, permission := range config.Permissions {
+		isAuthorized, err = c.resourceService.CheckAuthz(req.Context(), resource.Resource{
+			Name:        permissionAttributes[permission.Attribute].(string),
+			NamespaceID: permission.Namespace,
+		}, action.Action{
+			ID: permission.Name,
+		})
+		if err != nil {
+			c.log.Error("error while creating resource obj", "err", err)
+			c.notAllowed(rw)
+			return
+		}
+		if isAuthorized {
+			break
+		}
+	}
+
+	c.log.Info("authz check successful", "user", permissionAttributes["user"], "resource", permissionAttributes["resource"], "result", isAuthorized)
+	if !isAuthorized {
+		c.log.Info("user not allowed to make request", "user", permissionAttributes["user"], "resource", permissionAttributes["resource"], "result", isAuthorized)
+		c.notAllowed(rw)
+		return
 	}
 
 	c.next.ServeHTTP(rw, req)
