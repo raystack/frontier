dfe2acedd85e702a3661bb2149282dd982a9fd46
Kartik Verma <vkartik97@gmail.com>
2023-04-04 14:37:25 +0530

    fix: ignore value with quotations for jsonb (#5)
    * ignore value with quotations for jsonb
    
    * ignore int as well
    
    * Update 20230223172233_alter_value_metadata_column.up.sql

--- internal/store/postgres/migrations/20230223172233_alter_value_metadata_column.up.sql
+++ internal/store/postgres/migrations/20230223172233_alter_value_metadata_column.up.sql
@@ -1,3 +1,3 @@
-UPDATE metadata SET value = CONCAT('"',value,'"') where value NOT LIKE '[%' AND value NOT LIKE '{%';
+UPDATE metadata SET value = CONCAT('"',value,'"') where value NOT LIKE '[%' AND value NOT LIKE '{%' AND value NOT like '"%"' AND value NOT LIKE '%[^0-9.]%';
 
 ALTER TABLE metadata ALTER COLUMN value TYPE jsonb USING (value::jsonb);
