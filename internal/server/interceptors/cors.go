c3c8252c25009c13616edcc461ab7d9a9b24a68b
Praveen Yadav <pyadav9678@gmail.com>
2023-05-05 11:43:39 +0530

    fix: add DELETE in allow methods

--- internal/server/interceptors/cors.go
+++ internal/server/interceptors/cors.go
@@ -9,7 +9,7 @@ import (
 func WithCors(h http.Handler, origin string) http.Handler {
 	return handlers.CORS(
 		handlers.AllowedOrigins([]string{origin}),
-		handlers.AllowedMethods([]string{"GET", "HEAD", "POST"}),
+		handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "DELETE"}),
 		handlers.AllowedHeaders([]string{"Authorization", "Content-Type", "X-Requested-With"}),
 		handlers.AllowCredentials(),
 	)(h)
