d74cb1c091ef9cf3435b8560d3b47a56f657eca2
Rishabh <me@rsbh.dev>
2023-08-31 16:57:37 +0530

    chore: move routes out of the component; add type safety for the routes

--- sdks/js/packages/core/react/components/organization/profile.tsx
+++ sdks/js/packages/core/react/components/organization/profile.tsx
@@ -47,11 +47,132 @@ const rootRoute = new RootRoute({
   }
 });
 
+const indexRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: '/',
+  component: GeneralSetting
+});
+
+const deleteOrgRoute = new Route({
+  getParentRoute: () => indexRoute,
+  path: '/delete',
+  component: DeleteOrganization
+});
+
+const securityRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: '/security',
+  component: WorkspaceSecurity
+});
+
+const membersRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: '/members',
+  component: WorkspaceMembers
+});
+
+const inviteMemberRoute = new Route({
+  getParentRoute: () => membersRoute,
+  path: '/modal',
+  component: InviteMember
+});
+
+const teamsRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: '/members',
+  component: WorkspaceTeams
+});
+
+const addTeamRoute = new Route({
+  getParentRoute: () => teamsRoute,
+  path: '/modal',
+  component: AddTeam
+});
+
+const domainsRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: '/domains',
+  component: Domain
+});
+
+const verifyDomainRoute = new Route({
+  getParentRoute: () => domainsRoute,
+  path: '/$domainId/verify',
+  component: VerifyDomain
+});
+
+const addDomainRoute = new Route({
+  getParentRoute: () => domainsRoute,
+  path: '/modal',
+  component: AddDomain
+});
+
+const teamRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: '/teams/$teamId',
+  component: TeamPage
+});
+
+const deleteTeamRoute = new Route({
+  getParentRoute: () => teamRoute,
+  path: '/delete',
+  component: DeleteTeam
+});
+
+const projectsRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: '/projects',
+  component: WorkspaceProjects
+});
+
+const addProjectRoute = new Route({
+  getParentRoute: () => projectsRoute,
+  path: '/modal',
+  component: AddProject
+});
+
+const projectPageRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: '/projects/$projectId',
+  component: ProjectPage
+});
+
+const deleteProjectRoute = new Route({
+  getParentRoute: () => projectPageRoute,
+  path: '/delete',
+  component: DeleteProject
+});
+
+const profileRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: '/profile',
+  component: UserSetting
+});
+
+const perferencesRoute = new Route({
+  getParentRoute: () => rootRoute,
+  path: '/perferences',
+  component: UserPreferences
+});
+
+const routeTree = rootRoute.addChildren([
+  indexRoute.addChildren([deleteOrgRoute]),
+  securityRoute,
+  membersRoute.addChildren([inviteMemberRoute]),
+  teamsRoute.addChildren([addTeamRoute]),
+  domainsRoute.addChildren([addDomainRoute, verifyDomainRoute]),
+  teamRoute.addChildren([deleteTeamRoute]),
+  projectsRoute.addChildren([addProjectRoute]),
+  projectPageRoute.addChildren([deleteProjectRoute]),
+  profileRoute,
+  perferencesRoute
+]);
+const router = new Router({ routeTree });
+
 export const OrganizationProfile = ({
   organizationId,
   defaultRoute = '/'
 }: OrganizationProfileProps) => {
-  const [organization, setOrganization] = useState();
   const { client, setActiveOrganization } = useFrontier();
 
   const fetchOrganization = useCallback(async () => {
@@ -59,7 +180,6 @@ export const OrganizationProfile = ({
       // @ts-ignore
       data: { organization }
     } = await client?.frontierServiceGetOrganization(organizationId);
-    setOrganization(organization);
     setActiveOrganization(organization);
   }, [client, organizationId, setActiveOrganization]);
 
@@ -67,127 +187,11 @@ export const OrganizationProfile = ({
     if (organizationId) fetchOrganization();
   }, [organizationId, client, fetchOrganization]);
 
-  const indexRoute = new Route({
-    getParentRoute: () => rootRoute,
-    path: '/',
-    component: () => <GeneralSetting organization={organization} />
-  });
-
-  const deleteOrgRoute = new Route({
-    getParentRoute: () => indexRoute,
-    path: '/delete',
-    component: () => <DeleteOrganization organization={organization} />
-  });
-
-  const securityRoute = new Route({
-    getParentRoute: () => rootRoute,
-    path: '/security',
-    component: () => <WorkspaceSecurity organization={organization} />
-  });
-
-  const membersRoute = new Route({
-    getParentRoute: () => rootRoute,
-    path: '/members',
-    component: () => <WorkspaceMembers organization={organization} />
-  });
-
-  const inviteMemberRoute = new Route({
-    getParentRoute: () => membersRoute,
-    path: '/modal',
-    component: () => <InviteMember organization={organization} />
-  });
-
-  const teamsRoute = new Route({
-    getParentRoute: () => rootRoute,
-    path: '/members',
-    component: () => <WorkspaceTeams organization={organization} />
-  });
-
-  const addTeamRoute = new Route({
-    getParentRoute: () => teamsRoute,
-    path: '/modal',
-    component: () => <AddTeam organization={organization} />
-  });
-
-  const domainsRoute = new Route({
-    getParentRoute: () => rootRoute,
-    path: '/domains',
-    component: () => <Domain organization={organization} />
-  });
-
-  const verifyDomainRoute = new Route({
-    getParentRoute: () => domainsRoute,
-    path: '/$domainId/verify',
-    component: () => <VerifyDomain organization={organization} />
-  });
-
-  const addDomainRoute = new Route({
-    getParentRoute: () => domainsRoute,
-    path: '/modal',
-    component: () => <AddDomain organization={organization} />
-  });
-
-  const teamRoute = new Route({
-    getParentRoute: () => rootRoute,
-    path: '/teams/$teamId',
-    component: () => <TeamPage organization={organization} />
-  });
-
-  const deleteTeamRoute = new Route({
-    getParentRoute: () => teamRoute,
-    path: '/delete',
-    component: () => <DeleteTeam organization={organization} />
-  });
-
-  const projectsRoute = new Route({
-    getParentRoute: () => rootRoute,
-    path: '/projects',
-    component: () => <WorkspaceProjects organization={organization} />
-  });
-
-  const addProjectRoute = new Route({
-    getParentRoute: () => projectsRoute,
-    path: '/modal',
-    component: () => <AddProject organization={organization} />
-  });
-
-  const projectPageRoute = new Route({
-    getParentRoute: () => rootRoute,
-    path: '/projects/$projectId',
-    component: () => <ProjectPage organization={organization} />
-  });
-
-  const deleteProjectRoute = new Route({
-    getParentRoute: () => projectPageRoute,
-    path: '/delete',
-    component: () => <DeleteProject organization={organization} />
-  });
-
-  const profileRoute = new Route({
-    getParentRoute: () => rootRoute,
-    path: '/profile',
-    component: () => UserSetting
-  });
-
-  const perferencesRoute = new Route({
-    getParentRoute: () => rootRoute,
-    path: '/perferences',
-    component: () => UserPreferences
-  });
-
-  const routeTree = rootRoute.addChildren([
-    indexRoute.addChildren([deleteOrgRoute]),
-    securityRoute,
-    membersRoute.addChildren([inviteMemberRoute]),
-    teamsRoute.addChildren([addTeamRoute]),
-    domainsRoute.addChildren([addDomainRoute, verifyDomainRoute]),
-    teamRoute.addChildren([deleteTeamRoute]),
-    projectsRoute.addChildren([addProjectRoute]),
-    projectPageRoute.addChildren([deleteProjectRoute]),
-    profileRoute,
-    perferencesRoute
-  ]);
-  const router = new Router({ routeTree });
-
   return <RouterProvider router={router} />;
 };
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
