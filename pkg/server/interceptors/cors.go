51ef925b77835dbd7664ae6f365639e59ce1f6c7
Kush Sharma <thekushsharma@gmail.com>
2023-08-31 10:33:17 +0530

    feat: allow configuring cors middleware
    - few session variables are also exposed as config
    
    Signed-off-by: Kush Sharma <thekushsharma@gmail.com>

--- pkg/server/interceptors/cors.go
+++ pkg/server/interceptors/cors.go
@@ -6,11 +6,19 @@ import (
 	"github.com/gorilla/handlers"
 )
 
-func WithCors(h http.Handler, origin []string) http.Handler {
+type CorsConfig struct {
+	AllowedOrigins []string `yaml:"allowed_origins" mapstructure:"allowed_origins" default:""`
+	AllowedMethods []string `yaml:"allowed_methods" mapstructure:"allowed_methods" default:"GET POST PUT DELETE PATCH"`
+	AllowedHeaders []string `yaml:"allowed_headers" mapstructure:"allowed_headers" default:"Authorization"`
+	ExposedHeaders []string `yaml:"exposed_headers" mapstructure:"exposed_headers" default:"Content-Type"`
+}
+
+func WithCors(h http.Handler, conf CorsConfig) http.Handler {
 	return handlers.CORS(
-		handlers.AllowedOrigins(origin),
-		handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "DELETE", "PUT"}),
-		handlers.AllowedHeaders([]string{"Authorization", "Content-Type", "X-Requested-With", "Accept", "Accept-Language", "Content-Language", "Origin"}),
+		handlers.AllowedOrigins(conf.AllowedOrigins),
+		handlers.AllowedMethods(conf.AllowedMethods),
+		handlers.AllowedHeaders(conf.AllowedHeaders),
+		handlers.ExposedHeaders(conf.ExposedHeaders),
 		handlers.AllowCredentials(),
 	)(h)
 }
