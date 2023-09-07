a30dcd8b2b8c0d7ed535dd0e3f24c631421f5c3c
Rishabh <me@rsbh.dev>
2023-09-07 16:05:25 +0530

    feat: show loader in general org page

--- sdks/js/packages/core/react/components/organization/general/general.profile.tsx
+++ sdks/js/packages/core/react/components/organization/general/general.profile.tsx
@@ -1,27 +1,36 @@
 'use client';
 
 import { Avatar, Flex, Text } from '@raystack/apsara';
+import Skeleton from 'react-loading-skeleton';
 
 // @ts-ignore
 import { V1Beta1Organization } from '~/src';
 import { getInitials } from '~/utils';
-import styles from './general.module.css';
+// import styles from './general.module.css';
 
 interface GeneralProfileProps {
   organization?: V1Beta1Organization;
+  isLoading?: boolean;
 }
-export const GeneralProfile = ({ organization }: GeneralProfileProps) => {
+export const GeneralProfile = ({
+  organization,
+  isLoading
+}: GeneralProfileProps) => {
   return (
     <Flex direction="column" gap="small">
-      <Avatar
-        alt="Organization profile"
-        shape="circle"
-        fallback={getInitials(organization?.name)}
-        imageProps={{ width: '80px', height: '80px' }}
-      />
-      <Text size={4} className={styles.profileDescription}>
+      {isLoading ? (
+        <Skeleton style={{ width: '80px', height: '80px' }} circle />
+      ) : (
+        <Avatar
+          alt="Organization profile"
+          shape="circle"
+          fallback={getInitials(organization?.name)}
+          imageProps={{ width: '80px', height: '80px' }}
+        />
+      )}
+      {/* <Text size={4} className={styles.profileDescription}>
         Pick a logo for your organisation. Max size: 5 Mb
-      </Text>
+      </Text> */}
     </Flex>
   );
 };
