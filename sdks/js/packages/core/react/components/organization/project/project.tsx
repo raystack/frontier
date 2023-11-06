7db5b49b31f9888521555fcf437d540fe212355c
Rishabh <me@rsbh.dev>
2023-11-06 11:04:35 +0530

    feat: show project groups in member list table

--- sdks/js/packages/core/react/components/organization/project/project.tsx
+++ sdks/js/packages/core/react/components/organization/project/project.tsx
@@ -7,11 +7,11 @@ import {
   useParams,
   useRouterState
 } from '@tanstack/react-router';
-import { useEffect, useMemo, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { toast } from 'sonner';
 import backIcon from '~/react/assets/chevron-left.svg';
 import { useFrontier } from '~/react/contexts/FrontierContext';
-import { V1Beta1Project, V1Beta1User } from '~/src';
+import { V1Beta1Group, V1Beta1Project, V1Beta1User } from '~/src';
 import { Role } from '~/src/types';
 import { styles } from '../styles';
 import { General } from './general';
@@ -19,10 +19,15 @@ import { Members } from './members';
 
 export const ProjectPage = () => {
   let { projectId } = useParams({ from: '/projects/$projectId' });
-  const [isLoading, setIsLoading] = useState(false);
+  const [isProjectLoading, setIsProjectLoading] = useState(false);
   const [project, setProject] = useState<V1Beta1Project>();
   const [members, setMembers] = useState<V1Beta1User[]>([]);
   const [memberRoles, setMemberRoles] = useState<Record<string, Role[]>>({});
+  const [isMembersLoading, setIsMembersLoading] = useState(false);
+
+  const [teams, setTeams] = useState<V1Beta1Group[]>([]);
+  const [isTeamsLoading, setIsTeamsLoading] = useState(false);
+
   const { client, activeOrganization: organization } = useFrontier();
   let navigate = useNavigate({ from: '/projects/$projectId' });
   const routeState = useRouterState();
@@ -33,46 +38,81 @@ export const ProjectPage = () => {
     );
   }, [routeState.matches]);
 
-  useEffect(() => {
-    async function getProjectDetails() {
-      if (!organization?.id || !projectId || isDeleteRoute) return;
-
-      try {
-        setIsLoading(true);
+  const getProjectTeams = useCallback(async () => {
+    if (!organization?.id || !projectId || isDeleteRoute) return;
+    try {
+      setIsTeamsLoading(true);
+      const result = await client?.frontierServiceListProjectGroups(projectId);
+      if (result) {
         const {
-          // @ts-ignore
-          data: { project }
-        } = await client?.frontierServiceGetProject(projectId);
-
-        const {
-          // @ts-ignore
-          data: { users, role_pairs }
-        } = await client?.frontierServiceListProjectUsers(projectId, {
-          withRoles: true
-        });
-        setProject(project);
-        setMembers(users);
-        setMemberRoles(
-          role_pairs.reduce((previous: any, mr: any) => {
-            return { ...previous, [mr.user_id]: mr.roles };
-          }, {})
-        );
-      } catch ({ error }: any) {
-        toast.error('Something went wrong', {
-          description: error.message
-        });
-      } finally {
-        setIsLoading(false);
+          data: { groups = [] }
+        } = result;
+        setTeams(groups);
       }
+    } catch (error: any) {
+      toast.error('Something went wrong', {
+        description: error?.message
+      });
+    } finally {
+      setIsTeamsLoading(false);
     }
+  }, [client, isDeleteRoute, organization?.id, projectId]);
+
+  const getProjectMembers = useCallback(async () => {
+    if (!organization?.id || !projectId || isDeleteRoute) return;
+    try {
+      setIsMembersLoading(true);
+      const {
+        // @ts-ignore
+        data: { users, role_pairs }
+      } = await client?.frontierServiceListProjectUsers(projectId, {
+        withRoles: true
+      });
+      setMembers(users);
+      setMemberRoles(
+        role_pairs.reduce((previous: any, mr: any) => {
+          return { ...previous, [mr.user_id]: mr.roles };
+        }, {})
+      );
+    } catch (error: any) {
+      toast.error('Something went wrong', {
+        description: error?.message
+      });
+    } finally {
+      setIsMembersLoading(false);
+    }
+  }, [client, isDeleteRoute, organization?.id, projectId]);
+
+  const getProjectDetails = useCallback(async () => {
+    if (!organization?.id || !projectId || isDeleteRoute) return;
+    try {
+      setIsProjectLoading(true);
+      const {
+        // @ts-ignore
+        data: { project }
+      } = await client?.frontierServiceGetProject(projectId);
+      setProject(project);
+    } catch (error: any) {
+      toast.error('Something went wrong', {
+        description: error?.message
+      });
+    } finally {
+      setIsProjectLoading(false);
+    }
+  }, [client, isDeleteRoute, organization?.id, projectId]);
+
+  useEffect(() => {
     getProjectDetails();
-  }, [
-    client,
-    organization?.id,
-    projectId,
-    routeState.location.state.key,
-    isDeleteRoute
-  ]);
+    getProjectMembers();
+    getProjectTeams();
+  }, [getProjectDetails, getProjectMembers, getProjectTeams]);
+
+  const isLoading = isProjectLoading || isTeamsLoading || isMembersLoading;
+
+  const refetchTeamAndMembers = useCallback(() => {
+    getProjectMembers();
+    getProjectTeams();
+  }, [getProjectMembers, getProjectTeams]);
 
   return (
     <Flex direction="column" style={{ width: '100%' }}>
@@ -99,7 +139,7 @@ export const ProjectPage = () => {
           <General
             organization={organization}
             project={project}
-            isLoading={isLoading}
+            isLoading={isProjectLoading}
           />
         </Tabs.Content>
         <Tabs.Content value="members">
@@ -107,6 +147,8 @@ export const ProjectPage = () => {
             members={members}
             memberRoles={memberRoles}
             isLoading={isLoading}
+            teams={teams}
+            refetch={refetchTeamAndMembers}
           />
         </Tabs.Content>
       </Tabs>
