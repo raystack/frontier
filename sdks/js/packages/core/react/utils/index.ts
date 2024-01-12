338ab177903d81be6eec1de9389fc723fa9810d4
Rishabh <me@rsbh.dev>
2024-01-12 10:57:17 +0530

    refactor: move billing helpers to utils file

--- sdks/js/packages/core/react/utils/index.ts
+++ sdks/js/packages/core/react/utils/index.ts
@@ -1,2 +1,27 @@
+import dayjs from 'dayjs';
+import { V1Beta1Subscription, BillingAccountAddress } from '~/src';
+
 export const AuthTooltipMessage =
   'You don’t have access to perform this action';
+
+export const SUBSCRIPTION_STATES = {
+  ACTIVE: 'active'
+};
+
+export const converBillingAddressToString = (
+  address?: BillingAccountAddress
+) => {
+  if (!address) return '';
+  const { line1, line2, city, state, country, postal_code } = address;
+  return [line1, line2, city, state, country, postal_code]
+    .filter(v => v)
+    .join(', ');
+};
+
+export const getActiveSubscription = (subscriptions: V1Beta1Subscription[]) => {
+  const activeSubscriptions = subscriptions
+    .filter(sub => sub.state === SUBSCRIPTION_STATES.ACTIVE)
+    .sort((a, b) => (dayjs(a.updated_at).isAfter(b.updated_at) ? -1 : 1));
+
+  return activeSubscriptions[0];
+};
