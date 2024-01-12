db0affd58ae72a7a78d7f5193ae31f1c5eac4995
Rishabh <me@rsbh.dev>
2024-01-12 11:47:29 +0530

    feat: add no plans and loading screen

--- sdks/js/packages/core/react/components/organization/plans/index.tsx
+++ sdks/js/packages/core/react/components/organization/plans/index.tsx
@@ -1,6 +1,34 @@
-import { Button, Flex, Text } from '@raystack/apsara';
+import { Button, EmptyState, Flex, Text } from '@raystack/apsara';
 import { styles } from '../styles';
 import { useFrontier } from '~/react/contexts/FrontierContext';
+import { useEffect, useState } from 'react';
+import { V1Beta1Plan } from '~/src';
+import { toast } from 'sonner';
+import Skeleton from 'react-loading-skeleton';
+import plansStyles from './plans.module.css';
+
+const PlansLoader = () => {
+  return (
+    <Flex direction={'column'}>
+      {[...new Array(15)].map(i => (
+        <Skeleton containerClassName={plansStyles.flex1} key={`loader-${i}`} />
+      ))}
+    </Flex>
+  );
+};
+
+const NoPlans = () => {
+  return (
+    <EmptyState style={{ marginTop: 160 }}>
+      <Text size={5} style={{ fontWeight: 'bold' }}>
+        No Plans Available
+      </Text>
+      <Text size={2}>
+        Sorry, No plans available at this moment. Please try again later
+      </Text>
+    </EmptyState>
+  );
+};
 
 interface PlansHeaderProps {
   billingSupportEmail?: string;
@@ -32,8 +60,41 @@ const PlansHeader = ({ billingSupportEmail }: PlansHeaderProps) => {
   );
 };
 
+interface PlansListProps {
+  plans: V1Beta1Plan[];
+}
+
+const PlansList = ({ plans = [] }: PlansListProps) => {
+  if (plans.length === 0) return <NoPlans />;
+  return <Flex>No Plans</Flex>;
+};
+
 export default function Plans() {
-  const { config } = useFrontier();
+  const { config, client } = useFrontier();
+  const [isPlansLoading, setIsPlansLoading] = useState(false);
+  const [plans, setPlans] = useState<V1Beta1Plan[]>([]);
+
+  useEffect(() => {
+    async function getPlans() {
+      setIsPlansLoading(true);
+      try {
+        const resp = await client?.frontierServiceListPlans();
+        if (resp?.data?.plans) {
+          setPlans(resp?.data?.plans);
+        }
+      } catch (err: any) {
+        toast.error('Something went wrong', {
+          description: err.message
+        });
+        console.error(err);
+      } finally {
+        setIsPlansLoading(false);
+      }
+    }
+
+    getPlans();
+  }, [client]);
+
   return (
     <Flex direction="column" style={{ width: '100%' }}>
       <Flex style={styles.header}>
@@ -43,6 +104,7 @@ export default function Plans() {
         <Flex direction="column" style={{ gap: '24px' }}>
           <PlansHeader billingSupportEmail={config.billing?.supportEmail} />
         </Flex>
+        {isPlansLoading ? <PlansLoader /> : <PlansList plans={plans} />}
       </Flex>
     </Flex>
   );
