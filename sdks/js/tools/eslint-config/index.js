0189f5570d7bb29c64f8c6f8e9014bc23333027c
Rishabh <me@rsbh.dev>
2024-05-13 14:33:40 +0530

    chore: add eslint recommened rules

--- sdks/js/tools/eslint-config/index.js
+++ sdks/js/tools/eslint-config/index.js
@@ -6,8 +6,6 @@ module.exports = {
     'turbo',
     'prettier',
     "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
     'plugin:test-selectors/recommended'
   ],
   rules: {
