5ab4ecad849980a25f6e92611a5b88ef02abc3c4
Rishabh <me@rsbh.dev>
2024-05-14 12:54:57 +0530

    feat: show platform user status in service user table

--- ui/src/containers/organisations.list/serviceusers/columns.tsx
+++ ui/src/containers/organisations.list/serviceusers/columns.tsx
@@ -1,4 +1,5 @@
-import { V1Beta1User } from "@raystack/frontier";
+import { CheckCircledIcon } from "@radix-ui/react-icons";
+import { V1Beta1ServiceUser, V1Beta1User } from "@raystack/frontier";
 import type { ColumnDef } from "@tanstack/react-table";
 import { createColumnHelper } from "@tanstack/react-table";
 import { Link } from "react-router-dom";
@@ -7,10 +8,12 @@ const columnHelper = createColumnHelper<V1Beta1User>();
 
 interface getColumnsOptions {
   orgId: string;
+  platformUsers: V1Beta1ServiceUser[];
 }
 export const getColumns: (
   opt: getColumnsOptions
-) => ColumnDef<V1Beta1User, any>[] = ({ orgId }) => {
+) => ColumnDef<V1Beta1User, any>[] = ({ orgId, platformUsers }) => {
+  const platformUsersIdSet = new Set(platformUsers?.map((user) => user?.id));
   return [
     columnHelper.accessor("id", {
       header: "ID",
@@ -30,6 +33,12 @@ export const getColumns: (
       accessorKey: "title",
       filterVariant: "text",
       cell: (info) => info.getValue(),
+    },
+    {
+      header: "Platform User",
+      accessorKey: "",
+      cell: ({ row }) =>
+        platformUsersIdSet.has(row?.original?.id) ? <CheckCircledIcon /> : null,
     },
     {
       header: "Created At",
