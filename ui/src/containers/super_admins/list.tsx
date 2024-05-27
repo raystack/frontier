8ed14bbc2227c0fef05691e69ea19b42b976ddcf
Rishabh <me@rsbh.dev>
2024-05-27 11:28:56 +0530

    feat: add page to show all super admins

---
+++ ui/src/containers/super_admins/list.tsx
@@ -0,0 +1,30 @@
+import { DataTable, Flex } from "@raystack/apsara";
+import { useContext } from "react";
+import { AppContext } from "~/contexts/App";
+import { getColumns } from "./columns";
+
+export function SuperAdminList() {
+  const { platformUsers } = useContext(AppContext);
+
+  const tableStyle = { width: "100%" };
+  const columns = getColumns();
+  const data = [
+    ...(platformUsers?.users || []),
+    ...(platformUsers?.serviceusers || []),
+  ];
+  return (
+    <Flex direction="row" style={{ height: "100%", width: "100%" }}>
+      <DataTable
+        data={data || []}
+        // @ts-ignore
+        columns={columns}
+        parentStyle={{ height: "calc(100vh - 60px)" }}
+        style={tableStyle}
+      >
+        <DataTable.Toolbar>
+          <DataTable.FilterChips style={{ padding: "8px 24px" }} />
+        </DataTable.Toolbar>
+      </DataTable>
+    </Flex>
+  );
+}
