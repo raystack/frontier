ab67d3059b8f1c59271083a3c26e5891a94daf22
Rishabh <me@rsbh.dev>
2024-06-12 20:29:00 +0530

    refactor: remove loading check and add filter in invoices list

--- ui/src/containers/invoices.list/columns.tsx
+++ ui/src/containers/invoices.list/columns.tsx
@@ -1,13 +1,10 @@
 import { V1Beta1Invoice, V1Beta1Organization } from "@raystack/frontier";
-import { ColumnDef } from "@tanstack/react-table";
-import Skeleton from "react-loading-skeleton";
 import { Link } from "react-router-dom";
-import { Text } from "@raystack/apsara";
+import { ApsaraColumnDef, Text } from "@raystack/apsara";
 import dayjs from "dayjs";
 import * as R from "ramda";
 
 interface getColumnsOptions {
-  isLoading: boolean;
   billingOrgMap: Record<string, string>;
   orgMap: Record<string, V1Beta1Organization>;
 }
@@ -16,82 +13,68 @@ const currencyDecimal = 2;
 
 export const getColumns: (
   opt: getColumnsOptions
-) => ColumnDef<V1Beta1Invoice, any>[] = ({
-  orgMap,
-  isLoading,
-  billingOrgMap,
-}) => {
+) => ApsaraColumnDef<V1Beta1Invoice>[] = ({ orgMap, billingOrgMap }) => {
   return [
     {
       header: "Date",
+      filterVariant: "date",
       accessorKey: "due_date",
-      cell: isLoading
-        ? () => <Skeleton />
-        : ({ row, getValue }) => {
-            const date = getValue() || row.original.period_end_at;
-            return <Text>{dayjs(date).format("MMM DD, YYYY")}</Text>;
-          },
+      cell: ({ row, getValue }) => {
+        const date = getValue() || row.original.period_end_at;
+        return <Text>{dayjs(date).format("MMM DD, YYYY")}</Text>;
+      },
     },
     {
       header: "Amount",
       accessorKey: "amount",
-      cell: isLoading
-        ? () => <Skeleton />
-        : ({ row, getValue }) => {
-            const currency = row?.original?.currency;
+      cell: ({ row, getValue }) => {
+        const currency = row?.original?.currency;
 
-            const value = getValue();
+        const value = getValue();
 
-            const calculatedValue = (
-              value / Math.pow(10, currencyDecimal)
-            ).toFixed(currencyDecimal);
-            const [intValue, decimalValue] = calculatedValue
-              .toString()
-              .split(".");
+        const calculatedValue = (value / Math.pow(10, currencyDecimal)).toFixed(
+          currencyDecimal
+        );
+        const [intValue, decimalValue] = calculatedValue.toString().split(".");
 
-            return (
-              <Text>
-                {currency} {intValue}.{decimalValue}
-              </Text>
-            );
-          },
+        return (
+          <Text>
+            {currency} {intValue}.{decimalValue}
+          </Text>
+        );
+      },
     },
     {
       header: "Organization",
       accessorKey: "customer_id",
-      cell: isLoading
-        ? () => <Skeleton />
-        : ({ row, getValue }) => {
-            const billingId = getValue();
-            const orgId = R.pathOr("", [billingId], billingOrgMap);
-            const orgName = R.pathOr("", [orgId, "title"], orgMap);
-            return <Text>{orgName}</Text>;
-          },
+      cell: ({ row, getValue }) => {
+        const billingId = getValue();
+        const orgId = R.pathOr("", [billingId], billingOrgMap);
+        const orgName = R.pathOr("", [orgId, "title"], orgMap);
+        return <Text>{orgName}</Text>;
+      },
     },
     {
       header: "State",
       accessorKey: "state",
-      cell: isLoading
-        ? () => <Skeleton />
-        : ({ row, getValue }) => {
-            return <Text>{getValue()}</Text>;
-          },
+      cell: ({ row, getValue }) => {
+        return <Text>{getValue()}</Text>;
+      },
     },
     {
       header: "Link",
       accessorKey: "hosted_url",
-      cell: isLoading
-        ? () => <Skeleton />
-        : ({ row, getValue }) => {
-            const link = getValue();
-            return link ? (
-              <Link to={link} target="__blank">
-                Link
-              </Link>
-            ) : (
-              <Text>-</Text>
-            );
-          },
+      enableColumnFilter: false,
+      cell: ({ row, getValue }) => {
+        const link = getValue();
+        return link ? (
+          <Link to={link} target="__blank">
+            Link
+          </Link>
+        ) : (
+          <Text>-</Text>
+        );
+      },
     },
   ];
 };
