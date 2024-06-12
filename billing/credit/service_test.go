eed0b8b59776423d3dd944868c67ecf1afac038c
Anuj Khandelwal <khandelwal.anuj14@gmail.com>
2024-06-12 11:57:00 +0530

    feat: add tests for creditService.GetByID method

--- billing/credit/service_test.go
+++ billing/credit/service_test.go
@@ -2,10 +2,10 @@ package credit_test
 
 import (
 	"context"
-	"testing"
-
+	"errors"
 	"github.com/raystack/frontier/billing/credit"
 	"github.com/raystack/frontier/billing/credit/mocks"
+	"testing"
 )
 
 func mockService(t *testing.T) (*credit.Service, *mocks.TransactionRepository) {
@@ -39,6 +39,19 @@ func TestService_GetBalance(t *testing.T) {
 				return s
 			},
 		},
+		{
+			name: "should return an error if there is an error in fetching transactions",
+			args: args{
+				accountID: "1",
+			},
+			want:    0,
+			wantErr: true,
+			setup: func() *credit.Service {
+				s, mockTransaction := mockService(t)
+				mockTransaction.EXPECT().GetBalance(ctx, "1").Return(0, errors.New("An error occurred"))
+				return s
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -49,6 +62,66 @@ func TestService_GetBalance(t *testing.T) {
 				return
 			}
 			if got != tt.want {
+				t.Errorf("GetBalance() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestService_GetByID(t *testing.T) {
+	ctx := context.Background()
+	type args struct {
+		transactionID string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    credit.Transaction
+		wantErr bool
+		setup   func() *credit.Service
+	}{
+		{
+			name: "should return transaction if the transaction id passed is valid",
+			args: args{
+				transactionID: "1",
+			},
+			want: credit.Transaction{
+				ID:         "1",
+				CustomerID: "abc",
+			},
+			wantErr: false,
+			setup: func() *credit.Service {
+				s, mockTransaction := mockService(t)
+				mockTransaction.EXPECT().GetByID(ctx, "1").Return(credit.Transaction{
+					ID:         "1",
+					CustomerID: "abc",
+				}, nil)
+				return s
+			},
+		},
+		{
+			name: "should return error if there is an error in fetching the transaction",
+			args: args{
+				transactionID: "1",
+			},
+			want:    credit.Transaction{},
+			wantErr: true,
+			setup: func() *credit.Service {
+				s, mockTransaction := mockService(t)
+				mockTransaction.EXPECT().GetByID(ctx, "1").Return(credit.Transaction{}, errors.New("An error occurred"))
+				return s
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := tt.setup()
+			got, err := s.GetByID(ctx, tt.args.transactionID)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetBalance() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if (got.ID != tt.want.ID) || (got.CustomerID != tt.want.CustomerID) {
 				t.Errorf("GetBalance() got = %v, want %v", got, tt.want)
 			}
 		})
