da88f4d1a9062abe7f954ff66a4e0d63f9b24e00
Rishabh <me@rsbh.dev>
2024-06-12 20:02:42 +0530

    refactor: remove loading check from users list

--- ui/src/containers/users.list/columns.tsx
+++ ui/src/containers/users.list/columns.tsx
@@ -1,20 +1,17 @@
+import { ApsaraColumnDef } from "@raystack/apsara";
 import { V1Beta1User } from "@raystack/frontier";
-import type { ColumnDef } from "@tanstack/react-table";
-import { createColumnHelper } from "@tanstack/react-table";
 import { Link } from "react-router-dom";
 
-const columnHelper = createColumnHelper<V1Beta1User>();
-
-export const getColumns: () => ColumnDef<V1Beta1User, any>[] = () => {
+export const getColumns: () => ApsaraColumnDef<V1Beta1User>[] = () => {
   return [
-    columnHelper.accessor("id", {
+    {
+      accessorKey: "id",
       header: "ID",
-      //@ts-ignore
       filterVariant: "text",
       cell: ({ row, getValue }) => {
         return <Link to={`/users/${row.getValue("id")}`}>{getValue()}</Link>;
       },
-    }),
+    },
     {
       header: "Title",
       accessorKey: "title",
