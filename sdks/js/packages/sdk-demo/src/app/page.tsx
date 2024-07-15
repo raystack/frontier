bbf6a2d049d712cd7521c6b5d4c288f4b35fffb3
Rishabh <me@rsbh.dev>
2024-07-15 15:36:55 +0530

    feat: add org list and sdk page

--- sdks/js/packages/sdk-demo/src/app/page.tsx
+++ sdks/js/packages/sdk-demo/src/app/page.tsx
@@ -1,11 +1,14 @@
 'use client';
 import AuthContext from '@/contexts/auth';
+import { Flex } from '@raystack/apsara';
+import { useFrontier } from '@raystack/frontier/react';
+import Link from 'next/link';
 import { redirect } from 'next/navigation';
 import { useContext, useEffect } from 'react';
 
 export default function Home() {
   const { isAuthorized } = useContext(AuthContext);
-
+  const { organizations } = useFrontier();
   useEffect(() => {
     if (!isAuthorized) {
       redirect('/login');
@@ -14,7 +17,27 @@ export default function Home() {
 
   return (
     <main>
-      <h1>Frontier SDK</h1>
+      <Flex
+        justify="center"
+        align="center"
+        style={{ height: '100vh', width: '100vw' }}
+      >
+        <Flex direction="column">
+          {organizations.map(org => (
+            <Flex
+              key={org.id}
+              style={{
+                padding: '16px',
+                border: '1px solid var(--border-base)',
+                width: '100%',
+                margin: '8px'
+              }}
+            >
+              <Link href={`/${org.id}`}>{org.title}</Link>
+            </Flex>
+          ))}
+        </Flex>
+      </Flex>
     </main>
   );
 }
