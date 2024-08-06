671ae46af4f60952e2c7824c30dedcfe24f02cb5
Rishabh <me@rsbh.dev>
2024-08-06 18:56:31 +0530

    feat: add webhooks list page

--- ui/src/containers/webhooks/index.tsx
+++ ui/src/containers/webhooks/index.tsx
@@ -1,13 +1,39 @@
 import { DataTable, Flex } from "@raystack/apsara";
+import { V1Beta1Webhook } from "@raystack/frontier";
+import { useFrontier } from "@raystack/frontier/react";
+import { useEffect, useState } from "react";
+import { getColumns } from "./columns";
+import { date } from "zod";
 
 export function WebhooksList() {
   const tableStyle = { width: "100%" };
+  const { client } = useFrontier();
+  const [webhooks, setWebhooks] = useState<V1Beta1Webhook[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
+  useEffect(() => {
+    async function fetchWebhooks() {
+      try {
+        setIsLoading(true);
+        const resp = await client?.adminServiceListWebhooks();
+        const data = resp?.data?.webhooks || [];
+        setWebhooks(data);
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchWebhooks();
+  }, [client]);
+
+  const columns = getColumns();
   return (
     <Flex direction="row" style={{ height: "100%", width: "100%" }}>
       <DataTable
-        data={[]}
-        columns={[]}
+        data={webhooks}
+        columns={columns}
+        isLoading={isLoading}
         parentStyle={{ height: "calc(100vh - 60px)" }}
         style={tableStyle}
       >
