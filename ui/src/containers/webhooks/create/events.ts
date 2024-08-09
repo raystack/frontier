0c8063200fb50e6c062863be057b2c0bd44ac029
Rishabh <me@rsbh.dev>
2024-08-09 16:03:32 +0530

    feat: add page to create webhooks

---
+++ ui/src/containers/webhooks/create/events.ts
@@ -0,0 +1,44 @@
+const events = [
+  "app.user.created",
+  "app.user.updated",
+  "app.user.deleted",
+  "app.user.listed",
+  "app.serviceuser.created",
+  "app.serviceuser.deleted",
+
+  "app.group.created",
+  "app.group.updated",
+  "app.group.deleted",
+  "app.group.members.removed",
+
+  "app.role.created",
+  "app.role.updated",
+  "app.role.deleted",
+
+  "app.permission.created",
+  "app.permission.updated",
+  "app.permission.deleted",
+  "app.permission.checked",
+
+  "app.billing.entitlement.checked",
+
+  "app.policy.created",
+  "app.policy.deleted",
+
+  "app.organization.created",
+  "app.organization.updated",
+  "app.organization.deleted",
+  "app.organization.disabled",
+  "app.organization.member.created",
+  "app.organization.member.deleted",
+
+  "app.project.created",
+  "app.project.updated",
+  "app.project.deleted",
+
+  "app.resource.created",
+  "app.resource.updated",
+  "app.resource.deleted",
+];
+
+export default events;
