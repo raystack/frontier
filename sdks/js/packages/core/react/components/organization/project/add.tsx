c8286804d807d1724e81e4ea1e6ebb7e62006199
Rishabh <me@rsbh.dev>
2024-08-28 14:23:36 +0530

    feat: use title slug for project name

--- sdks/js/packages/core/react/components/organization/project/add.tsx
+++ sdks/js/packages/core/react/components/organization/project/add.tsx
@@ -11,27 +11,18 @@ import {
 
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useNavigate } from '@tanstack/react-router';
-import { useEffect } from 'react';
 import { Controller, useForm } from 'react-hook-form';
 import { toast } from 'sonner';
 import * as yup from 'yup';
 import cross from '~/react/assets/cross.svg';
 import { useFrontier } from '~/react/contexts/FrontierContext';
 import styles from '../organization.module.css';
+import { generateSlug } from '~/utils';
+import { V1Beta1ProjectRequestBody } from '~/api-client';
 
 const projectSchema = yup
   .object({
-    title: yup.string().required(),
-    name: yup
-      .string()
-      .required('name is a required field')
-      .min(3, 'name is not valid, Min 3 characters allowed')
-      .max(50, 'name is not valid, Max 50 characters allowed')
-      .matches(
-        /^[a-zA-Z0-9_-]{3,50}$/,
-        "Only numbers, letters, '-', and '_' are allowed. Spaces are not allowed."
-      ),
-    orgId: yup.string().required()
+    title: yup.string().required()
   })
   .required();
 
@@ -39,7 +30,6 @@ type FormData = yup.InferType<typeof projectSchema>;
 
 export const AddProject = () => {
   const {
-    reset,
     control,
     handleSubmit,
     formState: { errors, isSubmitting }
@@ -49,15 +39,17 @@ export const AddProject = () => {
   const navigate = useNavigate({ from: '/projects/modal' });
   const { client, activeOrganization: organization } = useFrontier();
 
-  useEffect(() => {
-    reset({ orgId: organization?.id });
-  }, [organization, reset]);
-
   async function onSubmit(data: FormData) {
-    if (!client) return;
+    if (!client || !organization?.id) return;
+
+    const payload: V1Beta1ProjectRequestBody = {
+      title: data.title,
+      org_id: organization?.id || '',
+      name: generateSlug(data.title)
+    };
 
     try {
-      await client.frontierServiceCreateProject(data);
+      await client.frontierServiceCreateProject(payload);
       toast.success('Project created');
       navigate({ to: '/projects' });
     } catch ({ error }: any) {
@@ -84,6 +76,7 @@ export const AddProject = () => {
             src={cross}
             onClick={() => navigate({ to: '/projects' })}
             style={{ cursor: 'pointer' }}
+            data-test-id="frontier-sdk-add-project-close-btn"
           />
         </Flex>
         <Separator />
@@ -114,24 +107,6 @@ export const AddProject = () => {
 
               <Text size={1} style={{ color: 'var(--foreground-danger)' }}>
                 {errors.title && String(errors.title?.message)}
-              </Text>
-            </InputField>
-            <InputField label="Project name">
-              <Controller
-                render={({ field }) => (
-                  <TextField
-                    {...field}
-                    // @ts-ignore
-                    size="medium"
-                    placeholder="Provide project name"
-                  />
-                )}
-                control={control}
-                name="name"
-              />
-
-              <Text size={1} style={{ color: 'var(--foreground-danger)' }}>
-                {errors.name && String(errors.name?.message)}
               </Text>
             </InputField>
           </Flex>
