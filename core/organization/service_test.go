03c6d1a995f9b929db91ec105bdc23e683e900f5
Anuj Khandelwal <khandelwal.anuj14@gmail.com>
2024-08-29 12:12:25 +0530

    add tests for GetRaw method

--- core/organization/service_test.go
+++ core/organization/service_test.go
@@ -67,3 +67,59 @@ func TestService_Get(t *testing.T) {
 		assert.Equal(t, organization.Organization{}, org)
 	})
 }
+
+func TestService_GetRaw(t *testing.T) {
+	mockRepo := mocks.NewRepository(t)
+	mockRelationSvc := mocks.NewRelationService(t)
+	mockUserSvc := mocks.NewUserService(t)
+	mockAuthnSvc := mocks.NewAuthnService(t)
+	mockPolicySvc := mocks.NewPolicyService(t)
+	mockPrefSvc := mocks.NewPreferencesService(t)
+
+	svc := organization.NewService(mockRepo, mockRelationSvc, mockUserSvc, mockAuthnSvc, mockPolicySvc, mockPrefSvc)
+
+	t.Run("should return an org based on ID passed", func(t *testing.T) {
+		IDParam := uuid.New()
+		expectedOrg := organization.Organization{
+			ID:    IDParam.String(),
+			Name:  "test-org",
+			Title: "test organization",
+			State: "enabled",
+		}
+
+		mockRepo.On("GetByID", mock.Anything, IDParam.String()).Return(expectedOrg, nil).Once()
+		org, err := svc.GetRaw(context.Background(), IDParam.String())
+		assert.Nil(t, err)
+		assert.Equal(t, expectedOrg, org)
+	})
+
+	t.Run("should return an org based on name passed", func(t *testing.T) {
+		nameParam := "test-org"
+		expectedOrg := organization.Organization{
+			ID:    uuid.New().String(),
+			Name:  nameParam,
+			Title: "test organization",
+			State: "enabled",
+		}
+
+		mockRepo.On("GetByName", mock.Anything, nameParam).Return(expectedOrg, nil).Once()
+		org, err := svc.GetRaw(context.Background(), nameParam)
+		assert.Nil(t, err)
+		assert.Equal(t, expectedOrg, org)
+	})
+
+	t.Run("should return an org even if it is disabled", func(t *testing.T) {
+		nameParam := "test-org"
+		expectedOrg := organization.Organization{
+			ID:    uuid.New().String(),
+			Name:  nameParam,
+			Title: "test organization",
+			State: organization.Disabled,
+		}
+
+		mockRepo.On("GetByName", mock.Anything, nameParam).Return(expectedOrg, nil).Once()
+		org, err := svc.GetRaw(context.Background(), nameParam)
+		assert.Nil(t, err)
+		assert.Equal(t, expectedOrg, org)
+	})
+}
