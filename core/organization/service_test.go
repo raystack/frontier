df9620453f61936797c81b1b2845948729efacc8
Anuj Khandelwal <khandelwal.anuj14@gmail.com>
2024-09-02 11:49:49 +0530

    add test for adding users to org and attaching org to platform

--- core/organization/service_test.go
+++ core/organization/service_test.go
@@ -6,9 +6,13 @@ import (
 	"testing"
 
 	"github.com/google/uuid"
+	"github.com/raystack/frontier/core/authenticate"
 	"github.com/raystack/frontier/core/organization"
 	"github.com/raystack/frontier/core/organization/mocks"
+	"github.com/raystack/frontier/core/policy"
 	"github.com/raystack/frontier/core/preference"
+	"github.com/raystack/frontier/core/relation"
+	"github.com/raystack/frontier/internal/bootstrap/schema"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
@@ -157,13 +161,84 @@ func TestService_GetDefaultOrgStateOnCreate(t *testing.T) {
 }
 
 func TestService_AddMember(t *testing.T) {
-	t.Run("", func(t *testing.T) {})
+	mockRepo := mocks.NewRepository(t)
+	mockRelationSvc := mocks.NewRelationService(t)
+	mockUserSvc := mocks.NewUserService(t)
+	mockAuthnSvc := mocks.NewAuthnService(t)
+	mockPolicySvc := mocks.NewPolicyService(t)
+	mockPrefSvc := mocks.NewPreferencesService(t)
 
-	t.Run("", func(t *testing.T) {})
+	svc := organization.NewService(mockRepo, mockRelationSvc, mockUserSvc, mockAuthnSvc, mockPolicySvc, mockPrefSvc)
+
+	t.Run("should create policy and relation for member as per role", func(t *testing.T) {
+		inputOrgID := "test-id"
+		inputRelationName := schema.MemberRelationName
+		inputPrincipal := authenticate.Principal{
+			ID:   "test-principal-id",
+			Type: schema.UserPrincipal,
+		}
+
+		policyToBeCreated := policy.Policy{
+			RoleID:        organization.MemberRole,
+			ResourceID:    inputOrgID,
+			ResourceType:  schema.OrganizationNamespace,
+			PrincipalID:   inputPrincipal.ID,
+			PrincipalType: inputPrincipal.Type,
+		}
+
+		relationToBeCreated := relation.Relation{
+			Object: relation.Object{
+				ID:        inputOrgID,
+				Namespace: schema.OrganizationNamespace,
+			},
+			Subject: relation.Subject{
+				ID:        inputPrincipal.ID,
+				Namespace: inputPrincipal.Type,
+			},
+			RelationName: schema.MemberRelationName,
+		}
+		mockPolicySvc.On("Create", mock.Anything, policyToBeCreated).Return(policy.Policy{}, nil)
+		mockRelationSvc.On("Create", mock.Anything, relationToBeCreated).Return(relation.Relation{}, nil)
+
+		err := svc.AddMember(context.Background(), inputOrgID, inputRelationName, inputPrincipal)
+		assert.Nil(t, err)
+	})
 }
 
 func TestService_AttachToPlatform(t *testing.T) {
-	t.Run("", func(t *testing.T) {})
+	mockRepo := mocks.NewRepository(t)
+	mockRelationSvc := mocks.NewRelationService(t)
+	mockUserSvc := mocks.NewUserService(t)
+	mockAuthnSvc := mocks.NewAuthnService(t)
+	mockPolicySvc := mocks.NewPolicyService(t)
+	mockPrefSvc := mocks.NewPreferencesService(t)
 
-	t.Run("", func(t *testing.T) {})
+	svc := organization.NewService(mockRepo, mockRelationSvc, mockUserSvc, mockAuthnSvc, mockPolicySvc, mockPrefSvc)
+
+	inputOrgID := "some-org-id"
+	relationToBeCreated := relation.Relation{
+		Object: relation.Object{
+			ID:        inputOrgID,
+			Namespace: schema.OrganizationNamespace,
+		},
+		Subject: relation.Subject{
+			ID:        schema.PlatformID,
+			Namespace: schema.PlatformNamespace,
+		},
+		RelationName: schema.PlatformRelationName,
+	}
+
+	t.Run("should create a relation for org in platform namespace", func(t *testing.T) {
+		mockRelationSvc.On("Create", mock.Anything, relationToBeCreated).Return(relation.Relation{}, nil).Once()
+		err := svc.AttachToPlatform(context.Background(), inputOrgID)
+		assert.Nil(t, err)
+	})
+
+	t.Run("should return an error if relation creation fails", func(t *testing.T) {
+		expectedErr := errors.New("Internal error")
+		mockRelationSvc.On("Create", mock.Anything, relationToBeCreated).Return(relation.Relation{}, expectedErr).Once()
+		err := svc.AttachToPlatform(context.Background(), inputOrgID)
+		assert.NotNil(t, err)
+		assert.Equal(t, expectedErr, err)
+	})
 }
