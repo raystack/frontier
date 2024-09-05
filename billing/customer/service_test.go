f006da7b6d724369cb4e68c9de93ea7639acf915
Aman Prasad <aman@Admins-MacBook-Pro.local>
2024-09-05 19:00:56 +0530

    add test cases for errors

--- billing/customer/service_test.go
+++ billing/customer/service_test.go
@@ -2,6 +2,7 @@ package customer_test
 
 import (
 	"context"
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -14,6 +15,8 @@ import (
 	"github.com/stripe/stripe-go/v79"
 	"github.com/stripe/stripe-go/v79/client"
 )
+
+var sampleError = errors.New("sample error")
 
 func mockService(t *testing.T) (*client.API, *stripemock.Backend, *mocks.Repository) {
 	t.Helper()
@@ -230,6 +233,77 @@ func TestService_Update(t *testing.T) {
 				return customer.NewService(stripeClient, mockRepo, cfg)
 			},
 		},
+		{
+
+			name: "return error if GetByID returns an error",
+			args: args{
+				customer: customer.Customer{
+					ID:    "1",
+					Name:  "customer1",
+					OrgID: "org1",
+				},
+			},
+			want: customer.Customer{},
+
+			wantErr: sampleError,
+			setup: func() *customer.Service {
+				stripeClient, _, mockRepo := mockService(t)
+
+				mockRepo.EXPECT().GetByID(ctx, "1").Return(customer.Customer{}, sampleError)
+
+				cfg := billing.Config{}
+
+				return customer.NewService(stripeClient, mockRepo, cfg)
+			},
+		},
+		{
+			name: "return error if stripe returns an error",
+			args: args{
+				customer: customer.Customer{
+					ID:    "1",
+					Name:  "customer1",
+					OrgID: "org1",
+				},
+			},
+			want:    customer.Customer{},
+			wantErr: sampleError,
+			setup: func() *customer.Service {
+				stripeClient, mockStripeBackend, mockRepo := mockService(t)
+
+				mockRepo.EXPECT().GetByID(ctx, "1").Return(customer.Customer{
+					ID:    "1",
+					Name:  "customer1",
+					OrgID: "org1",
+				}, nil)
+
+				mockStripeBackend.EXPECT().Call("POST", "/v1/customers/", "key_123",
+					&stripe.CustomerParams{
+						Params: stripe.Params{
+							Context: ctx,
+						},
+						Address: &stripe.AddressParams{
+							City:       stripe.String(""),
+							Country:    stripe.String(""),
+							Line1:      stripe.String(""),
+							Line2:      stripe.String(""),
+							PostalCode: stripe.String(""),
+							State:      stripe.String(""),
+						},
+						Email:     stripe.String(""),
+						Name:      stripe.String("customer1"),
+						Phone:     stripe.String(""),
+						TaxIDData: nil,
+						Metadata: map[string]string{
+							"managed_by": "frontier",
+							"org_id":     "org1",
+						},
+					}, &stripe.Customer{ID: ""}).Return(sampleError)
+
+				cfg := billing.Config{}
+
+				return customer.NewService(stripeClient, mockRepo, cfg)
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -279,6 +353,23 @@ func TestService_GetByID(t *testing.T) {
 						Name:  "customer1",
 						OrgID: "org1",
 					}, nil)
+
+				cfg := billing.Config{}
+
+				return customer.NewService(stripeClient, mockRepo, cfg)
+			},
+		},
+		{
+			name: "return an error if GetByID returns an error",
+			args: args{
+				id: "1",
+			},
+			want:    customer.Customer{},
+			wantErr: sampleError,
+			setup: func() *customer.Service {
+				stripeClient, _, mockRepo := mockService(t)
+				mockRepo.EXPECT().GetByID(ctx, "1").Return(
+					customer.Customer{}, sampleError)
 
 				cfg := billing.Config{}
 
@@ -346,6 +437,23 @@ func TestService_List(t *testing.T) {
 							Name:  "customer3",
 							OrgID: "org1"},
 					}, nil)
+
+				cfg := billing.Config{}
+
+				return customer.NewService(stripeClient, mockRepo, cfg)
+			},
+		},
+		{
+			name: "return error if List returns an error",
+			args: args{
+				filter: customer.Filter{},
+			},
+			want:    []customer.Customer{},
+			wantErr: sampleError,
+			setup: func() *customer.Service {
+				stripeClient, _, mockRepo := mockService(t)
+				mockRepo.EXPECT().List(ctx, customer.Filter{}).Return(
+					[]customer.Customer{}, sampleError)
 
 				cfg := billing.Config{}
 
