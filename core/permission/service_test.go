5ca50b63ede44f83f851c8a9987e1d1f29b31ada
Anuj Khandelwal <khandelwal.anuj14@gmail.com>
2024-09-11 13:22:14 +0530

    chore: uni tests for permission update, delete

--- core/permission/service_test.go
+++ core/permission/service_test.go
@@ -133,9 +133,9 @@ func TestService_List(t *testing.T) {
 		}
 
 		mockRepo.On("List", mock.Anything, filters).Return(expectedRes, nil).Once()
-		
+
 		perms, err := svc.List(context.Background(), filters)
-		
+
 		assert.Nil(t, err)
 		assert.Equal(t, expectedRes, perms)
 	})
@@ -148,9 +148,86 @@ func TestService_List(t *testing.T) {
 		expectedErr := errors.New("An error occurred")
 
 		mockRepo.On("List", mock.Anything, filters).Return([]permission.Permission{}, expectedErr).Once()
-		
+
 		_, err := svc.List(context.Background(), filters)
-		
+
+		assert.NotNil(t, err)
+		assert.Equal(t, expectedErr, err)
+	})
+}
+func TestService_Update(t *testing.T) {
+	mockRepo := mocks.NewRepository(t)
+	svc := permission.NewService(mockRepo)
+
+	t.Run("should update permission", func(t *testing.T) {
+		inputPermission := permission.Permission{
+			ID:   uuid.New().String(),
+			Name: "permissionname",
+			Slug: "app/permission_name",
+		}
+
+		mockRepo.On("Update", mock.Anything, inputPermission).Return(inputPermission, nil).Once()
+		perm, err := svc.Update(context.Background(), inputPermission)
+
+		assert.Nil(t, err)
+		assert.Equal(t, inputPermission, perm)
+	})
+
+	t.Run("should generate slug if not present in request", func(t *testing.T) {
+		inputPermission := permission.Permission{
+			ID:   uuid.New().String(),
+			Name: "permissionname",
+		}
+
+		expectedRepoInput := inputPermission
+		expectedRepoInput.Slug = inputPermission.GenerateSlug()
+
+		mockRepo.On("Update", mock.Anything, expectedRepoInput).Return(expectedRepoInput, nil).Once()
+		perm, err := svc.Update(context.Background(), inputPermission)
+
+		assert.Nil(t, err)
+		assert.Equal(t, expectedRepoInput, perm)
+	})
+
+	t.Run("should return an error if permission cannot be updated", func(t *testing.T) {
+		inputPermission := permission.Permission{
+			ID:   uuid.New().String(),
+			Name: "permissionname",
+			Slug: "app/permission_name",
+		}
+
+		expectedErr := errors.New("An error occurred")
+
+		mockRepo.On("Update", mock.Anything, inputPermission).Return(permission.Permission{}, expectedErr).Once()
+		_, err := svc.Update(context.Background(), inputPermission)
+
+		assert.NotNil(t, err)
+		assert.Equal(t, expectedErr, err)
+	})
+}
+
+func TestService_Delete(t *testing.T) {
+	mockRepo := mocks.NewRepository(t)
+	svc := permission.NewService(mockRepo)
+
+	t.Run("should delete permission", func(t *testing.T) {
+		permissionID := uuid.New().String()
+
+		mockRepo.On("Delete", mock.Anything, permissionID).Return(nil).Once()
+
+		err := svc.Delete(context.Background(), permissionID)
+
+		assert.Nil(t, err)
+	})
+
+	t.Run("should return an error if permissions cannot be list", func(t *testing.T) {
+		permissionID := uuid.New().String()
+		expectedErr := errors.New("An error occurred")
+
+		mockRepo.On("Delete", mock.Anything, permissionID).Return(expectedErr).Once()
+
+		err := svc.Delete(context.Background(), permissionID)
+
 		assert.NotNil(t, err)
 		assert.Equal(t, expectedErr, err)
 	})
