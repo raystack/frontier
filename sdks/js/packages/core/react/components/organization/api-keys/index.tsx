8a3ab1d4fb9df1839c2314e2a1c3a0a5308b30b2
Rishabh <me@rsbh.dev>
2024-11-20 10:58:58 +0530

    refactor: create seperate component for NoServiceAccounts

--- sdks/js/packages/core/react/components/organization/api-keys/index.tsx
+++ sdks/js/packages/core/react/components/organization/api-keys/index.tsx
@@ -4,35 +4,48 @@ import keyIcon from '~/react/assets/key.svg';
 import { Image } from '@raystack/apsara';
 import { useFrontier } from '~/react/contexts/FrontierContext';
 import { DEFAULT_API_PLATFORM_APP_NAME } from '~/react/utils/constants';
+import { FrontierClientAPIPlatformOptions } from '~/shared/types';
+
+const NoServiceAccounts = ({
+  config
+}: {
+  config?: FrontierClientAPIPlatformOptions;
+}) => {
+  const appName = config?.appName || DEFAULT_API_PLATFORM_APP_NAME;
+
+  return (
+    <EmptyState
+      icon={
+        <Image
+          // @ts-ignore
+          src={keyIcon}
+          alt="keyIcon"
+        />
+      }
+      heading="No service account"
+      subHeading={`Create a new account to use the APIs of ${appName}`}
+      primaryAction={
+        <Button
+          data-test-id="frontier-sdk-new-service-account-btn"
+          variant="secondary"
+        >
+          Create new service account
+        </Button>
+      }
+    />
+  );
+};
 
 export default function ApiKeys() {
   const { config } = useFrontier();
-  const appName = config?.apiPlatform?.appName || DEFAULT_API_PLATFORM_APP_NAME;
+
   return (
     <Flex direction="column" style={{ width: '100%' }}>
       <Flex className={styles.header}>
         <Text size={6}>API Keys</Text>
       </Flex>
       <Flex justify="center" align="center" className={styles.content}>
-        <EmptyState
-          icon={
-            <Image
-              // @ts-ignore
-              src={keyIcon}
-              alt="keyIcon"
-            />
-          }
-          heading="No service account"
-          subHeading={`Create a new account to use the APIs of ${appName}`}
-          primaryAction={
-            <Button
-              data-test-id="frontier-sdk-new-service-account-btn"
-              variant="secondary"
-            >
-              Create new service account
-            </Button>
-          }
-        />
+        <NoServiceAccounts config={config.apiPlatform} />
       </Flex>
     </Flex>
   );
