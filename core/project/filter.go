e41fe1ab732e3f05bf1987cccbe31e741bc62a77
aman <aman@pixxel.co.in>
2024-12-18 13:54:39 +0530

    add pagination for project list api

--- core/project/filter.go
+++ core/project/filter.go
@@ -1,5 +1,7 @@
 package project
 
+import "github.com/raystack/frontier/pkg/pagination"
+
 type Filter struct {
 	OrgID           string
 	WithMemberCount bool
@@ -7,4 +9,5 @@ type Filter struct {
 	State           State
 	// NonInherited filters out projects that are inherited from access given through an organization
 	NonInherited bool
+	Pagination   *pagination.Pagination
 }
