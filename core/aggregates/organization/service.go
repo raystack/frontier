c73da923a8f7a46ee015f169c94da33ed5e6acd2
Abhishek Sah <abhi.sah.97@gmail.com>
2025-02-27 13:14:05 +0530

    feat: add rql tags

--- core/aggregates/organization/service.go
+++ core/aggregates/organization/service.go
@@ -20,18 +20,18 @@ func NewService(repository Repository) *Service {
 }
 
 type AggregatedOrganization struct {
-	ID          string
-	Name        string
-	Title       string
-	CreatedBy   string
-	Plan        string
-	PaymentMode string
-	Country     string
-	Avatar      string
-	State       organization.State
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
-	CycleEndOn  time.Time
+	ID          string             `rql:"type=string"`
+	Name        string             `rql:"type=string"`
+	Title       string             `rql:"type=string"`
+	CreatedBy   string             `rql:"type=string"`
+	Plan        string             `rql:"type=string"`
+	PaymentMode string             `rql:"type=string"`
+	Country     string             `rql:"type=string"`
+	Avatar      string             `rql:"type=string"`
+	State       organization.State `rql:"type=string"`
+	CreatedAt   time.Time          `rql:"type=datetime"`
+	UpdatedAt   time.Time          `rql:"type=datetime"`
+	CycleEndOn  time.Time          `rql:"type=datetime"`
 }
 
 func (s Service) Search(ctx context.Context, query *rql.Query) ([]AggregatedOrganization, error) {
