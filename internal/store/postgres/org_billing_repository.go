f554bad1e9b54f7d2759e06e011209f05bf61193
Abhishek Sah <abhi.sah.97@gmail.com>
2025-02-28 11:27:03 +0530

    feat: fetch latest subscription entry for each org

--- internal/store/postgres/org_billing_repository.go
+++ internal/store/postgres/org_billing_repository.go
@@ -23,6 +23,8 @@ const (
 	COLUMN_PLAN_TITLE                    = "plan"
 	COLUMN_SUBSCRIPTION_STATE            = "subscription_state"
 	COLUMN_SUBSCRIPTION_END_TRIGGERED_AT = "subscription_end_triggered_at"
+	COLUMN_UPDATED_AT                    = "updated_at"
+	COLUMN_ROW_NUM                       = "row_num"
 )
 
 type OrgBilling struct {
@@ -45,7 +47,7 @@ func (r OrgBilling) Search(ctx context.Context, rql *rql.Query) ([]orgbilling.Ag
 }
 
 func prepareSQL() (string, []interface{}, error) {
-	query := goqu.From(TABLE_ORGANIZATIONS).
+	rankedSubscriptions := goqu.From(TABLE_ORGANIZATIONS).
 		Select(
 			goqu.I(TABLE_ORGANIZATIONS+"."+COLUMN_ID).As(COLUMN_ORG_ID),
 			goqu.I(TABLE_ORGANIZATIONS+"."+COLUMN_TITLE).As(COLUMN_ORG_TITLE),
@@ -54,6 +56,8 @@ func prepareSQL() (string, []interface{}, error) {
 			goqu.I(TABLE_BILLING_SUBSCRIPTIONS+"."+COLUMN_ENDED_AT).As(COLUMN_SUBSCRIPTION_END_TRIGGERED_AT),
 			goqu.I(TABLE_BILLING_SUBSCRIPTIONS+"."+COLUMN_TRIAL_ENDS_AT),
 			goqu.I(TABLE_BILLING_SUBSCRIPTIONS+"."+COLUMN_CURRENT_PERIOD_END_AT),
+			goqu.I(TABLE_BILLING_SUBSCRIPTIONS+"."+COLUMN_UPDATED_AT),
+			goqu.Literal("ROW_NUMBER() OVER (PARTITION BY ? ORDER BY ? DESC)", goqu.I(TABLE_ORGANIZATIONS+"."+COLUMN_ID), goqu.I(TABLE_BILLING_SUBSCRIPTIONS+"."+COLUMN_UPDATED_AT)).As(COLUMN_ROW_NUM),
 		).
 		LeftJoin(
 			goqu.T(TABLE_BILLING_CUSTOMERS),
@@ -68,5 +72,17 @@ func prepareSQL() (string, []interface{}, error) {
 			goqu.On(goqu.I(TABLE_BILLING_PLANS+"."+COLUMN_ID).Eq(goqu.I(TABLE_BILLING_SUBSCRIPTIONS+"."+COLUMN_PLAN_ID))),
 		)
 
-	return query.ToSQL()
+	finalQuery := goqu.From(rankedSubscriptions.As("ranked_subscriptions")).
+		Select(
+			goqu.I(COLUMN_ORG_ID),
+			goqu.I(COLUMN_ORG_TITLE),
+			goqu.I(COLUMN_PLAN_TITLE),
+			goqu.I(COLUMN_SUBSCRIPTION_STATE),
+			goqu.I(COLUMN_SUBSCRIPTION_END_TRIGGERED_AT),
+			goqu.I(COLUMN_TRIAL_ENDS_AT),
+			goqu.I(COLUMN_CURRENT_PERIOD_END_AT),
+			goqu.I(COLUMN_UPDATED_AT),
+		).
+		Where(goqu.I(COLUMN_ROW_NUM).Eq(1))
+	return finalQuery.ToSQL()
 }
