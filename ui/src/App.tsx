0a6574a2779212c3f8c5549dbae96f3f4a3e30dc
Rishabh <me@rsbh.dev>
2025-03-04 15:59:21 +0530

    chore: update apsara version

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Flex, ScrollArea, Sidebar, ThemeSwitcher } from "@raystack/apsara";
-import "@raystack/apsara/index.css";
+import "@raystack/apsara/style.css";
 import { useFrontier } from "@raystack/frontier/react";
 import { Outlet, useNavigate } from "react-router-dom";
 import "./App.css";
@@ -104,7 +104,10 @@ function App() {
               <Sidebar.NavigationCell asChild>
                 <ThemeSwitcher size={16} />
               </Sidebar.NavigationCell>
-              <Sidebar.NavigationCell onClick={logout} data-test-id="frontier-sdk-sidebar-logout">
+              <Sidebar.NavigationCell
+                onClick={logout}
+                data-test-id="frontier-sdk-sidebar-logout"
+              >
                 Logout
               </Sidebar.NavigationCell>
             </Sidebar.Navigations>
