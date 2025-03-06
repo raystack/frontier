a05151a3043ba8d14dcc07a39fcd6514186346bc
Rishabh <me@rsbh.dev>
2025-03-06 18:08:56 +0530

    refactor: move sidebar to seperate component

---
+++ ui/src/components/Sidebar/index.tsx
@@ -0,0 +1,144 @@
+import React from "react";
+import { Flex, Sidebar } from "@raystack/apsara/v1";
+import { api } from "~/api";
+import { useNavigate } from "react-router-dom";
+import IAMIcon from "~/assets/icons/iam.svg?react";
+
+export type NavigationItemsTypes = {
+  active?: boolean;
+  to?: string;
+  name: string;
+  icon?: React.ReactNode;
+  subItems?: NavigationItemsTypes[];
+};
+
+const BRAND_NAME = "Frontier";
+
+const navigationItems: NavigationItemsTypes[] = [
+  {
+    name: "Organisations",
+    to: `/organisations`,
+  },
+  {
+    name: "Users",
+    to: `/users`,
+  },
+  {
+    name: "Invoices",
+    to: `/invoices`,
+  },
+  {
+    name: "Authorization",
+    subItems: [
+      {
+        name: "Roles",
+        to: `/roles`,
+      },
+    ],
+  },
+  {
+    name: "Billing",
+    subItems: [
+      {
+        name: "Products",
+        to: `/products`,
+      },
+      {
+        name: "Plans",
+        to: `/plans`,
+      },
+    ],
+  },
+  {
+    name: "Features",
+    subItems: [
+      {
+        name: "Webhooks",
+        to: `/webhooks`,
+      },
+    ],
+  },
+  {
+    name: "Settings",
+    subItems: [
+      {
+        name: "Preferences",
+        to: `/preferences`,
+      },
+      {
+        name: "Super Admins",
+        to: `/super-admins`,
+      },
+    ],
+  },
+  // {
+  //   name: "Projects",
+  //   to: `/projects`,
+  // },
+
+  // {
+  //   name: "Groups",
+  //   to: `/groups`,
+  // },
+];
+
+export default function IAMSidebar() {
+  const navigate = useNavigate();
+
+  async function logout() {
+    await api?.frontierServiceAuthLogout();
+    window.location.href = "/";
+    window.location.reload();
+  }
+
+  return (
+    <Sidebar.Root open>
+      <Sidebar.Header
+        logo={
+          <Flex align="center" style={{ height: "100%" }}>
+            <IAMIcon />
+          </Flex>
+        }
+        title={BRAND_NAME}
+      />
+      <Sidebar.Main>
+        {navigationItems.map((nav) => {
+          return nav?.subItems?.length ? (
+            <Sidebar.Group name={nav.name} key={nav.name}>
+              {nav.subItems?.map((subItem) => (
+                <Sidebar.Item
+                  icon={subItem.icon}
+                  key={subItem.name}
+                  active={subItem.active}
+                  onClick={() => navigate(subItem?.to as string)}
+                  data-test-id={`admin-ui-sidebar-navigation-cell-${subItem.name}`}
+                >
+                  {subItem.name}
+                </Sidebar.Item>
+              ))}
+            </Sidebar.Group>
+          ) : (
+            <Sidebar.Item
+              icon={nav.icon}
+              key={nav.name}
+              active={nav.active}
+              onClick={() => navigate(nav?.to as string)}
+              data-test-id={`admin-ui-sidebar-navigation-cell-${nav.name}`}
+            >
+              {nav.name}
+            </Sidebar.Item>
+          );
+        })}
+      </Sidebar.Main>
+      <Sidebar.Footer>
+        <Sidebar.Item
+          icon={<span />}
+          onClick={logout}
+          data-test-id="frontier-sdk-sidebar-logout"
+        >
+          Logout
+        </Sidebar.Item>
+      </Sidebar.Footer>
+    </Sidebar.Root>
+  );
+}
