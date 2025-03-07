1a39dfefbafdf83215ba088d9e3b93f5c482fdc6
Rishabh <me@rsbh.dev>
2025-03-07 16:25:44 +0530

    feat: add pagination call to the server

--- ui/src/pages/organizations/list/index.tsx
+++ ui/src/pages/organizations/list/index.tsx
@@ -5,7 +5,7 @@ import {
   DataTableQuery,
 } from "@raystack/apsara/v1";
 import { V1Beta1Organization } from "@raystack/frontier";
-
+import { useDebounceCallback } from "usehooks-ts";
 import { useCallback, useEffect, useState } from "react";
 import { OrganizationsNavabar } from "./navbar";
 import OrganizationsIcon from "~/assets/icons/organization.svg?react";
@@ -28,35 +28,46 @@ const NoOrganizations = () => {
 };
 
 const LIMIT = 20;
+const DEFAULT_SORT = { name: "created_at", order: "desc" };
 
 export const OrganizationList = () => {
   const [data, setData] = useState<V1Beta1Organization[]>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [query, setQuery] = useState<DataTableQuery>({});
-  const [offset, setOffset] = useState(0);
+  const [nextOffset, setNextOffset] = useState(0);
 
   const columns = getColumns();
 
-  useEffect(() => {
-    async function fetchOrganizations() {
-      try {
-        setIsLoading(true);
-        const response = await api.adminServiceSearchOrganizations({});
-        const organizations = response.data.organizations || [];
-        setData(organizations);
-      } catch (error) {
-        console.error(error);
-      } finally {
-        setIsLoading(false);
-      }
+  const fetchOrganizations = useCallback(async (apiQuery = {}) => {
+    try {
+      setIsLoading(true);
+      const response = await api.adminServiceSearchOrganizations({
+        limit: LIMIT,
+        ...apiQuery,
+      });
+      const organizations = response.data.organizations || [];
+      setData((prev) => [...prev, ...organizations]);
+      setNextOffset(response.data.pagination?.offset || 0);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
     }
-    fetchOrganizations();
   }, []);
 
-  const onTableQueryChange = useCallback((newQuery: DataTableQuery) => {
-    console.log(newQuery);
+  useEffect(() => {
+    fetchOrganizations({ offset: 0, sort: [DEFAULT_SORT] });
+  }, [fetchOrganizations]);
+
+  async function fetchMoreOrganizations() {
+    fetchOrganizations({ offset: nextOffset + LIMIT, ...query });
+  }
+
+  const onTableQueryChange = useDebounceCallback((newQuery: DataTableQuery) => {
+    setData([]);
+    fetchOrganizations({ ...newQuery, offset: 0 });
     setQuery(newQuery);
-  }, []);
+  }, 500);
 
   const tableClassName =
     data.length || isLoading ? styles["table"] : styles["table-empty"];
@@ -65,9 +76,10 @@ export const OrganizationList = () => {
       columns={columns}
       data={data}
       isLoading={isLoading}
-      defaultSort={{ name: "created_at", order: "desc" }}
+      defaultSort={DEFAULT_SORT as any}
       onTableQueryChange={onTableQueryChange}
       mode="server"
+      onLoadMore={fetchMoreOrganizations}
     >
       <Flex direction="column" style={{ width: "100%" }}>
         <OrganizationsNavabar seachQuery={query.search} />
