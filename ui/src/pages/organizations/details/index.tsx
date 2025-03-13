9338a786dfe70a8aedac2967187c1a14baca53ad
Rishabh <me@rsbh.dev>
2025-03-13 12:35:13 +0530

    feat: add nav chips, actions and icon

--- ui/src/pages/organizations/details/index.tsx
+++ ui/src/pages/organizations/details/index.tsx
@@ -1,11 +1,12 @@
 import { OrganizationsDetailsNavabar } from "./navbar";
 import styles from "./details.module.css";
+import { Flex } from "@raystack/apsara/v1";
 
 export const OrganizationDetails = () => {
   return (
-    <div className={styles.page}>
+    <Flex direction="column" className={styles.page}>
       <OrganizationsDetailsNavabar />
       <p>This is the details page for an organization.</p>
-    </div>
+    </Flex>
   );
 };
