9eda249916ea092dbe703cdf79fa9252028e9950
Rohan Chakraborty <irohan1999@gmail.com>
2025-03-17 11:58:44 +0530

    feat: support for newsletter preference

--- sdks/js/packages/core/react/components/organization/preferences/index.tsx
+++ sdks/js/packages/core/react/components/organization/preferences/index.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useMemo } from 'react';
 import { GearIcon, MoonIcon, SunIcon } from '@radix-ui/react-icons';
 import { Image, Select, Separator, Box } from '@raystack/apsara';
 import { Flex, useTheme, Text } from '@raystack/apsara/v1';
@@ -7,6 +8,8 @@ import bell from '~/react/assets/bell.svg';
 import bellSlash from '~/react/assets/bell-slash.svg';
 import { styles } from '../styles';
 import { PreferencesSelectionTypes } from './preferences.types';
+import Skeleton from 'react-loading-skeleton';
+import { usePreferences } from '~/react/hooks/usePreferences';
 
 const themeOptions = [
   {
@@ -42,7 +45,7 @@ const updateOptions = [
         <Image alt="close" width={16} height={16} src={bell} /> Subscribed
       </Flex>
     ),
-    value: 'subscribed'
+    value: 'true'
   },
   {
     title: (
@@ -52,12 +55,21 @@ const updateOptions = [
         Unsubscribed
       </Flex>
     ),
-    value: 'unsubscribed'
+    value: 'false'
   }
 ];
 
 export default function UserPreferences() {
   const { theme, setTheme } = useTheme();
+  const { preferences, isLoading, isFetching, updatePreferences } =
+    usePreferences();
+
+  const newsletterValue = useMemo(
+    () =>
+      preferences.find(preference => preference.name === 'newsletter')?.value ??
+      'true',
+    [preferences]
+  );
 
   return (
     <Flex direction="column" style={{ width: '100%' }}>
@@ -77,10 +89,14 @@ export default function UserPreferences() {
         <PreferencesSelection
           label="Updates, News & Events"
           text="Stay informed on new features, improvements, and key updates."
-          name="update"
-          defaultValue="subscribed"
+          name="newsletter"
+          defaultValue={newsletterValue}
           values={updateOptions}
-          onSelection={console.log}
+          isLoading={isFetching}
+          disabled={isLoading}
+          onSelection={value => {
+            updatePreferences([{ name: 'newsletter', value }]);
+          }}
         />
         <Separator></Separator>
       </Flex>
@@ -105,6 +121,8 @@ export const PreferencesSelection = ({
   name,
   values,
   defaultValue,
+  isLoading = false,
+  disabled = false,
   onSelection
 }: PreferencesSelectionTypes) => {
   return (
@@ -115,20 +133,29 @@ export const PreferencesSelection = ({
           {text}
         </Text>
       </Flex>
-      <Select onValueChange={onSelection} defaultValue={defaultValue}>
-        <Select.Trigger>
-          <Select.Value placeholder={label} />
-        </Select.Trigger>
-        <Select.Content style={{ minWidth: '120px' }}>
-          <Select.Group>
-            {values.map(v => (
-              <Select.Item key={v.value} value={v.value}>
-                {v.title}
-              </Select.Item>
-            ))}
-          </Select.Group>
-        </Select.Content>
-      </Select>
+      {isLoading ? (
+        <Skeleton width={120} height={32} />
+      ) : (
+        <Select
+          onValueChange={onSelection}
+          defaultValue={defaultValue}
+          disabled={disabled}
+          name={name}
+        >
+          <Select.Trigger>
+            <Select.Value placeholder={label} />
+          </Select.Trigger>
+          <Select.Content style={{ minWidth: '120px' }}>
+            <Select.Group>
+              {values.map(v => (
+                <Select.Item key={v.value} value={v.value}>
+                  {v.title}
+                </Select.Item>
+              ))}
+            </Select.Group>
+          </Select.Content>
+        </Select>
+      )}
     </Flex>
   );
 };
---
+++ sdks/js/packages/core/react/hooks/usePreferences.ts
@@ -0,0 +1,76 @@
+import { useState, useCallback, useEffect } from 'react';
+import { V1Beta1Preference } from '~/api-client';
+import { useFrontier } from '../contexts/FrontierContext';
+
+export interface UsePreferences {
+  preferences: V1Beta1Preference[];
+  isLoading: boolean;
+  isFetching: boolean;
+  status: 'idle' | 'fetching' | 'loading';
+  fetchPreferences: () => Promise<V1Beta1Preference[] | undefined>;
+  updatePreferences: (
+    preferences: V1Beta1Preference[]
+  ) => Promise<V1Beta1Preference[] | undefined>;
+}
+
+export function usePreferences(): UsePreferences {
+  const { client } = useFrontier();
+  const [preferences, setPreferences] = useState<V1Beta1Preference[]>([]);
+  const [status, setStatus] = useState<UsePreferences['status']>('idle');
+
+  const fetchPreferences = useCallback(async () => {
+    try {
+      setStatus('fetching');
+      const response =
+        await client?.frontierServiceListCurrentUserPreferences();
+      const data = response?.data.preferences || [];
+      setPreferences(data);
+      return data;
+    } catch (err) {
+      console.error(
+        'frontier:sdk:: There is problem with fetching user preferences'
+      );
+      console.error(err);
+    } finally {
+      setStatus('idle');
+    }
+    return [];
+  }, [client]);
+
+  const updatePreferences = useCallback(
+    async (preferences: V1Beta1Preference[]) => {
+      try {
+        setStatus('loading');
+        const response =
+          await client?.frontierServiceCreateCurrentUserPreferences({
+            bodies: preferences
+          });
+        const data = response?.data.preferences || [];
+        setPreferences(data);
+        return data;
+      } catch (err) {
+        console.error(
+          'frontier:sdk:: There is problem with updating user preferences'
+        );
+        console.error(err);
+      } finally {
+        setStatus('idle');
+      }
+      return [];
+    },
+    [client]
+  );
+
+  useEffect(() => {
+    if (!preferences.length) fetchPreferences();
+  }, [preferences, fetchPreferences]);
+
+  return {
+    preferences,
+    status,
+    isLoading: status === 'loading',
+    isFetching: status === 'fetching',
+    fetchPreferences,
+    updatePreferences
+  };
+}
