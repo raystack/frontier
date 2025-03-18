81890e48cb883f63c9251e6d57b6e948a205af50
Rishabh <me@rsbh.dev>
2025-03-18 11:54:02 +0530

    fix: org list page link

--- ui/src/pages/organizations/details/navbar.tsx
+++ ui/src/pages/organizations/details/navbar.tsx
@@ -83,7 +83,7 @@ export const OrganizationsDetailsNavabar = ({
           items={[
             {
               label: "Organizations",
-              href: "/organizations",
+              href: "/organisations",
               icon: <OrganizationIcon />,
             },
             {
