7b350f6f2bcd0243bc420901c341f97032870071
Rishabh <me@rsbh.dev>
2025-03-19 16:00:09 +0530

    feat: add block org section

--- ui/src/pages/organizations/details/security/index.tsx
+++ ui/src/pages/organizations/details/security/index.tsx
@@ -19,31 +19,60 @@ const DomainItem = () => {
   );
 };
 
+const BlockOrganizationSection = () => {
+  return (
+    <Flex gap={5} justify="between">
+      <Flex direction="column" gap={3}>
+        <Text size={5}>Block organization</Text>
+        <Text size={3} variant="secondary">
+          Restrict access to safeguard platform integrity and prevent
+          unauthorized activities.
+        </Text>
+      </Flex>
+      <Button
+        color="danger"
+        size="small"
+        data-test-id="block-orgnanization-button"
+      >
+        Block
+      </Button>
+    </Flex>
+  );
+};
+
+const AddDomainSection = () => {
+  return (
+    <Flex gap={5} justify="between">
+      <Flex direction="column" gap={3}>
+        <Text size={5}>Allowed email domains</Text>
+        <Text size={3} variant="secondary">
+          Anyone with an email address at these domains is allowed to sign up
+          for this workspace.
+        </Text>
+      </Flex>
+      <Button
+        variant="outline"
+        color="neutral"
+        leadingIcon={<PlusIcon />}
+        size="small"
+        data-test-id="add-domain-button"
+      >
+        <Text size={1}>Add domain</Text>
+      </Button>
+    </Flex>
+  );
+};
+
 export const OrganizationSecurity = () => {
   return (
     <Flex justify="center" className={styles["container"]}>
       <Flex className={styles["content"]} direction="column" gap={9}>
-        <Flex gap={5} justify="between">
-          <Flex direction="column" gap={3}>
-            <Text size={5}>Allowed email domains</Text>
-            <Text size={3} variant="secondary">
-              Anyone with an email address at these domains is allowed to sign
-              up for this workspace.
-            </Text>
-          </Flex>
-          <Button
-            variant="outline"
-            color="neutral"
-            leadingIcon={<PlusIcon />}
-            size="small"
-            data-test-id="add-domain-button"
-          >
-            <Text size={1}>Add domain</Text>
-          </Button>
-        </Flex>
+        <AddDomainSection />
         <Flex direction="column" className={styles["domains-list"]}>
           <DomainItem />
         </Flex>
+        <Separator />
+        <BlockOrganizationSection />
       </Flex>
     </Flex>
   );
