61525069fddce588568c5def635c9c1517b12f3e
Rishabh <me@rsbh.dev>
2025-03-19 15:43:41 +0530

    feat: add design for security page

--- ui/src/pages/organizations/details/security/index.tsx
+++ ui/src/pages/organizations/details/security/index.tsx
@@ -1,8 +1,50 @@
+import { Button, Flex, IconButton, Separator, Text } from "@raystack/apsara/v1";
+
+import styles from "./security.module.css";
+import { CheckCircledIcon, PlusIcon, TrashIcon } from "@radix-ui/react-icons";
+
+const DomainItem = () => {
+  return (
+    <Flex className={styles["domains-list-item"]} justify="between">
+      <Flex gap={3}>
+        <Text size={3}>raystack.org</Text>
+        <CheckCircledIcon
+          color={"var(--rs-color-foreground-success-primary)"}
+        />
+      </Flex>
+      <IconButton size={3} data-test-id="delete-domain-button">
+        <TrashIcon />
+      </IconButton>
+    </Flex>
+  );
+};
+
 export const OrganizationSecurity = () => {
   return (
-    <div>
-      <h1>Organization Security</h1>
-      <p>This is the security page for the organization.</p>
-    </div>
+    <Flex justify="center" className={styles["container"]}>
+      <Flex className={styles["content"]} direction="column" gap={9}>
+        <Flex gap={5} justify="between">
+          <Flex direction="column" gap={3}>
+            <Text size={5}>Allowed email domains</Text>
+            <Text size={3} variant="secondary">
+              Anyone with an email address at these domains is allowed to sign
+              up for this workspace.
+            </Text>
+          </Flex>
+          <Button
+            variant="outline"
+            color="neutral"
+            leadingIcon={<PlusIcon />}
+            size="small"
+            data-test-id="add-domain-button"
+          >
+            <Text size={1}>Add domain</Text>
+          </Button>
+        </Flex>
+        <Flex direction="column" className={styles["domains-list"]}>
+          <DomainItem />
+        </Flex>
+      </Flex>
+    </Flex>
   );
 };
