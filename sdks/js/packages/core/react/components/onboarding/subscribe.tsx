3ba36de60408b8e50b6e6754f948b3d2520d409c
Rohan Chakraborty <irohan1999@gmail.com>
2025-03-21 16:31:55 +0530

    feat: subscribe for updates module (#910)
    * feat: subscribe for updates module
    
    * fix: resolve comments
    
    * fix: lint error temporarily

---
+++ sdks/js/packages/core/react/components/onboarding/subscribe.tsx
@@ -0,0 +1,112 @@
+'use client';
+
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Button, Flex, Text, Switch } from '@raystack/apsara/v1';
+import { Controller, useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { Container } from '../Container';
+import styles from './onboarding.module.css';
+import { PREFERENCE_OPTIONS } from '~/react/utils/constants';
+import { usePreferences } from '~/react/hooks/usePreferences';
+import { ReactNode } from '@tanstack/react-router';
+import { Header } from '../Header';
+import Skeleton from 'react-loading-skeleton';
+
+const schema = yup.object({
+  [PREFERENCE_OPTIONS.NEWSLETTER]: yup.boolean().optional()
+});
+
+type FormData = yup.InferType<typeof schema>;
+
+type SubscribeProps = {
+  logo?: ReactNode;
+  title?: string;
+  preferenceTitle?: string;
+  preferenceDescription?: string;
+  onSubmit?: (data: FormData) => void;
+};
+
+export const Subscribe = ({
+  logo,
+  title = 'Subscribe for updates',
+  preferenceTitle = 'Updates, News & Events',
+  preferenceDescription = 'Stay informed on new features, improvements, and key updates',
+  onSubmit
+}: SubscribeProps) => {
+  const { preferences, isFetching, updatePreferences } = usePreferences();
+
+  const newsletterValue =
+    preferences?.[PREFERENCE_OPTIONS.NEWSLETTER]?.value === 'true';
+
+  const {
+    control,
+    handleSubmit,
+    formState: { isSubmitting }
+  } = useForm<FormData>({
+    values: {
+      [PREFERENCE_OPTIONS.NEWSLETTER]: newsletterValue
+    },
+    resolver: yupResolver(schema)
+  });
+
+  async function onFormSubmit(data: FormData) {
+    return updatePreferences([
+      {
+        name: PREFERENCE_OPTIONS.NEWSLETTER,
+        value: String(data[PREFERENCE_OPTIONS.NEWSLETTER])
+      }
+    ])
+      .then(() => onSubmit?.(data))
+      .catch(err => {
+        console.error('frontier:sdk:: error during submit', err);
+      });
+  }
+  return (
+    <Flex direction="column" gap="large">
+      <Header logo={logo} title={title} />
+      <form onSubmit={handleSubmit(onFormSubmit)}>
+        <Container
+          className={styles.subscribeContainer}
+          shadow="sm"
+          radius="xs"
+        >
+          <Flex direction="column" gap="medium">
+            <Flex justify="between">
+              <Text size={6} weight={500}>
+                {preferenceTitle}
+              </Text>
+              {isFetching ? (
+                <Skeleton width={34} height={20} />
+              ) : (
+                <Controller
+                  render={({ field: { value, onChange, ...field } }) => (
+                    <Switch
+                      checked={value}
+                      onCheckedChange={onChange}
+                      {...field}
+                    />
+                  )}
+                  control={control}
+                  name={PREFERENCE_OPTIONS.NEWSLETTER}
+                />
+              )}
+            </Flex>
+            <Text size={4} style={{ color: 'var(--foreground-muted)' }}>
+              {preferenceDescription}
+            </Text>
+          </Flex>
+          <Button
+            style={{ width: '100%' }}
+            type="submit"
+            data-test-id="frontier-sdk-subscribe-btn"
+            disabled={isFetching || isSubmitting}
+            loading={isSubmitting}
+            loaderText="Loading..."
+          >
+            Continue
+          </Button>
+        </Container>
+      </form>
+    </Flex>
+  );
+};
