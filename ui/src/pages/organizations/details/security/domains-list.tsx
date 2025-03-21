72dbcb4274761c7990a506b07c0fb32bb2f7f674
Rishabh <me@rsbh.dev>
2025-03-21 10:37:07 +0530

    feat: add domain delete api

--- ui/src/pages/organizations/details/security/domains-list.tsx
+++ ui/src/pages/organizations/details/security/domains-list.tsx
@@ -1,14 +1,95 @@
-import { Flex, IconButton, Text } from "@raystack/apsara/v1";
+import {
+  Button,
+  Dialog,
+  Flex,
+  IconButton,
+  Text,
+  toast,
+} from "@raystack/apsara/v1";
 import { V1Beta1Domain } from "~/api/frontier";
 import styles from "./security.module.css";
 import { CheckCircledIcon, TrashIcon } from "@radix-ui/react-icons";
 import Skeleton from "react-loading-skeleton";
+import { useState } from "react";
+import { api } from "~/api";
 
-interface DomainItemProps {
+interface DeleteDomainDialogProps {
+  fetchDomains: () => Promise<void>;
   domain: V1Beta1Domain;
 }
 
-const DomainItem = ({ domain }: DomainItemProps) => {
+const DeleteDomainDialog = ({
+  fetchDomains,
+  domain,
+}: DeleteDomainDialogProps) => {
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const handleDelete = async () => {
+    setIsDeleting(true);
+    try {
+      await api?.frontierServiceDeleteOrganizationDomain(
+        domain?.org_id || "",
+        domain?.id || "",
+      );
+      await fetchDomains();
+      setIsDialogOpen(false);
+    } catch (error) {
+      toast.error("unable to delete domain");
+    } finally {
+      setIsDeleting(false);
+    }
+  };
+
+  const onOpenChange = (value: boolean) => {
+    setIsDialogOpen(value);
+  };
+
+  return (
+    <Dialog open={isDialogOpen} onOpenChange={onOpenChange}>
+      <Dialog.Trigger asChild>
+        <IconButton size={3} data-test-id="delete-domain-button">
+          <TrashIcon />
+        </IconButton>
+      </Dialog.Trigger>
+      <Dialog.Content width={400}>
+        <Dialog.Body>
+          <Dialog.Title>Delete email domain</Dialog.Title>
+          <Dialog.Description>
+            Are you sure you want to delete this email domain?
+          </Dialog.Description>
+        </Dialog.Body>
+        <Dialog.Footer>
+          <Dialog.Close>
+            <Button
+              color="neutral"
+              variant="outline"
+              data-test-id="delete-domain-cancel-button"
+            >
+              Cancel
+            </Button>
+          </Dialog.Close>
+          <Button
+            color={"danger"}
+            data-test-id="delete-domain-submit-button"
+            loading={isDeleting}
+            loaderText="Deleting..."
+            onClick={handleDelete}
+          >
+            Delete
+          </Button>
+        </Dialog.Footer>
+      </Dialog.Content>
+    </Dialog>
+  );
+};
+
+interface DomainItemProps {
+  domain: V1Beta1Domain;
+  fetchDomains: () => Promise<void>;
+}
+
+const DomainItem = ({ fetchDomains, domain }: DomainItemProps) => {
   return (
     <Flex className={styles["domains-list-item"]} justify="between">
       <Flex gap={3}>
@@ -19,9 +100,7 @@ const DomainItem = ({ domain }: DomainItemProps) => {
           />
         ) : null}
       </Flex>
-      <IconButton size={3} data-test-id="delete-domain-button">
-        <TrashIcon />
-      </IconButton>
+      <DeleteDomainDialog fetchDomains={fetchDomains} domain={domain} />
     </Flex>
   );
 };
@@ -29,10 +108,14 @@ const DomainItem = ({ domain }: DomainItemProps) => {
 interface DomainsListProps {
   isLoading: boolean;
   domains: V1Beta1Domain[];
-  fetchDomains: () => void;
+  fetchDomains: () => Promise<void>;
 }
 
-export const DomainsList = ({ isLoading, domains = [] }: DomainsListProps) => {
+export const DomainsList = ({
+  isLoading,
+  domains = [],
+  fetchDomains,
+}: DomainsListProps) => {
   return isLoading ? (
     <Flex direction="column" className={styles["domains-list"]}>
       {[...Array(3)].map((_, index) => (
@@ -46,7 +129,11 @@ export const DomainsList = ({ isLoading, domains = [] }: DomainsListProps) => {
   ) : domains.length ? (
     <Flex direction="column" className={styles["domains-list"]}>
       {domains.map((domain) => (
-        <DomainItem key={domain?.id} domain={domain} />
+        <DomainItem
+          key={domain?.id}
+          domain={domain}
+          fetchDomains={fetchDomains}
+        />
       ))}
     </Flex>
   ) : null;
