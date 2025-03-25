33ccafdc8dbd96f3c6ac8349a73b6623d5fb1ab0
Rishabh <me@rsbh.dev>
2025-03-25 09:41:03 +0530

    feat: call org member search api on page mount

---
+++ ui/src/pages/organizations/details/members/columns.tsx
@@ -0,0 +1,9 @@
+import { DataTableColumnDef } from "@raystack/apsara/v1";
+import { SearchOrganizationUsersResponseOrganizationUser } from "~/api/frontier";
+
+export const getColumns = (): DataTableColumnDef<
+  SearchOrganizationUsersResponseOrganizationUser,
+  unknown
+>[] => {
+  return [];
+};
--- ui/src/pages/organizations/details/members/index.tsx
+++ ui/src/pages/organizations/details/members/index.tsx
@@ -1,18 +1,71 @@
 import { useOutletContext } from "react-router-dom";
 import { OutletContext } from "../types";
-import { Flex } from "@raystack/apsara/v1";
+import {
+  DataTable,
+  DataTableQuery,
+  DataTableSort,
+  Flex,
+} from "@raystack/apsara/v1";
 import PageTitle from "~/components/page-title";
 import styles from "./members.module.css";
+import { useCallback, useEffect, useState } from "react";
+import { api } from "~/api";
+import { getColumns } from "./columns";
+import { SearchOrganizationUsersResponseOrganizationUser } from "~/api/frontier";
+
+const DEFAULT_SORT: DataTableSort = { name: "org_joined_at", order: "desc" };
 
 export function OrganizationMembersPage() {
   const { organization } = useOutletContext<OutletContext>();
+  const organizationId = organization.id || "";
+
+  const [data, setData] = useState<
+    SearchOrganizationUsersResponseOrganizationUser[]
+  >([]);
+  const [isDataLoading, setIsDataLoading] = useState(false);
 
   const title = `Members | ${organization.title} | Organizations`;
+
+  const fetchMembers = useCallback(
+    async (apiQuery: DataTableQuery = {}) => {
+      try {
+        setIsDataLoading(true);
+        const response = await api?.adminServiceSearchOrganizationUsers(
+          organizationId,
+          apiQuery,
+        );
+        const members = response.data.org_users || [];
+        setData((prev) => [...prev, ...members]);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsDataLoading(false);
+      }
+    },
+    [organizationId],
+  );
+
+  useEffect(() => {
+    fetchMembers({ offset: 0, sort: [DEFAULT_SORT] });
+  }, [fetchMembers]);
+
+  const columns = getColumns();
 
   return (
     <Flex justify="center" className={styles["container"]}>
       <PageTitle title={title} />
-      <Flex className={styles["content"]} direction="column" gap={9}></Flex>
+      <DataTable
+        columns={columns}
+        data={data}
+        isLoading={isDataLoading}
+        defaultSort={DEFAULT_SORT}
+        mode="server"
+      >
+        <Flex direction="column" style={{ width: "100%" }}>
+          <DataTable.Toolbar />
+          <DataTable.Content />
+        </Flex>
+      </DataTable>
     </Flex>
   );
 }
