f22dcd77f8e96999f506d4503ffd2c47b0e9271e
Rishabh <me@rsbh.dev>
2025-04-01 14:47:18 +0530

    feat: fetch member list

--- ui/src/pages/organizations/details/members/index.tsx
+++ ui/src/pages/organizations/details/members/index.tsx
@@ -9,12 +9,14 @@ import {
 } from "@raystack/apsara/v1";
 import PageTitle from "~/components/page-title";
 import styles from "./members.module.css";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 import { api } from "~/api";
 import { getColumns } from "./columns";
 import { SearchOrganizationUsersResponseOrganizationUser } from "~/api/frontier";
 import UserIcon from "~/assets/icons/users.svg?react";
+import { useDebounceCallback } from "usehooks-ts";
 
+const LIMIT = 50;
 const DEFAULT_SORT: DataTableSort = { name: "org_joined_at", order: "desc" };
 
 const NoMembers = () => {
@@ -39,32 +41,50 @@ export function OrganizationMembersPage() {
     SearchOrganizationUsersResponseOrganizationUser[]
   >([]);
   const [isDataLoading, setIsDataLoading] = useState(false);
+  const [query, setQuery] = useState<DataTableQuery>({
+    offset: 0,
+    sort: [DEFAULT_SORT],
+  });
+  const [nextOffset, setNextOffset] = useState(0);
+  const [hasMoreData, setHasMoreData] = useState(true);
 
   const title = `Members | ${organization.title} | Organizations`;
 
   const fetchMembers = useCallback(
-    async (apiQuery: DataTableQuery = {}) => {
-      if (!organizationId) return;
+    async (org_id: string, apiQuery: DataTableQuery = {}) => {
       try {
         setIsDataLoading(true);
         const response = await api?.adminServiceSearchOrganizationUsers(
-          organizationId,
-          apiQuery,
+          org_id,
+          { ...apiQuery, limit: LIMIT },
         );
         const members = response.data.org_users || [];
-        setData((prev) => [...prev, ...members]);
+        setData((prev) => {
+          return [...prev, ...members];
+        });
+        setNextOffset(response.data.pagination?.offset || 0);
+        setHasMoreData(members.length !== 0 && members.length === LIMIT);
       } catch (error) {
         console.error(error);
       } finally {
         setIsDataLoading(false);
       }
     },
-    [organizationId],
+    [],
   );
 
-  useEffect(() => {
-    fetchMembers({ offset: 0, sort: [DEFAULT_SORT] });
-  }, [fetchMembers]);
+  async function fetchMoreMembers() {
+    if (isDataLoading || !hasMoreData || !organizationId) {
+      return;
+    }
+    fetchMembers(organizationId, { offset: nextOffset + LIMIT, ...query });
+  }
+
+  const onTableQueryChange = useDebounceCallback((newQuery: DataTableQuery) => {
+    setData([]);
+    fetchMembers(organizationId, { ...newQuery, offset: 0 });
+    setQuery(newQuery);
+  }, 500);
 
   const columns = getColumns();
 
@@ -77,10 +97,18 @@ export function OrganizationMembersPage() {
         isLoading={isDataLoading}
         defaultSort={DEFAULT_SORT}
         mode="server"
+        onTableQueryChange={onTableQueryChange}
+        onLoadMore={fetchMoreMembers}
       >
         <Flex direction="column" style={{ width: "100%" }}>
           <DataTable.Toolbar />
-          <DataTable.Content emptyState={<NoMembers />} />
+          <DataTable.Content
+            emptyState={<NoMembers />}
+            classNames={{
+              table: styles["table"],
+              root: styles["table-wrapper"],
+            }}
+          />
         </Flex>
       </DataTable>
     </Flex>
