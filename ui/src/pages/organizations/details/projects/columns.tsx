da108e28ef4a2d5273e3a425e5eeb95fac4072d3
Rishabh <me@rsbh.dev>
2025-04-08 13:51:57 +0530

    feat: add page for org projects

---
+++ ui/src/pages/organizations/details/projects/columns.tsx
@@ -0,0 +1,65 @@
+import { AvatarGroup, DataTableColumnDef } from "@raystack/apsara/v1";
+import { SearchOrganizationProjectsResponseOrganizationProject } from "~/api/frontier";
+import styles from "./projects.module.css";
+import { Avatar, Flex, Text } from "@raystack/apsara/v1";
+import dayjs from "dayjs";
+import { NULL_DATE } from "~/utils/constants";
+
+export const getColumns = (): DataTableColumnDef<
+  SearchOrganizationProjectsResponseOrganizationProject,
+  unknown
+>[] => {
+  return [
+    {
+      accessorKey: "title",
+      header: "Title",
+      classNames: {
+        cell: styles["title-column"],
+        header: styles["title-column"],
+      },
+      cell: ({ row }) => {
+        return (
+          <Flex gap={4} align="center">
+            <Text>{row.original.title || "-"}</Text>
+          </Flex>
+        );
+      },
+      enableColumnFilter: true,
+      enableSorting: true,
+    },
+    {
+      accessorKey: "name",
+      header: "Name",
+      cell: ({ getValue }) => {
+        return getValue();
+      },
+      enableColumnFilter: true,
+    },
+    {
+      accessorKey: "user_ids",
+      header: "Members",
+      cell: ({ getValue }) => {
+        const user_ids = (getValue() as string[]) || [];
+        return (
+          <AvatarGroup max={4}>
+            {user_ids.map((id: string) => (
+              <Avatar key={id} fallback={id} radius="full" />
+            ))}
+          </AvatarGroup>
+        );
+      },
+    },
+    {
+      accessorKey: "created_at",
+      header: "Created On",
+      cell: ({ getValue }) => {
+        const value = getValue() as string;
+        return value !== NULL_DATE ? dayjs(value).format("YYYY-MM-DD") : "-";
+      },
+      enableSorting: true,
+      enableHiding: true,
+      enableColumnFilter: true,
+      filterType: "date",
+    },
+  ];
+};
---
+++ ui/src/pages/organizations/details/projects/index.tsx
@@ -0,0 +1,131 @@
+import {
+  DataTable,
+  DataTableQuery,
+  DataTableSort,
+  EmptyState,
+  Flex,
+} from "@raystack/apsara/v1";
+import PageTitle from "~/components/page-title";
+import styles from "./projects.module.css";
+import { useCallback, useContext, useEffect, useState } from "react";
+import { api } from "~/api";
+import { getColumns } from "./columns";
+import { SearchOrganizationProjectsResponseOrganizationProject } from "~/api/frontier";
+import { useDebounceCallback } from "usehooks-ts";
+import { OrganizationContext } from "../contexts/organization-context";
+import { FileIcon } from "@radix-ui/react-icons";
+
+const LIMIT = 50;
+const DEFAULT_SORT: DataTableSort = { name: "created_at", order: "desc" };
+
+const NoProjects = () => {
+  return (
+    <EmptyState
+      classNames={{
+        container: styles["empty-state"],
+        subHeading: styles["empty-state-subheading"],
+      }}
+      heading="No Projects found"
+      subHeading="We couldn’t find any matches for that keyword or filter. Try alternative terms or check for typos."
+      icon={<FileIcon />}
+    />
+  );
+};
+
+export function OrganizationProjectssPage() {
+  const { organization, search } = useContext(OrganizationContext);
+  const {
+    onChange: onSearchChange,
+    setVisibility: setSearchVisibility,
+    query: searchQuery,
+  } = search;
+
+  const organizationId = organization?.id || "";
+
+  const [data, setData] = useState<
+    SearchOrganizationProjectsResponseOrganizationProject[]
+  >([]);
+  const [isDataLoading, setIsDataLoading] = useState(false);
+  const [query, setQuery] = useState<DataTableQuery>({
+    offset: 0,
+    sort: [DEFAULT_SORT],
+  });
+  const [nextOffset, setNextOffset] = useState(0);
+  const [hasMoreData, setHasMoreData] = useState(true);
+
+  const title = `Projects | ${organization?.title} | Organizations`;
+
+  const fetchProjects = useCallback(
+    async (org_id: string, apiQuery: DataTableQuery = {}) => {
+      try {
+        setIsDataLoading(true);
+        const response = await api?.adminServiceSearchOrganizationProjects(
+          org_id,
+          { ...apiQuery, limit: LIMIT, search: search?.query || "" },
+        );
+        const data = response.data.org_projects || [];
+        setData((prev) => {
+          return [...prev, ...data];
+        });
+        setNextOffset(response.data.pagination?.offset || 0);
+        setHasMoreData(data.length !== 0 && data.length === LIMIT);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsDataLoading(false);
+      }
+    },
+    [search?.query],
+  );
+
+  async function fetchMoreProjects() {
+    if (isDataLoading || !hasMoreData || !organizationId) {
+      return;
+    }
+    fetchProjects(organizationId, { offset: nextOffset + LIMIT, ...query });
+  }
+
+  const onTableQueryChange = useDebounceCallback((newQuery: DataTableQuery) => {
+    setData([]);
+    fetchProjects(organizationId, { ...newQuery, offset: 0 });
+    setQuery(newQuery);
+  }, 500);
+
+  useEffect(() => {
+    setSearchVisibility(true);
+    return () => {
+      onSearchChange("");
+      setSearchVisibility(false);
+    };
+  }, [setSearchVisibility, onSearchChange]);
+
+  const columns = getColumns();
+
+  return (
+    <Flex justify="center" className={styles["container"]}>
+      <PageTitle title={title} />
+      <DataTable
+        columns={columns}
+        data={data}
+        isLoading={isDataLoading}
+        defaultSort={DEFAULT_SORT}
+        mode="server"
+        onTableQueryChange={onTableQueryChange}
+        onLoadMore={fetchMoreProjects}
+        query={{ ...query, search: searchQuery }}
+      >
+        <Flex direction="column" style={{ width: "100%" }}>
+          <DataTable.Toolbar />
+          <DataTable.Content
+            emptyState={<NoProjects />}
+            classNames={{
+              table: styles["table"],
+              root: styles["table-wrapper"],
+              header: styles["table-header"],
+            }}
+          />
+        </Flex>
+      </DataTable>
+    </Flex>
+  );
+}
