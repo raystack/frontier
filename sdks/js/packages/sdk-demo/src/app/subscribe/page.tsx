282a4e01c8054d272556e2f02549297d3a06fa85
paanSinghCoder <gaurav3017@gmail.com>
2025-04-10 07:52:05 +0530

    chore: add alert

--- sdks/js/packages/sdk-demo/src/app/subscribe/page.tsx
+++ sdks/js/packages/sdk-demo/src/app/subscribe/page.tsx
@@ -10,7 +10,7 @@ export default function SubscribeRoute() {
       align="center"
       style={{ height: '100vh', width: '100vw' }}
     >
-      <Subscribe  />
+      <Subscribe onSubmit={data => alert(JSON.stringify(data))} />
     </Flex>
   );
 }
