097f32fb46dc590a9b15a6ac68fb3e66d4f89e93
paanSinghCoder <gaurav3017@gmail.com>
2025-04-10 08:41:03 +0530

    chore: remove status field

--- sdks/js/packages/core/react/components/onboarding/subscribe.tsx
+++ sdks/js/packages/core/react/components/onboarding/subscribe.tsx
@@ -27,7 +27,6 @@ type FormData = yup.InferType<typeof schema>;
 
 interface ExtendedFormData extends FormData {
   activity: string;
-  status: string;
   source?: string;
   metadata?: {
     medium?: string;
@@ -53,7 +52,6 @@ export const Subscribe = ({
   const [title, setTitle] = useState(defaultTitle);
   const [description, setDescription] = useState(defaultDescription);
   const [activity, setActivity] = useState('');
-  const [status, setStatus] = useState('');
   const [medium, setMedium] = useState<string | null>(null);
   const [source, setSource] = useState<string | null>(null);
 
@@ -62,14 +60,12 @@ export const Subscribe = ({
     const titleFromQuery = searchParams.get('title');
     const descriptionFromQuery = searchParams.get('desc');
     const activityFromQuery = searchParams.get('activity') || '';
-    const statusFromQuery = searchParams.get('status') || 'subscribed';
     const utmMedium = searchParams.get('utm_medium');
     const utmSource = searchParams.get('utm_source');
 
     if (titleFromQuery) setTitle(decodeURIComponent(titleFromQuery));
     if (descriptionFromQuery) setDescription(decodeURIComponent(descriptionFromQuery));
     if (activityFromQuery) setActivity(decodeURIComponent(activityFromQuery));
-    if (statusFromQuery) setStatus(decodeURIComponent(statusFromQuery));
     if (utmMedium) setMedium(decodeURIComponent(utmMedium));
     if (utmSource) setSource(decodeURIComponent(utmSource));
   }, []);
@@ -84,7 +80,7 @@ export const Subscribe = ({
 
   async function onFormSubmit(data: FormData) {
     try {
-      const formData: ExtendedFormData = { ...data, activity, status };
+      const formData: ExtendedFormData = { ...data, activity };
       if (medium) {
         formData.metadata = { ...formData.metadata, medium };
       }
