0f3ac48fd6a81d663bb12b778f2daf0b6b4a39a8
Rishabh <me@rsbh.dev>
2025-04-21 14:05:00 +0530

    feat: integrate invoice search api

--- ui/src/pages/organizations/details/invoices/index.tsx
+++ ui/src/pages/organizations/details/invoices/index.tsx
@@ -1,9 +1,19 @@
-import { EmptyState, Flex } from "@raystack/apsara/v1";
+import {
+  DataTable,
+  DataTableQuery,
+  DataTableSort,
+  EmptyState,
+  Flex,
+} from "@raystack/apsara/v1";
 import styles from "./invoices.module.css";
 import { FileTextIcon } from "@radix-ui/react-icons";
-import { useContext } from "react";
+import { useCallback, useContext, useEffect, useState } from "react";
 import { OrganizationContext } from "../contexts/organization-context";
 import PageTitle from "~/components/page-title";
+import { getColumns } from "./columns";
+import { api } from "~/api";
+import { SearchOrganizationInvoicesResponseOrganizationInvoice } from "~/api/frontier";
+import { useDebounceCallback } from "usehooks-ts";
 
 const NoInvoices = () => {
   return (
@@ -20,14 +30,102 @@ const NoInvoices = () => {
   );
 };
 
+const LIMIT = 50;
+const DEFAULT_SORT: DataTableSort = { name: "created_at", order: "desc" };
+
 export function OrganizationInvoicesPage() {
-  const { organization } = useContext(OrganizationContext);
+  const { organization, search } = useContext(OrganizationContext);
+  const organizationId = organization?.id || "";
+
+  const {
+    onChange: onSearchChange,
+    setVisibility: setSearchVisibility,
+    query: searchQuery,
+  } = search;
 
   const title = `Invoices | ${organization?.title} | Organizations`;
 
+  const [query, setQuery] = useState<DataTableQuery>({
+    offset: 0,
+  });
+
+  const [isDataLoading, setIsDataLoading] = useState(false);
+  const [data, setData] = useState<
+    SearchOrganizationInvoicesResponseOrganizationInvoice[]
+  >([]);
+  const [nextOffset, setNextOffset] = useState(0);
+  const [hasMoreData, setHasMoreData] = useState(true);
+
+  const fetchInvoices = useCallback(
+    async (org_id: string, apiQuery: DataTableQuery = {}) => {
+      try {
+        setIsDataLoading(true);
+        const response = await api?.adminServiceSearchOrganizationInvoices(
+          org_id,
+          { ...apiQuery, limit: LIMIT, search: search?.query || "" },
+        );
+        const invoices = response.data.organization_invoices || [];
+        setData((prev) => {
+          return [...prev, ...invoices];
+        });
+        setNextOffset(response.data.pagination?.offset || 0);
+        setHasMoreData(invoices.length !== 0 && invoices.length === LIMIT);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsDataLoading(false);
+      }
+    },
+    [search?.query],
+  );
+
+  async function fetchMoreInvoices() {
+    if (isDataLoading || !hasMoreData || !organizationId) {
+      return;
+    }
+    fetchInvoices(organizationId, { ...query, offset: nextOffset + LIMIT });
+  }
+
+  useEffect(() => {
+    setSearchVisibility(true);
+    return () => {
+      onSearchChange("");
+      setSearchVisibility(false);
+    };
+  }, [setSearchVisibility, onSearchChange]);
+
+  const onTableQueryChange = useDebounceCallback((newQuery: DataTableQuery) => {
+    setData([]);
+    fetchInvoices(organizationId, { ...newQuery, offset: 0 });
+    setQuery(newQuery);
+  }, 500);
+
+  const columns = getColumns();
   return (
     <Flex justify="center" className={styles["container"]}>
       <PageTitle title={title} />
+      <DataTable
+        columns={columns}
+        data={data}
+        isLoading={isDataLoading}
+        defaultSort={DEFAULT_SORT}
+        mode="server"
+        onTableQueryChange={onTableQueryChange}
+        onLoadMore={fetchMoreInvoices}
+        query={{ ...query, search: searchQuery }}
+      >
+        <Flex direction="column" style={{ width: "100%" }}>
+          <DataTable.Toolbar />
+          <DataTable.Content
+            emptyState={<NoInvoices />}
+            classNames={{
+              table: styles["table"],
+              root: styles["table-wrapper"],
+              header: styles["table-header"],
+            }}
+          />
+        </Flex>
+      </DataTable>
     </Flex>
   );
 }
