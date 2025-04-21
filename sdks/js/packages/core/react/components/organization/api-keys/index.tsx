df2533eada7eaca0e3ee496e8e83b02d6373e093
Rishabh Mishra <me@rsbh.dev>
2025-04-21 16:27:31 +0530

    fix(sdk): pass config to NoServiceAccounts screen for app name (#961)

--- sdks/js/packages/core/react/components/organization/api-keys/index.tsx
+++ sdks/js/packages/core/react/components/organization/api-keys/index.tsx
@@ -229,7 +229,7 @@ export default function ApiKeys() {
               />
             </Flex>
           ) : (
-            <NoServiceAccounts />
+            <NoServiceAccounts config={config?.apiPlatform} />
           )
         ) : (
           <NoAccess />
