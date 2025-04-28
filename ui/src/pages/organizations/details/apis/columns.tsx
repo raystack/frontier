077e52c86e43bb587b90964bd422f767d66ca150
Rishabh <me@rsbh.dev>
2025-04-28 11:45:01 +0530

    feat: add org service user keys page

---
+++ ui/src/pages/organizations/details/apis/columns.tsx
@@ -0,0 +1,51 @@
+import { DataTableColumnDef } from "@raystack/apsara/v1";
+import dayjs from "dayjs";
+import { SearchOrganizationServiceUserCredentialsResponseOrganizationServiceUserCredential } from "~/api/frontier";
+import { NULL_DATE } from "~/utils/constants";
+import styles from "./apis.module.css";
+
+interface ColumnOptions {
+  groupCountMap: Record<string, Record<string, number>>;
+}
+
+export function getColumns(
+  options: ColumnOptions,
+): DataTableColumnDef<
+  SearchOrganizationServiceUserCredentialsResponseOrganizationServiceUserCredential,
+  unknown
+>[] {
+  return [
+    {
+      accessorKey: "title",
+      header: "Keys",
+      classNames: {
+        cell: styles["first-column"],
+        header: styles["first-column"],
+      },
+      cell: ({ getValue }) => {
+        const value = getValue() as string;
+        return <>{value}</>;
+      },
+    },
+    {
+      accessorKey: "serviceuser_title",
+      header: "Account",
+      cell: ({ getValue }) => {
+        const value = getValue() as string;
+        return <>{value}</>;
+      },
+    },
+    {
+      accessorKey: "created_at",
+      header: "Created on",
+      cell: ({ getValue }) => {
+        const value = getValue() as string;
+        return value !== NULL_DATE ? dayjs(value).format("YYYY-MM-DD") : "-";
+      },
+      enableSorting: true,
+      enableColumnFilter: true,
+      filterType: "date",
+      enableHiding: true,
+    },
+  ];
+}
