44f6ef71456d2acb188a1f2457c2156301b70c08
Rishabh <me@rsbh.dev>
2025-05-12 13:26:41 +0530

    chore: remove unused files

--- ui/src/containers/organisations.list/types.tsx
+++
@@ -1,9 +0,0 @@
-import { V1Beta1Organization } from "@raystack/frontier";
-
-export type OrganisationListTypes = {
-  organisations: V1Beta1Organization[];
-};
-
-export type OrganisationDetailsTypes = {
-  organisation: V1Beta1Organization;
-};
