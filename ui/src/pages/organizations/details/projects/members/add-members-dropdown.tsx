20c8bb4eb52e6248acbf3e6bed7d34dfc57e356d
Rishabh <me@rsbh.dev>
2025-05-19 11:08:22 +0530

    feat: add member from project member dropdown

--- ui/src/pages/organizations/details/projects/members/add-members-dropdown.tsx
+++ ui/src/pages/organizations/details/projects/members/add-members-dropdown.tsx
@@ -6,15 +6,14 @@ import {
   getAvatarColor,
   Text,
 } from "@raystack/apsara/v1";
-import { useCallback, useContext, useEffect, useMemo, useState } from "react";
+import type React from "react";
 import Skeleton from "react-loading-skeleton";
 import styles from "./members.module.css";
-import { api } from "~/api";
-import { OrganizationContext } from "../../contexts/organization-context";
-import { V1Beta1User } from "@raystack/frontier";
+import { useAddProjectMembers } from "../useAddProjectMembers";
 
 interface AddMembersDropdownProps {
   projectId: string;
+  refetchMembers: () => Promise<void>;
 }
 
 function Loader() {
@@ -29,70 +28,56 @@ function Loader() {
   );
 }
 
-export function AddMembersDropdown({ projectId }: AddMembersDropdownProps) {
-  const { orgMembersMap } = useContext(OrganizationContext);
+export function AddMembersDropdown({
+  projectId,
+  refetchMembers,
+}: AddMembersDropdownProps) {
+  const { eligibleMembers, isLoading, addMember, setSearchQuery } =
+    useAddProjectMembers({
+      projectId: projectId,
+    });
 
-  const [projectMembersSet, setProjectMembersSet] = useState<Set<string>>(
-    new Set(),
-  );
-  const [isProjectMembersLoading, setIsProjectMembersLoading] = useState(true);
-
-  const fetchMembers = useCallback(async () => {
-    try {
-      setIsProjectMembersLoading(true);
-      const response = await api?.frontierServiceListProjectUsers(projectId);
-      const members = response?.data?.users || [];
-      const memberSet = new Set(members.map((member) => member.id || ""));
-      setProjectMembersSet(memberSet);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setIsProjectMembersLoading(false);
-    }
-  }, [projectId]);
-
-  useEffect(() => {
-    fetchMembers();
-  }, [fetchMembers]);
-
-  const eligibleUsers = useMemo(() => {
-    return Object.values(orgMembersMap).reduce((acc, member) => {
-      if (member.id && !projectMembersSet.has(member.id)) {
-        acc.push(member);
-      }
-      return acc;
-    }, [] as V1Beta1User[]);
-  }, [orgMembersMap, projectMembersSet]);
-
-  const topUsers = useMemo(() => {
-    return eligibleUsers.slice(0, 7);
-  }, [eligibleUsers]);
+  function onAddMember(userId: string) {
+    return async (e: React.MouseEvent<HTMLDivElement>) => {
+      e.stopPropagation();
+      await addMember(userId);
+      refetchMembers();
+    };
+  }
 
   return (
-    <DropdownMenu open autocomplete>
+    <DropdownMenu
+      autocomplete
+      placement="bottom-end"
+      onSearch={setSearchQuery}
+      autocompleteMode="manual"
+    >
       <DropdownMenu.Trigger asChild>
         <Button data-test-id="add-project-member-btn">Add member</Button>
       </DropdownMenu.Trigger>
-      <DropdownMenu.Content align="end">
-        {isProjectMembersLoading ? (
+      <DropdownMenu.Content>
+        {isLoading ? (
           <Loader />
         ) : (
-          topUsers.map((user) => {
-            const nameInitial = user.title?.[0] || user?.email?.[0];
-            const avatarColor = getAvatarColor(user?.id || "");
-            return (
-              <DropdownMenu.Item key={user.id}>
+          <>
+            {eligibleMembers?.slice(0, 5).map((user) => (
+              <DropdownMenu.Item
+                key={user.id}
+                onClick={onAddMember(user?.id || "")}
+                data-test-id={`admin-ui-add-member-${user.id}`}
+              >
                 <Flex gap={4} align="center">
                   <Avatar
                     src={user.avatar}
-                    fallback={nameInitial}
-                    color={avatarColor}
+                    fallback={user?.title?.[0] || user?.email?.[0]}
+                    radius="full"
+                    color={getAvatarColor(user.id || "")}
                   />
-                  <Text>{user.title || "-"}</Text>
+                  <Text>{user.title || user.email}</Text>
                 </Flex>
               </DropdownMenu.Item>
-            );
-          })
+            ))}
+          </>
         )}
       </DropdownMenu.Content>
     </DropdownMenu>
