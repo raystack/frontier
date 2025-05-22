1f9df6541d0ac933e395815edf1a302b398a4a01
Rohan Chakraborty <irohan1999@gmail.com>
2025-05-22 11:28:45 +0530

    fix: use config name

--- ui/src/pages/users/details/audit-log/audit-log.tsx
+++ ui/src/pages/users/details/audit-log/audit-log.tsx
@@ -1,10 +1,13 @@
 import { EmptyState, Flex, Button } from "@raystack/apsara/v1";
 import PageTitle from "~/components/page-title";
 import ChipIcon from "~/assets/icons/cpu-chip.svg?react";
+import { useAppContext } from "~/contexts/App";
 import { useUser } from "../user-context";
 
 export const UserDetailsAuditLogPage = () => {
   const user = useUser();
+  const { config } = useAppContext();
+
   const title = `Audit Log | ${user?.email} | Users`;
 
   return (
@@ -13,7 +16,7 @@ export const UserDetailsAuditLogPage = () => {
       <EmptyState
         variant="empty2"
         heading="Audit log"
-        subHeading="The audit log in Aurora provides a detailed record of all key actions taken within the platform. Track user activity, monitor changes, and ensure security and compliance with a transparent activity log"
+        subHeading={`The audit log in ${config.title} provides a detailed record of all key actions taken within the platform. Track user activity, monitor changes, and ensure security and compliance with a transparent activity log`}
         icon={<ChipIcon />}
         primaryAction={
           <Button disabled color="neutral" variant="outline">
