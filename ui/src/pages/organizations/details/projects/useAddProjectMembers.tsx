c56295c5722f056f708276a4394aa6475bd96aa3
Rishabh Mishra <me@rsbh.dev>
2025-05-22 11:23:34 +0530

    feat(admin-ui): add dropdown to add users in project (#968)
    * chore: update apsara version
    
    * fix: lint issues
    
    * fix: navbar dropdown
    
    * fix: sidebar menu
    
    * fix: update navbar menu click
    
    * fix: dropdown migration
    
    * feat: add dropdown to fetch members
    
    * refactor: split project actions to reduce api call on render
    
    * fix: lint issues
    
    * feat: add member from project member dropdown
    
    * fix: styles and dialog close on dropdown click
    
    * fix: requested changes

---
+++ ui/src/pages/organizations/details/projects/useAddProjectMembers.tsx
@@ -0,0 +1,86 @@
+import { useCallback, useContext, useEffect, useMemo, useState } from "react";
+import type {
+  V1Beta1CreatePolicyForProjectBody,
+  V1Beta1User,
+} from "~/api/frontier";
+import { OrganizationContext } from "../contexts/organization-context";
+import { api } from "~/api";
+import { toast } from "@raystack/apsara/v1";
+import { DEFAULT_ROLES } from "~/utils/constants";
+
+interface useAddProjectMembersProps {
+  projectId: string;
+}
+
+export function useAddProjectMembers({ projectId }: useAddProjectMembersProps) {
+  const { orgMembersMap } = useContext(OrganizationContext);
+  const [isLoading, setIsLoading] = useState(false);
+  const [nonMembers, setNonMembers] = useState<V1Beta1User[]>([]);
+  const [searchQuery, setSearchQuery] = useState<string>("");
+
+  const fetchProjectMembers = useCallback(async () => {
+    if (!projectId) {
+      return;
+    }
+    try {
+      setIsLoading(true);
+      const resp = await api.frontierServiceListProjectUsers(projectId);
+      const members = resp.data.users || [];
+      const memberSet = new Set(members.map((member) => member.id));
+      const projectNonMembers = Object.values(orgMembersMap)
+        .filter((member) => !memberSet.has(member.id))
+        .sort((a, b) => {
+          const aName = a.title || a.email || "";
+          const bName = b.title || b.email || "";
+          return aName < bName ? -1 : 1;
+        });
+      setNonMembers(projectNonMembers);
+      return members;
+    } catch (error) {
+      console.error("Error fetching project members:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [orgMembersMap, projectId]);
+
+  const eligibleMembers = useMemo(() => {
+    return searchQuery
+      ? nonMembers.filter((member) => {
+          const name = member.title || member.email || "";
+          return name.toLowerCase().includes(searchQuery.toLowerCase());
+        })
+      : nonMembers;
+  }, [nonMembers, searchQuery]);
+
+  useEffect(() => {
+    fetchProjectMembers();
+  }, [fetchProjectMembers]);
+
+  const addMember = useCallback(
+    async (userId: string) => {
+      if (!userId || !projectId) return;
+      try {
+        const principal = `app/user:${userId}`;
+        const policy: V1Beta1CreatePolicyForProjectBody = {
+          role_id: DEFAULT_ROLES.PROJECT_VIEWER,
+          principal,
+        };
+        await api?.frontierServiceCreatePolicyForProject(projectId, policy);
+        toast.success("member added");
+        return fetchProjectMembers();
+      } catch (error: unknown) {
+        console.error(error);
+        toast.error("Something went wrong");
+      }
+    },
+    [projectId, fetchProjectMembers],
+  );
+
+  return {
+    isLoading,
+    eligibleMembers,
+    fetchProjectMembers,
+    setSearchQuery,
+    addMember,
+  };
+}
