c20a4a6518c2a7601cbb98e9ed6fe500047e2e32
paanSinghCoder <gaurav3017@gmail.com>
2025-06-25 21:18:59 +0530

    fix: team page

--- sdks/js/packages/core/react/components/organization/teams/index.tsx
+++ sdks/js/packages/core/react/components/organization/teams/index.tsx
@@ -152,7 +152,11 @@ const TeamsTable = ({
       <Flex direction="column" gap={7} className={styles.tableWrapper}>
         <Flex justify="between" gap={3}>
           <Flex gap={3} justify="start" className={styles.tableSearchWrapper}>
-            <DataTable.Search placeholder="Search by name " size="medium" />
+            {isLoading ? (
+              <Skeleton height={'34px'} width={'500px'} />
+            ) : (
+              <DataTable.Search placeholder="Search by name " size="medium" />
+            )}
             {canListOrgGroups ? (
               <Select
                 defaultValue={teamsSelectOptions[0].value}
@@ -172,7 +176,7 @@ const TeamsTable = ({
             ) : null}
           </Flex>
           {isLoading ? (
-            <Skeleton height={'32px'} width={'64px'} />
+            <Skeleton height='34px' width='64px' />
           ) : (
             <Tooltip
               message={AuthTooltipMessage}
@@ -180,7 +184,6 @@ const TeamsTable = ({
               disabled={canCreateGroup}
             >
               <Button
-                style={{ width: 'fit-content', height: '100%' }}
                 disabled={!canCreateGroup}
                 onClick={() => navigate({ to: '/teams/modal' })}
                 data-test-id="frontier-sdk-add-team-btn"
