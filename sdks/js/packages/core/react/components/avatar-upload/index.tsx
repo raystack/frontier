80009919bd9ba370f198e1660e25c29cb3355115
paanSinghCoder <gaurav3017@gmail.com>
2025-06-27 09:20:05 +0530

    chore: spelling

--- sdks/js/packages/core/react/components/avatar-upload/index.tsx
+++ sdks/js/packages/core/react/components/avatar-upload/index.tsx
@@ -14,6 +14,7 @@ import styles from './avatar-upload.module.css';
 interface CropModalProps {
   imgSrc?: string;
   onClose: () => void;
+  // eslint-disable-next-line no-unused-vars
   onSave: (data: string) => void;
 }
 
