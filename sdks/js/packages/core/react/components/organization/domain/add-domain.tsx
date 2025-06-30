c5993600051d435c19991ac409db9e0820e390e8
Gaurav Singh <gaurav3017@gmail.com>
2025-06-30 15:17:47 +0530

    Update sdks/js/packages/core/react/components/organization/domain/add-domain.tsx

--- sdks/js/packages/core/react/components/organization/domain/add-domain.tsx
+++ sdks/js/packages/core/react/components/organization/domain/add-domain.tsx
@@ -73,7 +73,7 @@ export const AddDomain = () => {
           <Dialog.Header>
             <Flex justify="between" align="center" style={{ width: '100%' }}>
               <Text size="large" weight="medium">
-                Add Domain
+                Add domain
               </Text>
 
               <Image
