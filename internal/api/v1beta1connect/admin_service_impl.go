a41f4f5574dcdca3a5a4476432f71d15267a28d0
Abhishek Sah <abhi.sah.97@gmail.com>
2025-07-01 10:29:41 +0530

    add handler to list all users

--- internal/api/v1beta1connect/admin_service_impl.go
+++ internal/api/v1beta1connect/admin_service_impl.go
@@ -4,11 +4,57 @@ import (
 	"context"
 
 	"connectrpc.com/connect"
+	"github.com/raystack/frontier/core/user"
 	frontierv1beta1 "github.com/raystack/frontier/proto/v1beta1"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
-func (h *ConnectHandler) ListAllUsers(context.Context, *connect.Request[frontierv1beta1.ListAllUsersRequest]) (*connect.Response[frontierv1beta1.ListAllUsersResponse], error) {
-	return nil, connect.NewError(connect.CodeUnimplemented, nil)
+func (h *ConnectHandler) ListAllUsers(ctx context.Context, request *connect.Request[frontierv1beta1.ListAllUsersRequest]) (*connect.Response[frontierv1beta1.ListAllUsersResponse], error) {
+	var users []*frontierv1beta1.User
+	usersList, err := h.userService.List(ctx, user.Filter{
+		Limit:   request.Msg.GetPageSize(),
+		Page:    request.Msg.GetPageNum(),
+		Keyword: request.Msg.GetKeyword(),
+		OrgID:   request.Msg.GetOrgId(),
+		GroupID: request.Msg.GetGroupId(),
+		State:   user.State(request.Msg.GetState()),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	for _, user := range usersList {
+		userPB, err := transformUserToPB(user)
+		if err != nil {
+			return nil, err
+		}
+		users = append(users, userPB)
+	}
+
+	return connect.NewResponse(&frontierv1beta1.ListAllUsersResponse{
+		Count: int32(len(users)),
+		Users: users,
+	}), nil
+
+}
+
+func transformUserToPB(usr user.User) (*frontierv1beta1.User, error) {
+	metaData, err := usr.Metadata.ToStructPB()
+	if err != nil {
+		return nil, err
+	}
+
+	return &frontierv1beta1.User{
+		Id:        usr.ID,
+		Title:     usr.Title,
+		Email:     usr.Email,
+		Name:      usr.Name,
+		Metadata:  metaData,
+		Avatar:    usr.Avatar,
+		State:     usr.State.String(),
+		CreatedAt: timestamppb.New(usr.CreatedAt),
+		UpdatedAt: timestamppb.New(usr.UpdatedAt),
+	}, nil
 }
 
 func (h *ConnectHandler) ListAllServiceUsers(context.Context, *connect.Request[frontierv1beta1.ListAllServiceUsersRequest]) (*connect.Response[frontierv1beta1.ListAllServiceUsersResponse], error) {
