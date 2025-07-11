e3f3eb748dbabf6968e236e6e827c47fb5f01f65
Abhishek Sah <abhi.sah.97@gmail.com>
2025-07-11 14:43:18 +0530

    refactor

--- pkg/server/connect_interceptors/authentication.go
+++ pkg/server/connect_interceptors/authentication.go
@@ -12,10 +12,6 @@ import (
 func UnaryAuthenticationCheck(h *v1beta1connect.ConnectHandler) connect.UnaryInterceptorFunc {
 	interceptor := func(next connect.UnaryFunc) connect.UnaryFunc {
 		return connect.UnaryFunc(func(ctx context.Context, req connect.AnyRequest) (connect.AnyResponse, error) {
-			// if _, ok := info.Server.(*health.Handler); ok {
-			// 	// pass through health handler
-			// 	return handler(ctx, req)
-			// }
 			if authenticationSkipList[req.Spec().Procedure] {
 				return next(ctx, req)
 			}
