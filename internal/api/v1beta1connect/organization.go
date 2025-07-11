00db713999d2c03de44fb40a984716a13a723023
Abhishek Sah <abhi.sah.97@gmail.com>
2025-07-11 10:18:03 +0530

    organize handler function into files for readability

---
+++ internal/api/v1beta1connect/organization.go
@@ -0,0 +1,53 @@
+package v1beta1connect
+
+import (
+	"context"
+
+	"connectrpc.com/connect"
+	"github.com/raystack/frontier/core/organization"
+	frontierv1beta1 "github.com/raystack/frontier/proto/v1beta1"
+
+	"github.com/raystack/frontier/pkg/errors"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func (h *ConnectHandler) GetOrganization(ctx context.Context, request *connect.Request[frontierv1beta1.GetOrganizationRequest]) (*connect.Response[frontierv1beta1.GetOrganizationResponse], error) {
+	fetchedOrg, err := h.orgService.GetRaw(ctx, request.Msg.GetId())
+	if err != nil {
+		switch {
+		case errors.Is(err, organization.ErrNotExist), errors.Is(err, organization.ErrInvalidID):
+			return nil, connect.NewError(connect.CodeNotFound, err)
+		case errors.Is(err, organization.ErrInvalidUUID):
+			return nil, connect.NewError(connect.CodeInvalidArgument, err)
+		default:
+			return nil, connect.NewError(connect.CodeInternal, err)
+		}
+	}
+
+	orgPB, err := transformOrgToPB(fetchedOrg)
+	if err != nil {
+		return nil, connect.NewError(connect.CodeInternal, err)
+	}
+
+	return connect.NewResponse(&frontierv1beta1.GetOrganizationResponse{
+		Organization: orgPB,
+	}), nil
+}
+
+func transformOrgToPB(org organization.Organization) (*frontierv1beta1.Organization, error) {
+	metaData, err := org.Metadata.ToStructPB()
+	if err != nil {
+		return nil, err
+	}
+
+	return &frontierv1beta1.Organization{
+		Id:        org.ID,
+		Name:      org.Name,
+		Title:     org.Title,
+		Metadata:  metaData,
+		State:     org.State.String(),
+		Avatar:    org.Avatar,
+		CreatedAt: timestamppb.New(org.CreatedAt),
+		UpdatedAt: timestamppb.New(org.UpdatedAt),
+	}, nil
+}
