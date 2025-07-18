8f9f5330ca27d54856996006f24e8a72a6f0a1a8
Rishabh <me@rsbh.dev>
2025-07-18 14:05:49 +0530

    chore: update proto

--- proto/v1beta1/admin.pb.validate.go
+++ proto/v1beta1/admin.pb.validate.go
@@ -15231,6 +15231,35 @@ func (m *GetCurrentAdminUserResponse) validate(all bool) error {
 		}
 	}
 
+	if all {
+		switch v := interface{}(m.GetServiceUser()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, GetCurrentAdminUserResponseValidationError{
+					field:  "ServiceUser",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, GetCurrentAdminUserResponseValidationError{
+					field:  "ServiceUser",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetServiceUser()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return GetCurrentAdminUserResponseValidationError{
+				field:  "ServiceUser",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
 	if len(errors) > 0 {
 		return GetCurrentAdminUserResponseMultiError(errors)
 	}
