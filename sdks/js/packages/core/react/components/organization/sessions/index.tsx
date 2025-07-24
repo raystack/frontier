cc2d024c492e78a04808f03690d3fb177f1f2e1c
paanSinghCoder <gaurav3017@gmail.com>
2025-07-24 15:58:48 +0200

    feat: add list items

--- sdks/js/packages/core/react/components/organization/sessions/index.tsx
+++ sdks/js/packages/core/react/components/organization/sessions/index.tsx
@@ -4,26 +4,53 @@ import {
   Text,
   Flex,
   Headline,
+  Button,
 } from '@raystack/apsara/v1';
 import { Outlet } from '@tanstack/react-router';
 import styles from './sessions.module.css';
 
 export default function SessionsPage() {
-
   return (
     <Flex direction="column" style={{ width: '100%' }}>
-      <Flex direction="column" gap={9} className={styles.container}>
-        <Flex direction="row" justify="between" align="center">
-        <Flex direction="column" gap={3}>
-          <Headline size="t1">
-            Session
-          </Headline>
-          <Text size="regular" variant="secondary">
-            Devices logged into this account.
-          </Text>
+      <Flex direction="column" className={styles.container}>
+        <Flex direction="row" justify="between" align="center" className={styles.header}>
+          <Flex direction="column" gap={2}>
+            <Headline size="t1">
+              Session
+            </Headline>
+            <Text size="regular" variant="secondary">
+              Devices logged into this account.
+            </Text>
+          </Flex>
         </Flex>
-      </Flex>
-        {/* Sessions list will go here */}
+        
+        <Flex direction="column">
+
+          <Flex justify="between" align="center" className={styles.sessionItem}>
+            <Flex direction="column" gap={3}>
+              <Text weight="medium" size="regular">Chrome on Mac OS x</Text>
+              <Flex gap={2} align="center">
+                <Text variant="tertiary" size="micro">Bangalore</Text>
+                <Text variant="tertiary" size="micro">•</Text>
+                <Text variant="success" size="micro">Current session</Text>
+              </Flex>
+            </Flex>
+            <Button variant="text" color="neutral">Revoke</Button>
+          </Flex>
+
+          <Flex justify="between" align="center" className={styles.sessionItem}>
+            <Flex direction="column" gap={3}>
+              <Text weight="medium" size="regular">Chrome on Mac OS x</Text>
+              <Flex gap={2} align="center">
+                <Text variant="tertiary" size="micro">Bangalore</Text>
+                <Text variant="tertiary" size="micro">•</Text>
+                <Text variant="tertiary" size="micro">Last active 10 minutes ago</Text>
+              </Flex>
+            </Flex>
+            <Button variant="text" color="neutral">Revoke</Button>
+          </Flex>
+
+        </Flex>
       </Flex>
       <Outlet />
     </Flex>
