03da4760fd17e840f90e18133b908958c767a50a
Abhishek Sah <abhi.sah.97@gmail.com>
2025-07-30 14:30:09 +0530

    feat: migrate OrgExport RPC to ConnectRPC server (#1080)
    * wip: streaming API
    
    * migrate export org billing api
    
    * implement interceptor interface for session
    
    * implement interceptor interface for authorization
    
    * implement WrapStreamingClient for Session interceptor
    
    * refactor
    
    * bump chunk size
    
    * remove redundant lines
    
    * compile fix

--- pkg/server/connect_interceptors/session.go
+++ pkg/server/connect_interceptors/session.go
@@ -10,6 +10,7 @@ import (
 	"github.com/lestrrat-go/jwx/v2/jwt"
 	"github.com/raystack/frontier/core/authenticate"
 
+	"github.com/raystack/frontier/internal/api/v1beta1connect"
 	"github.com/raystack/frontier/pkg/server/consts"
 
 	"connectrpc.com/connect"
@@ -17,15 +18,115 @@ import (
 	"google.golang.org/grpc/metadata"
 )
 
-type Session struct {
+type SessionInterceptor struct {
 	// TODO(kushsharma): server should be able to rotate encryption keys of codec
 	// use secure cookie EncodeMulti/DecodeMulti
 	cookieCodec securecookie.Codec
 	conf        authenticate.SessionConfig
+	h           *v1beta1connect.ConnectHandler
+}
+
+func (s *SessionInterceptor) WrapStreamingClient(next connect.StreamingClientFunc) connect.StreamingClientFunc {
+	return connect.StreamingClientFunc(func(ctx context.Context, spec connect.Spec) connect.StreamingClientConn {
+		conn := next(ctx, spec)
+		return conn
+	})
+}
+
+func (s *SessionInterceptor) WrapStreamingHandler(next connect.StreamingHandlerFunc) connect.StreamingHandlerFunc {
+	return connect.StreamingHandlerFunc(func(ctx context.Context, conn connect.StreamingHandlerConn) error {
+		// parse and process cookies
+		incomingMD := metadata.MD{}
+		if s.cookieCodec != nil {
+			if mdCookies := conn.RequestHeader().Get("cookie"); len(mdCookies) > 0 {
+				header := http.Header{}
+				header.Add("Cookie", mdCookies)
+				request := http.Request{Header: header}
+				for _, requestCookie := range request.Cookies() {
+					// check if cookie is session cookie
+					if requestCookie.Name == consts.SessionRequestKey {
+						var sessionID string
+						// extract and decode session from cookie
+						if err := s.cookieCodec.Decode(requestCookie.Name, requestCookie.Value, &sessionID); err == nil {
+							// pass cookie in context
+							incomingMD.Set(consts.SessionIDGatewayKey, strings.TrimSpace(sessionID))
+						}
+					}
+				}
+			}
+		}
+
+		// pass user token if in token header as gateway context
+		if userToken := conn.RequestHeader().Values(consts.UserTokenRequestKey); len(userToken) > 0 {
+			incomingMD.Set(consts.UserTokenGatewayKey, strings.TrimSpace(userToken[0]))
+		}
+		// check if the same token is part of Authorization header
+		if authHeader := conn.RequestHeader().Values("authorization"); len(authHeader) > 0 {
+			tokenVal := strings.TrimSpace(strings.TrimPrefix(authHeader[0], "Bearer "))
+			if token, err := jwt.ParseInsecure([]byte(tokenVal)); err == nil {
+				if token.JwtID() != "" && token.Expiration().After(time.Now().UTC()) {
+					incomingMD.Set(consts.UserTokenGatewayKey, tokenVal)
+				}
+			}
+			secretVal := strings.TrimSpace(strings.TrimPrefix(authHeader[0], "Basic "))
+			if len(secretVal) > 0 {
+				incomingMD.Set(consts.UserSecretGatewayKey, secretVal)
+			}
+		}
+
+		ctx = metadata.NewIncomingContext(ctx, incomingMD)
+		return next(ctx, conn)
+	})
+}
+
+func (s *SessionInterceptor) WrapUnary(next connect.UnaryFunc) connect.UnaryFunc {
+	return connect.UnaryFunc(func(ctx context.Context, req connect.AnyRequest) (resp connect.AnyResponse, err error) {
+		// parse and process cookies
+		incomingMD := metadata.MD{}
+		if s.cookieCodec != nil {
+			if mdCookies := req.Header().Get("cookie"); len(mdCookies) > 0 {
+				header := http.Header{}
+				header.Add("Cookie", mdCookies)
+				request := http.Request{Header: header}
+				for _, requestCookie := range request.Cookies() {
+					// check if cookie is session cookie
+					if requestCookie.Name == consts.SessionRequestKey {
+						var sessionID string
+						// extract and decode session from cookie
+						if err = s.cookieCodec.Decode(requestCookie.Name, requestCookie.Value, &sessionID); err == nil {
+							// pass cookie in context
+							incomingMD.Set(consts.SessionIDGatewayKey, strings.TrimSpace(sessionID))
+						}
+					}
+				}
+			}
+		}
+
+		// pass user token if in token header as gateway context
+		if userToken := req.Header().Values(consts.UserTokenRequestKey); len(userToken) > 0 {
+			incomingMD.Set(consts.UserTokenGatewayKey, strings.TrimSpace(userToken[0]))
+		}
+		// check if the same token is part of Authorization header
+		if authHeader := req.Header().Values("authorization"); len(authHeader) > 0 {
+			tokenVal := strings.TrimSpace(strings.TrimPrefix(authHeader[0], "Bearer "))
+			if token, err := jwt.ParseInsecure([]byte(tokenVal)); err == nil {
+				if token.JwtID() != "" && token.Expiration().After(time.Now().UTC()) {
+					incomingMD.Set(consts.UserTokenGatewayKey, tokenVal)
+				}
+			}
+			secretVal := strings.TrimSpace(strings.TrimPrefix(authHeader[0], "Basic "))
+			if len(secretVal) > 0 {
+				incomingMD.Set(consts.UserSecretGatewayKey, secretVal)
+			}
+		}
+
+		ctx = metadata.NewIncomingContext(ctx, incomingMD)
+		return next(ctx, req)
+	})
 }
 
 // UnaryConnectResponseInterceptor adds session cookie to response if session id is present in header
-func (s Session) UnaryConnectResponseInterceptor() connect.UnaryInterceptorFunc {
+func (s *SessionInterceptor) UnaryConnectResponseInterceptor() connect.UnaryInterceptorFunc {
 	interceptor := func(next connect.UnaryFunc) connect.UnaryFunc {
 		return connect.UnaryFunc(func(ctx context.Context, req connect.AnyRequest) (connect.AnyResponse, error) {
 			// Let the handler and other interceptors run first
@@ -85,11 +186,12 @@ func (s Session) UnaryConnectResponseInterceptor() connect.UnaryInterceptorFunc 
 	return connect.UnaryInterceptorFunc(interceptor)
 }
 
-func NewSession(cookieCutter securecookie.Codec, conf authenticate.SessionConfig) *Session {
-	return &Session{
+func NewSessionInterceptor(cookieCutter securecookie.Codec, conf authenticate.SessionConfig, h *v1beta1connect.ConnectHandler) *SessionInterceptor {
+	return &SessionInterceptor{
 		// could be nil if not configured by user
 		cookieCodec: cookieCutter,
 		conf:        conf,
+		h:           h,
 	}
 }
 
@@ -108,7 +210,7 @@ func CookieSameSite(name string) http.SameSite {
 
 // UnaryConnectRequestHeadersAnnotator converts session cookies set in grpc metadata to context
 // this requires decrypting the cookie and setting it as context
-func (s Session) UnaryConnectRequestHeadersAnnotator() connect.UnaryInterceptorFunc {
+func (s *SessionInterceptor) UnaryConnectRequestHeadersAnnotator() connect.UnaryInterceptorFunc {
 	interceptor := func(next connect.UnaryFunc) connect.UnaryFunc {
 		return connect.UnaryFunc(func(ctx context.Context, req connect.AnyRequest) (resp connect.AnyResponse, err error) {
 			// parse and process cookies
