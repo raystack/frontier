49d54f1e14ee04c4eac8d9f811bb42c831e1da03
Abhishek Sah <abhi.sah.97@gmail.com>
2025-08-20 10:38:32 +0530

    migrate ExportOrganizationProjects api

--- internal/api/v1beta1connect/organization_projects.go
+++ internal/api/v1beta1connect/organization_projects.go
@@ -11,6 +11,7 @@ import (
 	"github.com/raystack/frontier/pkg/utils"
 	frontierv1beta1 "github.com/raystack/frontier/proto/v1beta1"
 	"github.com/raystack/salt/rql"
+	httpbody "google.golang.org/genproto/googleapis/api/httpbody"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
@@ -77,4 +78,30 @@ func transformAggregatedProjectToPB(p orgprojects.AggregatedProject) *frontierv1
 		CreatedAt:      timestamppb.New(p.CreatedAt),
 		OrganizationId: p.OrganizationID,
 	}
+}
+
+func (h *ConnectHandler) ExportOrganizationProjects(ctx context.Context, request *connect.Request[frontierv1beta1.ExportOrganizationProjectsRequest], stream *connect.ServerStream[httpbody.HttpBody]) error {
+	orgProjectsDataBytes, contentType, err := h.orgProjectsService.Export(ctx, request.Msg.GetId())
+	if err != nil {
+		if errors.Is(err, orgprojects.ErrNoContent) {
+			return connect.NewError(connect.CodeInvalidArgument, fmt.Errorf("no data to export: %v", err))
+		}
+		return connect.NewError(connect.CodeInternal, ErrInternalServerError)
+	}
+
+	chunkSize := 1024 * 200 // 200KB chunks
+	for i := 0; i < len(orgProjectsDataBytes); i += chunkSize {
+		end := min(i+chunkSize, len(orgProjectsDataBytes))
+
+		chunk := orgProjectsDataBytes[i:end]
+		msg := &httpbody.HttpBody{
+			ContentType: contentType,
+			Data:        chunk,
+		}
+
+		if err := stream.Send(msg); err != nil {
+			return connect.NewError(connect.CodeInternal, ErrInternalServerError)
+		}
+	}
+	return nil
 }
