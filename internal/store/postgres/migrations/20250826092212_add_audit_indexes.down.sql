1a1cdf01219f01b3a4c46e75aa584ba54810ad9f
aman <aman@pixxel.co.in>
2025-08-28 14:05:15 +0530

    refactor: remove CONCURRENTLY option from audit log index operations

--- internal/store/postgres/migrations/20250826092212_add_audit_indexes.down.sql
+++ internal/store/postgres/migrations/20250826092212_add_audit_indexes.down.sql
@@ -1,7 +1,5 @@
--- +migrate Down notransaction
-
-DROP INDEX CONCURRENTLY IF EXISTS auditlogs_occurred_at_idx;
-DROP INDEX CONCURRENTLY IF EXISTS auditlogs_resource_type_idx;
-DROP INDEX CONCURRENTLY IF EXISTS auditlogs_resource_id_idx;
-DROP INDEX CONCURRENTLY IF EXISTS auditlogs_actor_id_idx;
-DROP INDEX CONCURRENTLY IF EXISTS auditlogs_idempotency_key_idx;
+DROP INDEX IF EXISTS auditlogs_occurred_at_idx;
+DROP INDEX IF EXISTS auditlogs_resource_type_idx;
+DROP INDEX IF EXISTS auditlogs_resource_id_idx;
+DROP INDEX IF EXISTS auditlogs_actor_id_idx;
+DROP INDEX IF EXISTS auditlogs_idempotency_key_idx;
