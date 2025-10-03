25660935f68ea3433d87738b99bdf80c1ceab175
Rohil Surana <rohilsurana96@gmail.com>
2025-10-03 12:45:09 +0530

    feat: migrate ListOrganizationGroups API to Connect RPC

--- internal/api/v1beta1connect/group.go
+++ internal/api/v1beta1connect/group.go
@@ -2,9 +2,13 @@ package v1beta1connect
 
 import (
 	"context"
+	"errors"
 
 	"connectrpc.com/connect"
 	"github.com/raystack/frontier/core/group"
+	"github.com/raystack/frontier/core/organization"
+	"github.com/raystack/frontier/core/user"
+	"github.com/raystack/frontier/pkg/utils"
 	frontierv1beta1 "github.com/raystack/frontier/proto/v1beta1"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
@@ -30,6 +34,63 @@ func (h *ConnectHandler) ListGroups(ctx context.Context, request *connect.Reques
 	}
 
 	return connect.NewResponse(&frontierv1beta1.ListGroupsResponse{Groups: groups}), nil
+}
+
+func (h *ConnectHandler) ListOrganizationGroups(ctx context.Context, request *connect.Request[frontierv1beta1.ListOrganizationGroupsRequest]) (*connect.Response[frontierv1beta1.ListOrganizationGroupsResponse], error) {
+	orgResp, err := h.orgService.Get(ctx, request.Msg.GetOrgId())
+	if err != nil {
+		switch {
+		case errors.Is(err, organization.ErrDisabled):
+			return nil, connect.NewError(connect.CodeNotFound, ErrOrgDisabled)
+		case errors.Is(err, organization.ErrNotExist):
+			return nil, connect.NewError(connect.CodeNotFound, ErrNotFound)
+		default:
+			return nil, connect.NewError(connect.CodeInternal, ErrInternalServerError)
+		}
+	}
+
+	var groups []*frontierv1beta1.Group
+	groupList, err := h.groupService.List(ctx, group.Filter{
+		OrganizationID:  orgResp.ID,
+		State:           group.State(request.Msg.GetState()),
+		GroupIDs:        request.Msg.GetGroupIds(),
+		WithMemberCount: request.Msg.GetWithMemberCount(),
+	})
+	if err != nil {
+		return nil, connect.NewError(connect.CodeInternal, ErrInternalServerError)
+	}
+
+	for _, v := range groupList {
+		groupPB, err := transformGroupToPB(v)
+		if err != nil {
+			return nil, connect.NewError(connect.CodeInternal, ErrInternalServerError)
+		}
+
+		if request.Msg.GetWithMembers() {
+			groupUsers, err := h.userService.ListByGroup(ctx, v.ID, "")
+			if err != nil {
+				return nil, connect.NewError(connect.CodeInternal, ErrInternalServerError)
+			}
+			var groupUsersErr error
+			groupPB.Users = utils.Filter(utils.Map(groupUsers, func(user user.User) *frontierv1beta1.User {
+				pb, err := transformUserToPB(user)
+				if err != nil {
+					groupUsersErr = errors.Join(groupUsersErr, err)
+					return nil
+				}
+				return pb
+			}), func(user *frontierv1beta1.User) bool {
+				return user != nil
+			})
+			if groupUsersErr != nil {
+				return nil, connect.NewError(connect.CodeInternal, ErrInternalServerError)
+			}
+		}
+
+		groups = append(groups, &groupPB)
+	}
+
+	return connect.NewResponse(&frontierv1beta1.ListOrganizationGroupsResponse{Groups: groups}), nil
 }
 
 func transformGroupToPB(grp group.Group) (frontierv1beta1.Group, error) {
