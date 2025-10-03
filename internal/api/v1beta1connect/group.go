315ef20a4c4659a1fecac65e2c486a9456b5d487
Rohil Surana <rohilsurana96@gmail.com>
2025-10-03 14:26:12 +0530

    feat: migrate DisableGroup API to Connect RPC

--- internal/api/v1beta1connect/group.go
+++ internal/api/v1beta1connect/group.go
@@ -392,6 +392,29 @@ func (h *ConnectHandler) EnableGroup(ctx context.Context, request *connect.Reque
 	return connect.NewResponse(&frontierv1beta1.EnableGroupResponse{}), nil
 }
 
+func (h *ConnectHandler) DisableGroup(ctx context.Context, request *connect.Request[frontierv1beta1.DisableGroupRequest]) (*connect.Response[frontierv1beta1.DisableGroupResponse], error) {
+	_, err := h.orgService.Get(ctx, request.Msg.GetOrgId())
+	if err != nil {
+		switch {
+		case errors.Is(err, organization.ErrDisabled):
+			return nil, connect.NewError(connect.CodeNotFound, ErrOrgDisabled)
+		case errors.Is(err, organization.ErrNotExist):
+			return nil, connect.NewError(connect.CodeNotFound, ErrOrgNotFound)
+		default:
+			return nil, connect.NewError(connect.CodeInternal, ErrInternalServerError)
+		}
+	}
+	if err := h.groupService.Disable(ctx, request.Msg.GetId()); err != nil {
+		switch {
+		case errors.Is(err, group.ErrNotExist):
+			return nil, connect.NewError(connect.CodeNotFound, ErrGroupNotFound)
+		default:
+			return nil, connect.NewError(connect.CodeInternal, ErrInternalServerError)
+		}
+	}
+	return connect.NewResponse(&frontierv1beta1.DisableGroupResponse{}), nil
+}
+
 func transformGroupToPB(grp group.Group) (frontierv1beta1.Group, error) {
 	metaData, err := grp.Metadata.ToStructPB()
 	if err != nil {
