232c72df78534d81357de7211c75d86468c0c04f
Rohil Surana <rohilsurana96@gmail.com>
2025-10-03 15:25:55 +0530

    feat: migrate CreateProjectResource API to Connect RPC

--- internal/api/v1beta1connect/resource.go
+++ internal/api/v1beta1connect/resource.go
@@ -2,10 +2,14 @@ package v1beta1connect
 
 import (
 	"context"
+	"errors"
 
 	"connectrpc.com/connect"
+	"github.com/raystack/frontier/core/audit"
 	"github.com/raystack/frontier/core/resource"
+	"github.com/raystack/frontier/core/user"
 	"github.com/raystack/frontier/internal/bootstrap/schema"
+	"github.com/raystack/frontier/pkg/metadata"
 	frontierv1beta1 "github.com/raystack/frontier/proto/v1beta1"
 	"google.golang.org/protobuf/types/known/structpb"
 	"google.golang.org/protobuf/types/known/timestamppb"
@@ -56,6 +60,67 @@ func (h *ConnectHandler) ListProjectResources(ctx context.Context, request *conn
 	}
 	return connect.NewResponse(&frontierv1beta1.ListProjectResourcesResponse{
 		Resources: resources,
+	}), nil
+}
+
+func (h *ConnectHandler) CreateProjectResource(ctx context.Context, request *connect.Request[frontierv1beta1.CreateProjectResourceRequest]) (*connect.Response[frontierv1beta1.CreateProjectResourceResponse], error) {
+	if request.Msg.GetBody() == nil {
+		return nil, connect.NewError(connect.CodeInvalidArgument, ErrBadRequest)
+	}
+
+	var metaDataMap metadata.Metadata
+	var err error
+	if request.Msg.GetBody().GetMetadata() != nil {
+		metaDataMap = metadata.Build(request.Msg.GetBody().GetMetadata().AsMap())
+	}
+
+	parentProject, err := h.projectService.Get(ctx, request.Msg.GetProjectId())
+	if err != nil {
+		return nil, connect.NewError(connect.CodeInternal, ErrInternalServerError)
+	}
+
+	principalType := schema.UserPrincipal
+	principalID := request.Msg.GetBody().GetPrincipal()
+	if ns, id, err := schema.SplitNamespaceAndResourceID(request.Msg.GetBody().GetPrincipal()); err == nil {
+		principalType = ns
+		principalID = id
+	}
+	namespaceID := schema.ParseNamespaceAliasIfRequired(request.Msg.GetBody().GetNamespace())
+	newResource, err := h.resourceService.Create(ctx, resource.Resource{
+		ID:            request.Msg.GetId(),
+		Name:          request.Msg.GetBody().GetName(),
+		Title:         request.Msg.GetBody().GetTitle(),
+		ProjectID:     parentProject.ID,
+		NamespaceID:   namespaceID,
+		PrincipalID:   principalID,
+		PrincipalType: principalType,
+		Metadata:      metaDataMap,
+	})
+	if err != nil {
+		switch {
+		case errors.Is(err, user.ErrInvalidEmail):
+			return nil, connect.NewError(connect.CodeUnauthenticated, ErrUnauthenticated)
+		case errors.Is(err, resource.ErrInvalidUUID),
+			errors.Is(err, resource.ErrInvalidDetail):
+			return nil, connect.NewError(connect.CodeInvalidArgument, ErrBadRequest)
+		case errors.Is(err, resource.ErrConflict):
+			return nil, connect.NewError(connect.CodeAlreadyExists, ErrConflictRequest)
+		default:
+			return nil, connect.NewError(connect.CodeInternal, ErrInternalServerError)
+		}
+	}
+
+	resourcePB, err := transformResourceToPB(newResource)
+	if err != nil {
+		return nil, connect.NewError(connect.CodeInternal, ErrInternalServerError)
+	}
+
+	audit.GetAuditor(ctx, parentProject.Organization.ID).Log(audit.ResourceCreatedEvent, audit.Target{
+		ID:   newResource.ID,
+		Type: newResource.NamespaceID,
+	})
+	return connect.NewResponse(&frontierv1beta1.CreateProjectResourceResponse{
+		Resource: resourcePB,
 	}), nil
 }
 
