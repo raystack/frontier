8d4c27cf4f69f5ec2ba547e9a9de34330624e037
paanSinghCoder <gaurav3017@gmail.com>
2026-02-03 16:34:02 +0530

    chore: add ESLint configuration and update lint script to skip turbo

--- web/tools/eslint-config/index.js
+++ web/tools/eslint-config/index.js
@@ -1,13 +1,15 @@
 /* eslint-disable strict */
+const extendsList = [
+  'next',
+  ...(process.env.ESLINT_SKIP_TURBO === '1' ? [] : ['turbo']),
+  'prettier',
+  "eslint:recommended",
+  'plugin:test-selectors/recommended'
+];
+
 module.exports = {
   plugins: ["test-selectors"],
-  extends: [
-    'next',
-    'turbo',
-    'prettier',
-    "eslint:recommended",
-    'plugin:test-selectors/recommended'
-  ],
+  extends: extendsList,
   rules: {
     '@next/next/no-html-link-for-pages': 'off',
     "no-unused-vars": "warn",
