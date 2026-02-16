1957ac84cba524a77393d348870115d057407d68
paanSinghCoder <gaurav3017@gmail.com>
2026-02-16 11:28:33 +0530

    refactor(vite.config): remove unused path and alias configurations

--- web/apps/admin/vite.config.ts
+++ web/apps/admin/vite.config.ts
@@ -1,13 +1,9 @@
 import react from "@vitejs/plugin-react-swc";
 import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import svgr from "vite-plugin-svgr";
 dotenv.config();
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
@@ -32,11 +28,6 @@ export default defineConfig(() => {
       fs: {
         // Allow serving files from one level up to the project root
         allow: [".."],
-      },
-    },
-    resolve: {
-      alias: {
-        "react-router-dom": path.resolve(__dirname, "node_modules/react-router-dom"),
       },
     },
     plugins: [react(), svgr(), tsconfigPaths()],
