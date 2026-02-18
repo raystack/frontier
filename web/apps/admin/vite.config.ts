635c627b9436346ed1ec445f67ee792f7b2ca907
paanSinghCoder <gaurav3017@gmail.com>
2026-02-18 15:46:30 +0530

    feat(audit-logs): remove react-router-dom dep

--- web/apps/admin/vite.config.ts
+++ web/apps/admin/vite.config.ts
@@ -1,14 +1,9 @@
 import react from "@vitejs/plugin-react-swc";
 import dotenv from "dotenv";
-import { createRequire } from "module";
-import path from "path";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import svgr from "vite-plugin-svgr";
 dotenv.config();
-
-const require = createRequire(import.meta.url);
-const reactRouterDomPath = path.dirname(require.resolve("react-router-dom/package.json"));
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
@@ -33,11 +28,6 @@ export default defineConfig(() => {
       fs: {
         // Allow serving files from one level up to the project root
         allow: [".."],
-      },
-    },
-    resolve: {
-      alias: {
-        "react-router-dom": reactRouterDomPath,
       },
     },
     plugins: [react(), svgr(), tsconfigPaths()],
