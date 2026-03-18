f66488d9bbaa7bc082438bcda5272ce5f2806c01
Rishabh <me@rsbh.dev>
2026-03-18 10:53:03 +0530

    fix: make project list clickable

--- web/sdk/react/views/projects/list/projects-columns.tsx
+++ web/sdk/react/views/projects/list/projects-columns.tsx
@@ -15,51 +15,39 @@ export const getColumns = (
     onProjectClick?: (projectId: string) => void,
     onDeleteProjectClick?: (projectId: string) => void
 ): DataTableColumnDef<Project, unknown>[] => [
-    {
-        header: 'Title',
-        accessorKey: 'title',
-        cell: ({ row, getValue }) => {
-            return (
-                <span
-                    onClick={() => onProjectClick?.(row.original.id || '')}
-                    style={{
-                        textDecoration: 'none',
-                        color: 'var(--rs-color-foreground-base-primary)',
-                        fontSize: 'var(--rs-font-size-small)',
-                        cursor: 'pointer'
-                    }}
-                >
-                    {getValue() as string}
-                </span>
-            );
-        }
-    },
-    {
-        header: 'Members',
-        accessorKey: 'membersCount',
-        cell: ({ row, getValue }) => {
-            const value = getValue() as string;
-            return value ? <Text>{value} members</Text> : null;
-        }
-    },
-    {
-        header: '',
-        accessorKey: 'id',
-        meta: {
-            style: {
-                textAlign: 'end'
-            }
-        },
-        enableSorting: false,
-        cell: ({ row, getValue }) => (
-            <ProjectActions
-                project={row.original as Project}
-                userAccessOnProject={userAccessOnProject}
-                onProjectClick={onProjectClick}
-                onDeleteProjectClick={onDeleteProjectClick}
-            />
-        )
+  {
+    header: 'Title',
+    accessorKey: 'title',
+    cell: ({ row, getValue }) => {
+      return <Text>{getValue() as string}</Text>;
     }
+  },
+  {
+    header: 'Members',
+    accessorKey: 'membersCount',
+    cell: ({ row, getValue }) => {
+      const value = getValue() as string;
+      return value ? <Text>{value} members</Text> : null;
+    }
+  },
+  {
+    header: '',
+    accessorKey: 'id',
+    meta: {
+      style: {
+        textAlign: 'end'
+      }
+    },
+    enableSorting: false,
+    cell: ({ row, getValue }) => (
+      <ProjectActions
+        project={row.original as Project}
+        userAccessOnProject={userAccessOnProject}
+        onProjectClick={onProjectClick}
+        onDeleteProjectClick={onDeleteProjectClick}
+      />
+    )
+  }
 ];
 
 const ProjectActions = ({
@@ -73,41 +61,52 @@ const ProjectActions = ({
     onProjectClick?: (projectId: string) => void;
     onDeleteProjectClick?: (projectId: string) => void;
 }) => {
-    const canUpdateProject = (userAccessOnProject[project.id!] ?? []).includes(
-        'update'
-    );
-    const canDeleteProject = (userAccessOnProject[project.id!] ?? []).includes(
-        'delete'
-    );
-    const canDoActions = canUpdateProject || canDeleteProject;
+  const canUpdateProject = (userAccessOnProject[project.id!] ?? []).includes(
+    'update'
+  );
+  const canDeleteProject = (userAccessOnProject[project.id!] ?? []).includes(
+    'delete'
+  );
+  const canDoActions = canUpdateProject || canDeleteProject;
 
-    return canDoActions ? (
-        <DropdownMenu placement="bottom-end">
-            <DropdownMenu.Trigger asChild style={{ cursor: 'pointer' }}>
-                <DotsHorizontalIcon />
-            </DropdownMenu.Trigger>
-            {/* @ts-ignore */}
-            <DropdownMenu.Content portal={false}>
-                <DropdownMenu.Group>
-                    {canUpdateProject ? (
-                        <DropdownMenu.Item
-                            onClick={() => onProjectClick?.(project.id || '')}
-                            className={orgStyles.dropdownActionItem}
-                        >
-                            <Pencil1Icon /> Rename
-                        </DropdownMenu.Item>
-                    ) : null}
-                    {canDeleteProject ? (
-                        <DropdownMenu.Item
-                            onClick={() => onDeleteProjectClick?.(project.id || '')}
-                            className={orgStyles.dropdownActionItem}
-                        >
-                            <TrashIcon /> Delete project
-                        </DropdownMenu.Item>
-                    ) : null}
-                </DropdownMenu.Group>
-            </DropdownMenu.Content>
-        </DropdownMenu>
-    ) : null;
+  function onDeleteClick(e: React.MouseEvent) {
+    e.stopPropagation();
+    onDeleteProjectClick?.(project.id || '');
+  }
+
+  function onRenameClick(e: React.MouseEvent) {
+    e.stopPropagation();
+    onProjectClick?.(project.id || '');
+  }
+
+  return canDoActions ? (
+    <DropdownMenu placement="bottom-end">
+      <DropdownMenu.Trigger asChild style={{ cursor: 'pointer' }}>
+        <DotsHorizontalIcon />
+      </DropdownMenu.Trigger>
+      {/* @ts-ignore */}
+      <DropdownMenu.Content portal={false}>
+        <DropdownMenu.Group>
+          {canUpdateProject ? (
+            <DropdownMenu.Item
+              onClick={onRenameClick}
+              className={orgStyles.dropdownActionItem}
+              data-test-id="frontier-sdk-project-list-rename-link"
+            >
+              <Pencil1Icon /> Rename
+            </DropdownMenu.Item>
+          ) : null}
+          {canDeleteProject ? (
+            <DropdownMenu.Item
+              onClick={onDeleteClick}
+              className={orgStyles.dropdownActionItem}
+              data-test-id="frontier-sdk-project-list-delete-link"
+            >
+              <TrashIcon /> Delete project
+            </DropdownMenu.Item>
+          ) : null}
+        </DropdownMenu.Group>
+      </DropdownMenu.Content>
+    </DropdownMenu>
+  ) : null;
 };
-
