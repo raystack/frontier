52708031212fc2bcdd791afeabece911d11f175b
Rohil Surana <rohilsurana96@gmail.com>
2026-06-08 12:05:51 +0530

    refactor(api): use shared ErrInternalServerError sentinel in error sanitizer

--- pkg/server/connect_interceptors/error_sanitizer.go
+++ pkg/server/connect_interceptors/error_sanitizer.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 
 	"connectrpc.com/connect"
+	pkgerrors "github.com/raystack/frontier/pkg/errors"
 )
 
 func UnaryConnectErrorSanitizerInterceptor() connect.UnaryInterceptorFunc {
@@ -17,12 +18,12 @@ func UnaryConnectErrorSanitizerInterceptor() connect.UnaryInterceptorFunc {
 
 			var connectErr *connect.Error
 			if !errors.As(err, &connectErr) {
-				return nil, connect.NewError(connect.CodeInternal, errors.New("internal server error"))
+				return nil, connect.NewError(connect.CodeInternal, pkgerrors.ErrInternalServerError)
 			}
 
 			code := connectErr.Code()
 			if code == connect.CodeInternal || code == connect.CodeUnknown {
-				return nil, connect.NewError(connect.CodeInternal, errors.New("internal server error"))
+				return nil, connect.NewError(connect.CodeInternal, pkgerrors.ErrInternalServerError)
 			}
 
 			return resp, err
